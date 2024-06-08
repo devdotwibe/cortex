b35147dd926b0f3ad4d520af82bd2c11ce160f59
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-06-08 10:45:09 +0530

    up

--- database/migrations/2024_06_07_145718_profile_detail_to_users.php
+++ database/migrations/2024_06_07_145718_profile_detail_to_users.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
             $table->string('phone')->nullable();
+            $table->string('schooling_year')->nullable();
         });
     }
 
