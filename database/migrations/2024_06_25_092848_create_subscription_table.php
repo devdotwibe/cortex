d0c09a2e95341e0d6094e769b8213cbb7d2dcdba
fathimafyrooz <fathimafyroozdotwibe@outlook.com>
2024-06-25 18:41:02 +0530

    Subscription files

---
+++ database/migrations/2024_06_25_092848_create_subscription_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscription', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->string('subscription_type_id')->nullable();
+            $table->string('payment_id');
+            $table->string('customer_id');
+            $table->string('status')->default('pending');
+            $table->decimal('amount',10,2);
+            $table->timestamp('start_date');
+            $table->timestamp('expiration_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscription');
+    }
+};
