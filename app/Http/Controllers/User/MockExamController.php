8ad14b311ac1be25a9e217ce1d1712f682a67ceb
Akhil Kannan <dev.dotwibe@gmail.com>
2024-06-26 15:00:26 +0530

    te

--- app/Http/Controllers/User/MockExamController.php
+++ app/Http/Controllers/User/MockExamController.php
@@ -3,9 +3,122 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\SubmitReview;
+use App\Models\Answer;
+use App\Models\Exam;
+use App\Models\Question;
+use App\Models\User;
+use App\Models\UserExamReview;
+use App\Trait\ResourceController;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MockExamController extends Controller
 {
-    //
+    
+    use ResourceController; 
+ 
+    public function index(Request $request){
+        self::reset();
+        self::$model = Exam::class; 
+
+        $exams=$this->where("name",'full-mock-exam')->buildPagination();
+         
+        /**
+         *  @var User
+         */
+        $user=Auth::user();
+        return view("user.full-mock-exam.index",compact('exams','user'));
+    } 
+
+    public function show(Request $request,Exam $exam){ 
+        /**
+         * @var User
+         */
+        $user=Auth::user(); 
+        if($request->ajax()){ 
+            if(!empty($request->question)){
+                $question=Question::findSlug($request->question);
+                return Answer::where('question_id',$question->id)->get(['slug','title']);
+            }
+            return Question::where('exam_id',$exam->id)->paginate(1,['slug','title','description','duration']);
+        }
+        $questioncount=Question::where('exam_id',$exam->id)->count();
+        $endtime=0;
+        foreach (Question::where('exam_id',$exam->id)->get() as $d) {
+            $endtime+=intval(explode(' ',$d->duration)[0]);
+            $user->setProgress("exam-{$exam->id}-answer-of-{$d->slug}",null);
+        }
+        $attemtcount=UserExamReview::where('exam_id',$exam->id)->count()+1;
+        return view("user.full-mock-exam.show",compact('exam','user','questioncount','endtime','attemtcount'));
+    } 
+
+    public function examverify(Request $request,Exam $exam){
+        $request->validate([
+            "question"=>'required'
+        ]);
+        /**
+        * @var User
+        */
+       $user=Auth::user();   
+        $question=Question::findSlug($request->question);
+        $ans=Answer::findSlug($request->answer);
+        if(empty($ans)||$ans->exam_id!=$exam->id||$ans->question_id!=$question->id||!$ans->iscorrect){
+            return response()->json(["iscorrect"=>false]);
+        }else{
+            return response()->json(["iscorrect"=>true]);
+        }
+    }
+
+    public function examsubmit(Request $request,Exam $exam){
+        /**
+         * @var User
+         */
+        $user=Auth::user(); 
+        $review=UserExamReview::store([
+            "title"=>$exam->title,
+            "name"=>$exam->name,
+            "progress"=>$user->progress("exam-".$exam->id,0),
+            "user_id"=>$user->id,
+            "exam_id"=>$exam->id,   
+        ]); 
+        $user->setProgress("exam-review-".$review->id."-timed","untimed");
+        $user->setProgress("exam-review-".$review->id."-timetaken",$request->input("timetaken",''));
+        $user->setProgress("exam-review-".$review->id."-flags",json_encode($request->input("flags",[])));
+        $user->setProgress("exam-review-".$review->id."-times",json_encode($request->input("times",[])));
+         
+        if($user->progress('exam-'.$exam->id.'-complete-date',"")==""){
+            $user->setProgress('exam-'.$exam->id.'-complete-date',date('Y-m-d H:i:s'));
+        }
+        $user->setProgress("exam-".$exam->id."-complete-review",'yes');
+        dispatch(new SubmitReview($review)); 
+        if($request->ajax()){
+            return  response()->json(["success"=>$exam->title." Submited","preview"=>route('full-mock-exam.preview',$review->slug)]);    
+        }
+        return  redirect()->route('full-mock-exam.index')->with("success",$exam->title." Submited");
+    }
+
+
+    public function examhistory(Request $request,Exam $exam){
+        /**
+         * @var User
+         */
+        $user=Auth::user();
+        $data=[];
+        foreach(UserExamReview::where('user_id',$user->id)->where('exam_id',$exam->id)->get() as  $row){
+            $data[]=[
+                'slug'=>$row->slug,
+                'date'=>Carbon::parse($row->created_at)->format('Y-m-d h:i a'),
+                'progress'=>$row->progress,
+                'url'=>route('full-mock-exam.preview',$row->slug),
+            ];
+        }
+        return [
+            'data'=>$data,
+            'url'=>route('full-mock-exam.show',$exam->slug),
+            'name'=>$exam->name
+        ];
+    }
+    
 }
---
+++ resources/views/user/full-mock-exam/preview.blade.php
@@ -0,0 +1,114 @@
+@extends('layouts.exam')
+@section('title', $exam->subtitle($category->id,"Topic ".($category->getIdx()+1)).':'.$category->name)
+@section('content')
+<section class="exam-container">
+    <div class="container-wrap">
+        <div class="lesson">            
+            <a class="lesson-exit float-start" href="{{route('topic-test.index')}}">
+                <img src="{{asset("assets/images/exiticon.svg")}}" alt="exiticon">
+            </a>
+            <div class="lesson-title">
+                <h3><span>{{$exam->subtitle($category->id,"Topic ".($category->getIdx()+1))}}</span><span> : </span><span>{{$category->name}}</span></h3>
+            </div>
+            <div class="lesson-body"> 
+                <div class="row" id="lesson-questionlist-list" style="display: none">
+                </div>
+            </div>
+            <div class="lesson-footer" id="lesson-footer-pagination"> 
+            </div>           
+        </div>
+    </div> 
+</section> 
+@endsection
+
+@push('footer-script') 
+
+<script>
+        function generateRandomId(length) {
+            const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            let result = '';
+            const charactersLength = characters.length;
+
+            for (let i = 0; i < length; i++) {
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+
+            return result;
+        }
+        function loadlessonreview(reviewurl){ 
+            $.get(reviewurl||"{{ route('topic-test.preview',$userExamReview->slug) }}",function(res){
+                $('.pagination-arrow').hide();
+                $('#lesson-footer-pagination').html('')
+                const lesseonId=generateRandomId(10); 
+                $.each(res.data,function(k,v){  
+                    $('#lesson-questionlist-list').html(`
+                        <div class="col-md-12">
+                            <div class="mcq-row" >
+                                <div class="mcq-title">
+                                    <span>${v.title||""}</span>
+                                </div>
+                                <div class="mcq-container">
+                                    <div id="mcq-${lesseonId}">
+                                        ${v.note||""}
+                                    </div>
+                                    <div id="mcq-${lesseonId}-ans" class="form-group">
+                                        <div class="form-data" >
+                                            <div class="forms-inputs mb-4" id="mcq-${lesseonId}-list"> 
+                                                
+                                            </div> 
+                                        </div>
+                                    </div>
+                                    <div id="mcq-${lesseonId}-explanation"> 
+                                        <label>Correct Answer <span id="mcq-${lesseonId}-correct"></span></label>
+                                        ${v.explanation||''}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    `).fadeIn();
+                    $.get("{{ route('topic-test.preview',$userExamReview->slug) }}",{question:v.slug},function(ans){
+                        $(`#mcq-${lesseonId}-list`).html('')
+                        $.each(ans,function(ai,av){
+                            const letter = String.fromCharCode(ai + 'A'.charCodeAt(0))
+                            $(`#mcq-${lesseonId}-list`).append(`
+                            <div class="form-check-ans">
+                                <span class="question-user-ans ${av.iscorrect?"correct":"wrong"}" data-ans="${av.slug}"></span>
+                                <div class="form-check">
+                                    <input type="radio" disabled name="answer" data-question="${v.slug}" id="user-answer-${lesseonId}-ans-item-${ai}" value="${av.slug}" class="form-check-input" ${av.user_answer?"checked":""}  >        
+                                    <label for="user-answer-${lesseonId}-ans-item-${ai}" >${ letter }. ${av.title}</label>
+                                </div>  
+                            </div>
+                            `)
+                            if(av.iscorrect){
+                                $(`#mcq-${lesseonId}-correct`).text(`: ${ letter } `)
+                            }
+                        }) 
+                    },'json')
+                     
+                }) 
+                if(res.total>1){
+                     $.each(res.links,function(k,v){
+                        if(v.active||!v.url){
+                            $('#lesson-footer-pagination').append(`
+                                <button class="btn btn-secondary ${v.active?"active":""}" disabled  >${v.label}</button>
+                            `)
+                        }else{
+                            $('#lesson-footer-pagination').append(`
+                                <button class="btn btn-secondary" onclick="loadlessonreview('${v.url}')" >${v.label}</button>
+                            `)
+                        }
+                     })
+                }
+ 
+                $('.lesson-end').show();
+            },'json')
+
+         }
+
+         $(function(){
+            loadlessonreview()
+         })
+
+</script>
+
+@endpush
