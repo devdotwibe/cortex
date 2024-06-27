9ada6c1b899b764cb0c0361f2b7b7ffd9444dd20
fathimafyrooz <fathimafyroozdotwibe@outlook.com>
2024-06-27 19:49:55 +0530

    admin route

--- routes/admin.php
+++ routes/admin.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Http\Controllers\Admin\CategoryController;
-use App\Http\Controllers\Admin\ExamController; 
+use App\Http\Controllers\Admin\ExamController;
 use App\Http\Controllers\Admin\FullMockExamController;
 use App\Http\Controllers\Admin\LearnController;
-use App\Http\Controllers\Admin\MainController as AdminMainController; 
-use App\Http\Controllers\Admin\QuestionBankController; 
+use App\Http\Controllers\Admin\MainController as AdminMainController;
+use App\Http\Controllers\Admin\QuestionBankController;
 use App\Http\Controllers\Admin\QuestionController;
 use App\Http\Controllers\Admin\SetController;
 use App\Http\Controllers\Admin\SubCategoryController;
@@ -30,42 +30,43 @@ Route::name('admin.')->prefix('admin')->group(function(){
         Route::resource("/user",UserController::class);
         Route::post('/user/{user}/resetpassword',[UserController::class,'resetpassword'])->name('user.resetpassword');
         Route::post('/user/bulk/action',[UserController::class,'bulkaction'])->name('user.bulkaction');
+        Route::post('/user/getdata',[UserController::class,'getdata'])->name('user.students');
         Route::resource("/exam",ExamController::class);
 
-        Route::prefix('full-mock-exam')->name('full-mock-exam.')->group(function () { 
+        Route::prefix('full-mock-exam')->name('full-mock-exam.')->group(function () {
             Route::get('/{exam}',[FullMockExamController::class,'index'])->name('index');
             Route::get('/{exam}/question/{question}',[FullMockExamController::class,'show'])->name('show');
             Route::get('/{exam}/create',[FullMockExamController::class,'create'])->name('create');
             Route::get('/{exam}/question/{question}/edit',[FullMockExamController::class,'edit'])->name('edit');
-            Route::post('/{exam}/store',[FullMockExamController::class,'store'])->name('store');  
+            Route::post('/{exam}/store',[FullMockExamController::class,'store'])->name('store');
         });
-        Route::prefix('question-bank')->name('question-bank.')->group(function () { 
+        Route::prefix('question-bank')->name('question-bank.')->group(function () {
             Route::get('/',[QuestionBankController::class,'index'])->name('index');
-            Route::post('/subtitle',[QuestionBankController::class,'subtitle'])->name('subtitle');  
+            Route::post('/subtitle',[QuestionBankController::class,'subtitle'])->name('subtitle');
             Route::get('/{category}',[QuestionBankController::class,'show'])->name('show');
             Route::get('/{category}/create',[QuestionBankController::class,'create'])->name('create');
             Route::get('/{category}/{question}/edit',[QuestionBankController::class,'edit'])->name('edit');
-            Route::post('/{category}/store',[QuestionBankController::class,'store'])->name('store');  
+            Route::post('/{category}/store',[QuestionBankController::class,'store'])->name('store');
         });
-       
-        Route::prefix('topic-test')->name('topic-test.')->group(function () { 
+
+        Route::prefix('topic-test')->name('topic-test.')->group(function () {
             Route::get('/',[TopicTestController::class,'index'])->name('index');
-            Route::post('/subtitle',[TopicTestController::class,'subtitle'])->name('subtitle');  
+            Route::post('/subtitle',[TopicTestController::class,'subtitle'])->name('subtitle');
             Route::get('/{category}',[TopicTestController::class,'show'])->name('show');
             Route::get('/{category}/create',[TopicTestController::class,'create'])->name('create');
             Route::get('/{category}/{question}/edit',[TopicTestController::class,'edit'])->name('edit');
-            Route::post('/{category}/store',[TopicTestController::class,'store'])->name('store');  
+            Route::post('/{category}/store',[TopicTestController::class,'store'])->name('store');
         });
         Route::resource("/question",QuestionController::class);
 
         // Route::resource("/learn",LearnController::class);
 
-        Route::prefix('learn')->name('learn.')->group(function () { 
+        Route::prefix('learn')->name('learn.')->group(function () {
             Route::get('/',[LearnController::class,'index'])->name('index');
             Route::get('/{category}',[LearnController::class,'show'])->name('show');
             Route::get('/{category}/create',[LearnController::class,'create'])->name('create');
             Route::get('/{category}/{learn}/edit',[LearnController::class,'edit'])->name('edit');
-            Route::post('/{category}/store',[LearnController::class,'store'])->name('store');  
+            Route::post('/{category}/store',[LearnController::class,'store'])->name('store');
             Route::put('/{category}/{learn}/update',[LearnController::class,'update'])->name('update');
 
             Route::delete('/{category}/{learn}',[LearnController::class,'destroy'])->name('destroy');
@@ -73,7 +74,7 @@ Route::name('admin.')->prefix('admin')->group(function(){
 
         // Route::resource("/options",CategoryController::class);
 
-        Route::prefix('options')->name('options.')->group(function () { 
+        Route::prefix('options')->name('options.')->group(function () {
             Route::get('/',[CategoryController::class,'index'])->name('index');
             Route::get('/create',[CategoryController::class,'create'])->name('create');
             Route::post('/',[CategoryController::class,'store'])->name('store');
@@ -87,11 +88,11 @@ Route::name('admin.')->prefix('admin')->group(function(){
         Route::post('/add-subcatecory/{slug}',[CategoryController::class,'add_subcatecory'])->name('add_subcatecory');
 
         Route::get('/get-category',[CategoryController::class,'get_edit_details'])->name('get_edit_details');
-        
+
 
         // Route::resource("/subcategory",SubCategoryController::class);
 
-        Route::prefix('subcategory')->name('subcategory.')->group(function () { 
+        Route::prefix('subcategory')->name('subcategory.')->group(function () {
             Route::get('/',[SubCategoryController::class,'index'])->name('index');
             Route::get('/create',[SubCategoryController::class,'create'])->name('create');
             Route::post('/',[SubCategoryController::class,'store'])->name('store');
@@ -106,7 +107,7 @@ Route::name('admin.')->prefix('admin')->group(function(){
 
         // Route::resource("/set",SetController::class);
 
-        Route::prefix('set')->name('set.')->group(function () { 
+        Route::prefix('set')->name('set.')->group(function () {
             Route::get('/',[SetController::class,'index'])->name('index');
             Route::get('/create',[SetController::class,'create'])->name('create');
             Route::post('/',[SetController::class,'store'])->name('store');
@@ -120,9 +121,9 @@ Route::name('admin.')->prefix('admin')->group(function(){
         Route::get('/set/view',[SetController::class,'set_table_show'])->name('set_table.show');
 
         Route::post('/set/store/{slug}',[SetController::class,'set_store'])->name('set.set_store');
-        
 
-        
+
+
 
     });
 
