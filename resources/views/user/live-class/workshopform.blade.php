c785ff9f815bade652b66c68166a2816a0bb5009
Akhil Kannan <dev.dotwibe@gmail.com>
2024-07-08 12:05:46 +0530

    c

--- resources/views/user/live-class/workshopform.blade.php
+++ resources/views/user/live-class/workshopform.blade.php
@@ -13,7 +13,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="workshop-content"> 
-                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfc-gOkpIRCfsvQjUjwPuVdj9lCnzcvNGjatEbWsgdxCUjXJQ/viewform?embedded=true" width="640" height="1084" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfc-gOkpIRCfsvQjUjwPuVdj9lCnzcvNGjatEbWsgdxCUjXJQ/viewform?embedded=true" width="100%" height="1084" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
                 </div> 
             </div>
         </div>
