d39dc0c0dc3bb07f300e9d71546a52cfbf57e814
Akhil Kannan <dev.dotwibe@gmail.com>
2024-07-10 20:45:14 +0530

    te

--- resources/views/user/live-class/pdfrender.blade.php
+++ resources/views/user/live-class/pdfrender.blade.php
@@ -64,11 +64,11 @@
             windowContent += '<html>';
             windowContent += '<head><title>{{ucfirst($subLessonMaterial->pdf_name)}}</title></head>';
             windowContent += '<body>';
-            windowContent += '<img src="' + canvas.toDataURL() + '" onload="window.print();window.close()" />';
+            windowContent += '<img src="' + canvas.toDataURL() + '" onload="window.print();window.close()"  />';
             windowContent += '</body>';
             windowContent += '</html>';
 
-            var printWin = window.open('', '', 'width=800,height=600');
+            var printWin = window.open('', '', 'width=500,height=800');
             printWin.document.open();
             printWin.document.write(windowContent);
             printWin.document.close();
