04c63b4cb4a4b71746ea8c80818380f32608fb62
Akhil Kannan <dev.dotwibe@gmail.com>
2024-07-10 18:42:24 +0530

    cc

--- app/Http/Controllers/User/LiveClassController.php
+++ app/Http/Controllers/User/LiveClassController.php
@@ -146,7 +146,7 @@ class LiveClassController extends Controller
             $count= $imginfo->getNumberImages();
         
             $imagic = new \Imagick();
-            // $imagic->setResolution(180, 253);
+            $imagic->setResolution(127, 179);
             $imagic->readImage($filepath);
             
             $imgdata=[]; 
