28fda39db09bb2d482e90f4db5cafe0ecf83ebbc
Akhil Kannan <dev.dotwibe@gmail.com>
2024-07-10 15:41:05 +0530

    te

--- public/assets/js/worker.js
+++ public/assets/js/worker.js
@@ -2,18 +2,11 @@ decryptData=async function(encryptedData, key) {
     let encryptedBinary = atob(encryptedData); 
     let iv = encryptedBinary.slice(0, 16);
     let encryptedText = encryptedBinary.slice(16); 
-    let keyBuffer =new TextEncoder().encode(key) // new Uint8Array(Array.prototype.map.call(key, function(c) { return c.charCodeAt(0); }));// new TextEncoder().encode(key);
+    let keyBuffer = new Uint8Array(Array.prototype.map.call(key, function(c) { return c.charCodeAt(0); }));// new TextEncoder().encode(key);
     let keyArray = await crypto.subtle.importKey('raw', keyBuffer, { name: 'AES-CBC',length: 256 }, false,['decrypt']); 
     let ivBuffer = new Uint8Array(Array.prototype.map.call(iv, function(c) { return c.charCodeAt(0); }));
     let encryptedDataBuffer = new Uint8Array(Array.prototype.map.call(encryptedText, function(c) { return c.charCodeAt(0); }));
-    
-    let decryptedData =await  crypto.subtle.decrypt({ name: 'AES-CBC',length: 256, iv: ivBuffer }, keyArray, encryptedDataBuffer).then(function(d){
-        console.log(d)
-        return d;
-    }).catch(function(re){
-        console.log(re)
-        return new Uint8Array([]);
-    });
+    let decryptedData =await  crypto.subtle.decrypt({ name: 'AES-CBC',length: 256, iv: ivBuffer }, keyArray, encryptedDataBuffer);
     let decryptedText = new TextDecoder().decode(decryptedData);
     return decryptedText;
 };
