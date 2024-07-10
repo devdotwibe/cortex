581cc4755ae0540086c2e4bab9af71f326600605
Akhil Kannan <dev.dotwibe@gmail.com>
2024-07-10 14:20:33 +0530

    te

--- public/assets/js/worker.js
+++ public/assets/js/worker.js
@@ -3,10 +3,10 @@ async function decryptData(encryptedData, key) {
     let iv = encryptedBinary.slice(0, 16);
     let encryptedText = encryptedBinary.slice(16); 
     let keyBuffer = new TextEncoder().encode(key);
-    let keyArray = await crypto.subtle.importKey('raw', keyBuffer, { name: 'AES-256-CBC' }, false,['encrypt', 'decrypt']); 
+    let keyArray = await crypto.subtle.importKey('raw', keyBuffer, { name: 'AES-CBC',length: 256 }, false,['encrypt', 'decrypt']); 
     let ivBuffer = new TextEncoder().encode(iv);
     let encryptedDataBuffer = new Uint8Array(Array.prototype.map.call(encryptedText, function(c) { return c.charCodeAt(0); }));
-    let decryptedData = await crypto.subtle.decrypt({ name: 'AES-256-CBC', iv: ivBuffer }, keyArray, encryptedDataBuffer);
+    let decryptedData = await crypto.subtle.decrypt({ name: 'AES-CBC', iv: ivBuffer }, keyArray, encryptedDataBuffer);
     let decryptedText = new TextDecoder().decode(decryptedData);
     return decryptedText;
 }  
