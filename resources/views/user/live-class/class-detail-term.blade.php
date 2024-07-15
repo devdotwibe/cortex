12647f8c55fe616298ac8b747210c6c39123581b
Akhil Kannan <dev.dotwibe@gmail.com>
2024-07-15 11:38:10 +0530

    te

--- resources/views/user/live-class/class-detail-term.blade.php
+++ resources/views/user/live-class/class-detail-term.blade.php
@@ -8,4 +8,36 @@
         </div>
     </div>
 </section>
+
+<section class="content_section">
+    <div class="container">
+        <div class="row">
+            @foreach ($terms as  $k=>$item)
+            <div class="col-md-6">
+                <div class="card mb-3">
+                    <div class="card-body">
+                         
+                            <div class="category">
+                                <div class="category-image">
+                                    <img src="{{ asset('assets/images/User-red.png') }}">
+                                </div> 
+                                <div class="category-content"> 
+                                    <div class="class-term">
+                                        <span>Meeting ID : {{$item->meeting_id}}</span>
+                                    </div>
+                                    <div class="class-term">
+                                        <span>Passcode : {{$item->passcode}}</span>
+                                    </div>
+                                    <div class="class-term">
+                                        <span>Zoom Link : <a href="{{$item->zoom_link}}" target="_blank" rel="noopener noreferrer">{{$item->meeting_id}}</a> </span>
+                                    </div>
+                                </div>
+                            </div> 
+                    </div>
+                </div>
+            </div>                
+            @endforeach
+        </div>
+    </div>
+</section>
 @endsection
