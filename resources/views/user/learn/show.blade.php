39a86f6816f2659217ee8b975f3cf8dce4e6b34f
Akhil Kannan <dev.dotwibe@gmail.com>
2024-07-15 20:36:12 +0530

    v

--- resources/views/user/learn/show.blade.php
+++ resources/views/user/learn/show.blade.php
@@ -51,7 +51,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Lesson <span id="review-history-label"></span></h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
