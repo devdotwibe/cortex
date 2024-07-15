9fe29530cbd09e53d3f92d5c6e7b556329102b42
Akhil Kannan <dev.dotwibe@gmail.com>
2024-07-15 15:47:53 +0530

    vvvv

--- app/Models/RecordVideo.php
+++ app/Models/RecordVideo.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\VisibleStatus;
 use App\Trait\ResourceModel;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+#[ScopedBy([VisibleStatus::class])]
 class RecordVideo extends Model
 {
     use HasFactory,ResourceModel;
