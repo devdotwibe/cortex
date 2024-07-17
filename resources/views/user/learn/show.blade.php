4485fe7850a904866b6311be8d670c7b31acced5
Akhil Kannan <dev.dotwibe@gmail.com>
2024-07-17 18:17:11 +0530

    te

--- resources/views/user/learn/show.blade.php
+++ resources/views/user/learn/show.blade.php
@@ -83,26 +83,6 @@
     </div>
 </div>
  
-@if ($user->progress('cortext-subscription-payment','')!="paid")
-<div class="modal fade" id="cortext-subscription-payment-modal" tabindex="-1" role="dialog"  aria-labelledby="cortext-subscription-paymentLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="cortext-subscription-paymentLablel">Subscription</h5>
-                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            </div>
-            <div class="modal-body">
-                <form action="{{route('payment.subscription')}}"  id="cortext-subscription-payment-form" >
-                    <p>The {{config('app.name')}} Subscription Peyment required </p>
-                    <button type="button" data-bs-dismiss="modal"  class="btn btn-secondary">Cancel</button>
-                    <button type="submit" class="btn btn-dark">Pay Now ${{ get_option('stripe.subscription.payment.amount-price','0') }} </button>
-                </form>
-            </div>
-
-        </div>
-    </div>
-</div>
-@endif
 @endpush
 
 @push('footer-script') 
