61af29705f4aa16558635e4b4c09356a960c88c1
Akhil Kannan <dev.dotwibe@gmail.com>
2024-07-17 17:57:58 +0530

    te

--- app/Http/Middleware/SubscriptionCheck.php
+++ app/Http/Middleware/SubscriptionCheck.php
@@ -28,19 +28,11 @@ class SubscriptionCheck
          */
         $user=Auth::user();
         if(in_array('learn',$opt)){
-            $cat=$request->route('category');
-            if(!empty($cat)){
-                $category=Category::findSlug($cat);
-                if(empty($category)){
-                    return $next($request);
-                }
+            $category=$request->route('category');
+            if(!empty($category)){ 
                 if(Category::where('id','<',$category->id)->whereIn("id",Learn::select('category_id'))->count()==0){
-                    $scat=$request->route('sub_category');
-                    if(!empty($scat)){
-                        $subcategory=SubCategory::findSlug($scat);
-                        if(empty($subcategory)){
-                            return $next($request);
-                        }
+                    $subcategory=$request->route('sub_category');
+                    if(!empty($subcategory)){ 
                         if(SubCategory::where('id','<',$subcategory->id)->whereIn("id",Learn::select('sub_category_id'))->count()==0){
                             return $next($request);
                         }
@@ -57,13 +49,8 @@ class SubscriptionCheck
          */
         $user=Auth::user();
         if(in_array('question-bank',$opt)){
-            $cat=$request->route('category');
-            if(!empty($cat)){
-                $category=Category::findSlug($cat);
-                if(empty($category)){
-                    // return $next($request);
-                    echo $cat;exit;
-                }
+            $category=$request->route('category');
+            if(!empty($category)){ 
                 $exam=Exam::where("name",'question-bank')->first();
                 if(empty($exam)){
                     $exam=Exam::store([
@@ -73,12 +60,8 @@ class SubscriptionCheck
                     $exam=Exam::find( $exam->id );
                 }
                 if(Category::where('id','<',$category->id)->whereIn("id",Question::where('exam_id',$exam->id)->select('category_id'))->count()==0){
-                    $scat=$request->route('setname');
-                    if(!empty($scat)){
-                        $setname=Setname::findSlug($scat);
-                        if(empty($setname)){
-                            return $next($request);
-                        }
+                    $setname=$request->route('setname');
+                    if(!empty($setname)){ 
                         if(Setname::where('id','<',$setname->id)->whereIn("id",Question::where('exam_id',$exam->id)->select('sub_category_set'))->count()==0){
                             return $next($request);
                         }
@@ -90,12 +73,8 @@ class SubscriptionCheck
 
         }
         if(in_array('topic-test',$opt)){
-            $cat=$request->route('category');
-            if(!empty($cat)){
-                $category=Category::findSlug($cat);
-                if(empty($category)){
-                    return $next($request);
-                }
+            $category=$request->route('category');
+            if(!empty($category)){ 
                 $exam=Exam::where("name",'topic-test')->first();
                 if(empty($exam)){
                     $exam=Exam::store([
@@ -111,12 +90,8 @@ class SubscriptionCheck
         }
 
         if(in_array('full-mock-exam',$opt)){
-            $xm=$request->route('exam');
-            if(!empty($xm)){
-                $exam=Exam::findSlug($xm);
-                if(empty($exam)){
-                    return $next($request);
-                }
+            $exam=$request->route('exam');
+            if(!empty($exam)){ 
                 if(Exam::where('id','<',$exam->id)->where("name",'full-mock-exam')->whereIn("id",Question::select('exam_id'))->count()==0){ 
                     return $next($request);
                 }
