2891f89c2d8c1f7e58345342614ee7fdd9d32890
Akhil Kannan <dev.dotwibe@gmail.com>
2024-08-05 16:53:55 +0530

    gg

--- app/Http/Controllers/User/MainController.php
+++ app/Http/Controllers/User/MainController.php
@@ -4,12 +4,42 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\UserExamReview;
+use App\Models\UserReviewAnswer;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class MainController extends Controller
 { 
     public function index(Request $request){
+        /**
+         * @var User
+         */
+        $user=Auth::user();
+        if($request->ajax()){
+            $responceData=[];
+            $start=Carbon::parse($request->startStr??date("Y-m-d"));
+            $end=Carbon::parse($request->endStr??date("Y-m-d"));
+            foreach (CarbonPeriod::create($start, $end) as $date) {
+                $cnt=UserReviewAnswer::whereDate('created_at',$date->format('Y-m-d'))->where('user_id',$user->id)->whereIn('user_exam_review_id',UserExamReview::where('user_id',$user->id)->groupBy('exam_id')->select(DB::raw('MAX(id)')))->where('iscorrect',true)->where('user_answer',true)->count();
+                $bgcolor="#8FFFAD";
+                if($cnt>30){
+                    $bgcolor="#21853C";
+                }
+                $responceData[]=[ 
+                    "start"=>$date->format('Y-m-d'),
+                    "rendering"=> 'background',
+                    "backgroundColor"=> "$bgcolor", 
+                    "title"=> "$cnt",
+                    "textColor"=> '#FFFFFF',
+                    "className"=> 'event-full',
+                ];
+            }
+            return response()->json($responceData);
+        }
         return view("user.dashboard");
     }
 
--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -4,76 +4,113 @@
 <section class="header_nav">
     <div class="header_wrapp">
         <div class="header_title">
-            <h2>Dashboard</h2>
+            <h2>My Journey</h2>
+        </div> 
+        <div class="progress-list">
+            <div class="progress-item">
+                <div class="progress-body">
+                    <span class="progress-notification"></span>
+                    <div class="progress-avathar">
+                        <img src="{{asset('assets/images/lessonmaterial.svg')}}" alt="">
+                    </div>
+                    <div class="progress-title">
+                        <strong>Learn</strong>
+                    </div>
+                </div>
+            </div>
+            <div class="progress-item">
+                <div class="progress-body">
+                    <span class="progress-notification"></span>
+                    <div class="progress-avathar">
+                        <img src="{{asset('assets/images/lessonmaterial.svg')}}" alt="">
+                    </div>
+                    <div class="progress-title">
+                        <strong>Practice</strong>
+                    </div>
+                </div>
+            </div>
+            <div class="progress-item">
+                <div class="progress-body">
+                    <span class="progress-notification"></span>
+                    <div class="progress-avathar">
+                        <img src="{{asset('assets/images/lessonmaterial.svg')}}" alt="">
+                    </div>
+                    <div class="progress-title">
+                        <strong>Simulate</strong>
+                    </div>
+                </div>
+            </div>
+            <div class="progress-item">
+                <div class="progress-body">
+                    <span class="progress-notification"></span>
+                    <div class="progress-avathar">
+                        <img src="{{asset('assets/images/lessonmaterial.svg')}}" alt="">
+                    </div>
+                    <div class="progress-title">
+                        <strong>Review</strong>
+                    </div>
+                </div>
+            </div>
         </div>
-        {{-- <div class="header_right">
-            <ul class="nav_bar">
-                <li class="nav_item"><a href="" class="nav_link btn">Demo</a></li>
-            </ul>
-        </div> --}}
     </div>
 </section>
-{{-- <section class="content_section">
-    <div class="container">
-        <div class="row">
-            <div class="dash_card">
-                <div class="admin-icon">
-                    <span class="wht-icon"><img
-                            src="https://suryaplacement.com/assets/images/Settings-wht.png"></span>
-                    <span class="red-icon"><img
-                            src="https://suryaplacement.com/assets/images/Settings-red.png"></span>
+<section class="content_section"> 
+    <div class="row"> 
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h3>Calendar</h3>
+                    <div class="calendar">
+                        <div class="calendar-body">
+                            <div id="calendar-dashboard"></div>
+                        </div>
+                    </div>
                 </div>
-                <h3>Settings</h3>
-            </div>
-            <div class="dash_card">
-                <div class="admin-icon">
-                    <span class="wht-icon"><img
-                            src="https://suryaplacement.com/assets/images/Settings-wht.png"></span>
-                    <span class="red-icon"><img
-                            src="https://suryaplacement.com/assets/images/Settings-red.png"></span>
-                </div>
-                <h3>Settings</h3>
-            </div>
-            <div class="dash_card">
-                <div class="admin-icon">
-                    <span class="wht-icon"><img
-                            src="https://suryaplacement.com/assets/images/Settings-wht.png"></span>
-                    <span class="red-icon"><img
-                            src="https://suryaplacement.com/assets/images/Settings-red.png"></span>
-                </div>
-                <h3>Settings</h3>
-            </div>
-            <div class="dash_card">
-                <div class="admin-icon">
-                    <span class="wht-icon"><img
-                            src="https://suryaplacement.com/assets/images/Settings-wht.png"></span>
-                    <span class="red-icon"><img
-                            src="https://suryaplacement.com/assets/images/Settings-red.png"></span>
-                </div>
-                <h3>Settings</h3>
             </div>
         </div>
-    </div>
-</section>
-<section class="table-section">
-    <div class="container">
-        <div class="row">
-            <div class="table-outer">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Sl.No</th>
-                            <th>Date</th>
-                            <th>Name</th>
-                            <th>Place</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody> 
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-</section> --}}
+    </div> 
+</section> 
 @endsection
+
+@push('footer-script')
+<link href="https://cdn.jsdelivr.net/npm/@fullcalendar/core@6.1.9/main.min.css" rel="stylesheet">
+<script src='https://cdn.jsdelivr.net/npm/@fullcalendar/core@6.1.9/index.global.min.js'></script>
+<script src='https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid@6.1.9/index.global.min.js'></script>
+<script src="https://cdn.jsdelivr.net/npm/@fullcalendar/interaction@6.1.9/index.global.min.js"></script>
+
+<script>
+    $(function(){
+        
+   let calendar = new FullCalendar.Calendar($("#calendar-dashboard").get(0), {
+        initialView: 'dayGridMonth',
+        headerToolbar: {
+            left: 'prev',
+            center: 'title',
+            right: 'next'
+        },
+        weekends: true,
+        weekNumbers: false,
+        editable: true,
+        navLinks: false,
+        dayMaxEvents: false,
+        displayEventTime: false,
+        unselectAuto: true,
+        selectable: true, 
+        dateClick:function(info){ 
+            console.log(info)
+        // selectedDates=[`${info.date.getFullYear()}-${info.date.getMonth()+1}-${info.date.getDate()}`];
+        }, 
+        events: function(fetchInfo, successCallback, failureCallback) { 
+            $.get("{{route('dashboard')}}",fetchInfo,function(res){
+                successCallback(res);
+            },'json').fail(function(){
+                failureCallback("Network response was not ok")
+            }) 
+        }, 
+ 
+    });
+    calendar.render();
+
+})
+</script>
+@endpush
