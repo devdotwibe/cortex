ab9377632499bd11a84ccbfef208c2b9dcd8bbc8
Akhil Kannan <dev.dotwibe@gmail.com>
2024-08-05 20:28:59 +0530

    c

--- app/Http/Controllers/User/MainController.php
+++ app/Http/Controllers/User/MainController.php
@@ -98,16 +98,22 @@ class MainController extends Controller
         }
         $practiceprogress=UserReviewAnswer::whereIn('question_id',Question::where("exam_id",$practiceexam->id)->has('category')->has('subCategory')->has('setname')->select('id'))->where('user_id',$user->id)->whereIn('user_exam_review_id',UserExamReview::where('user_id',$user->id)->groupBy('exam_id')->select(DB::raw('MAX(id)')))->where('iscorrect',true)->where('user_answer',true)->count();
         $practicecnt=Question::where("exam_id",$practiceexam->id)->has('category')->has('subCategory')->has('setname')->count();
- 
-        if($learncnt>0){
-            $learnprogress=round($learnprogress/$learncnt,2);
-        }
         if($practicecnt>0){
             $practiceprogress=round($practiceprogress/$practicecnt,2);
+        } 
+        
+        $simulateprogress=UserReviewAnswer::whereIn('question_id',Question::whereIn("exam_id",Exam::whereIn("name",['full-mock-exam','topic-test'])->select('id'))->has('category')->select('id'))->where('user_id',$user->id)->whereIn('user_exam_review_id',UserExamReview::where('user_id',$user->id)->groupBy('exam_id')->select(DB::raw('MAX(id)')))->where('iscorrect',true)->where('user_answer',true)->count();
+        $simulatecnt=Question::whereIn("exam_id",Exam::whereIn("name",['full-mock-exam','topic-test'])->select('id'))->has('category')->has('subCategory')->has('setname')->count();
+        if($simulatecnt>0){
+            $simulateprogress=round($simulateprogress/$simulatecnt,2);
         }
-        if($learncnt>0){
-            $learnprogress=round($learnprogress/$learncnt,2);
-        }
+        // if($learncnt>0){
+        //     $learnprogress=round($learnprogress/$learncnt,2);
+        // }
+
+        // if($learncnt>0){
+        //     $learnprogress=round($learnprogress/$learncnt,2);
+        // }
         return view("user.dashboard",compact('chartdata','chartbackgroundColor','chartlabel','learnprogress','practiceprogress'));
     }
 
