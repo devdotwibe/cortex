9320e4c30fd3a461f0c4583e405d3b2962cdc567
Akhil Kannan <dev.dotwibe@gmail.com>
2024-08-06 15:24:55 +0530

    c

--- app/Http/Controllers/User/MainController.php
+++ app/Http/Controllers/User/MainController.php
@@ -107,7 +107,7 @@ class MainController extends Controller
         if($simulatecnt>0){
             $simulateprogress=round($simulateprogress/$simulatecnt,2);
         } 
-        print_r(UserReviewAnswer::where('user_id',$user->id)->select(DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d') ndate"))->groupBy('ndate')->pluck('ndate')->toArray());exit;
+        print_r(UserReviewAnswer::where('user_id',$user->id)->select(DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d') ndate"))->pluck('ndate')->toArray());exit;
         return view("user.dashboard",compact('chartdata','chartbackgroundColor','chartlabel','learnprogress','practiceprogress','simulateprogress'));
     }
 
