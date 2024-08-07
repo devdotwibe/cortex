05bc075c5a98c19d9ca64ecc8a15b363af5f475e
Akhil Kannan <dev.dotwibe@gmail.com>
2024-08-07 18:23:07 +0530

    c

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -269,7 +269,7 @@
          $.get('{{route("reminder.index")}}',function(res){
             if(res.reminder){
                 $('#calendar-exam-data').html(`
-                    <span>${res.reminder.title}</span><button class="btn btn-default float-end" onclick="editreminder('${res.reminder.showUrl}')">  Edit ${es.reminder.name} Date <button>
+                    <span>${res.reminder.title}</span><button class="btn btn-default float-end" onclick="editreminder('${res.reminder.showUrl}')">  Edit ${res.reminder.name} Date <button>
                 `)
             }else{
                 $('#calendar-exam-data').html(`
