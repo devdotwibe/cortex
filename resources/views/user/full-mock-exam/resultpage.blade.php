d23e815340ec2cfeabd90b89753890fa4ff2d9f0
Akhil Kannan <dev.dotwibe@gmail.com>
2024-08-08 15:40:14 +0530

    c

--- resources/views/user/full-mock-exam/resultpage.blade.php
+++ resources/views/user/full-mock-exam/resultpage.blade.php
@@ -33,7 +33,7 @@
         
                                 <p>Next Step: Review and Improve</p>
                                 <div class="exam-mark-bottom">
-                                    <a class="btn btn-warning btn-lg" id="review-link" href="{{route('full-mock-exam.preview',$review->slug)}}">Review Set</a>
+                                    <a class="btn btn-warning btn-lg" id="review-link" href="{{route('full-mock-exam.preview',$userExamReview->slug)}}">Review Set</a>
                                     <a href="{{route('full-mock-exam.index')}}" class="btn btn-outline-dark btn-lg">Exit Set</a>
                                 </div>
                             </div>
@@ -62,8 +62,8 @@
                                                 <th>Marks</th>
                                                 <td>{{$passed}}/{{$questioncount}}</td>
                                                 @foreach ($category as $item)
-                                                    @if ($review->categoryCount($item->id)>0)
-                                                    <td>{{$review->categoryMark($item->id)}}/{{$review->categoryCount($item->id)}}</td>
+                                                    @if ($userExamReview->categoryCount($item->id)>0)
+                                                    <td>{{$userExamReview->categoryMark($item->id)}}/{{$userExamReview->categoryCount($item->id)}}</td>
                                                     @else
                                                     <td></td>
                                                 @endif
@@ -78,10 +78,10 @@
                                             </tr>
                                             <tr>
                                                 <th>Average Time <br>Per Question</th>
-                                                <td>{{$review->avgTime()}}</td>                                                
+                                                <td>{{$userExamReview->avgTime()}}</td>                                                
                                                 @foreach ($category as $item)
-                                                    @if ($review->avgTime($item->id)>0)
-                                                        <td>{{$review->avgTime($item->id)}}</td>
+                                                    @if ($userExamReview->avgTime($item->id)>0)
+                                                        <td>{{$userExamReview->avgTime($item->id)}}</td>
                                                     @else
                                                         <td></td>
                                                     @endif                                                                                                   
