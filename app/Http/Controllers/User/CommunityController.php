fd43325f25d9242aafa0f68c00dd66836cc7314d
Akhil Kannan <dev.dotwibe@gmail.com>
2024-08-09 16:49:44 +0530

    c

--- app/Http/Controllers/User/CommunityController.php
+++ app/Http/Controllers/User/CommunityController.php
@@ -81,10 +81,10 @@ class CommunityController extends Controller
     public function store(Request $request){ 
         $data=$request->validate([
             'title'=>["required","max:255"],
-            'description'=>["required"],
             'type'=>["required"],
-            'option'=>["required_if:type,poll",'array','min:2'],
-            'option.*'=>["required_if:type,poll",'max:255'],
+            'description'=>["required_if:type,'post'"],
+            'option'=>["required_if:type,'poll'",'array','min:2'],
+            'option.*'=>["required_if:type,'poll'",'max:255'],
         ],[
             'option.required_if'=>"This field is required",
             'option.*.required_if'=>"This field is required",
