66e1c2d776554e31213f71e772802274cf272719
Akhil Kannan <dev.dotwibe@gmail.com>
2024-08-12 17:01:01 +0530

    v

--- app/Http/Controllers/User/CommunityController.php
+++ app/Http/Controllers/User/CommunityController.php
@@ -13,13 +13,71 @@ use Illuminate\Support\Facades\Auth;
 
 class CommunityController extends Controller
 {  
-    public function index(Request $request){
+    public function posts(Request $request){
         /**
          *  @var User
          */
         $user=Auth::user();
         if($request->ajax()){   
             $posts=Post::where('id','>',0)->orderBy('id','DESC')->paginate();
+            $results=[];
+            foreach ($posts->items() as $row) { 
+                $options=[];
+                $tvotes=$row->pollOption->sum('votes');
+                foreach($row->pollOption as $opt){
+                    $options[]=[
+                        "slug"=>$opt->slug,
+                        "option"=>$opt->option,
+                        "votes"=>$opt->votes,
+                        'percentage'=>$tvotes>0?round(($opt->votes*100)/$tvotes,2):0,
+                        'voteUrl'=>route('community.poll.vote',$opt->slug),
+                    ];
+                }
+                $vote=Poll::where('user_id',$user->id)->where('post_id',$row->id)->first();
+                if(!empty($vote)){
+                    $vote=[
+                        'slug'=>$vote->slug, 
+                        'option'=>optional($vote->pollOption)->slug,
+                    ];
+                }
+                $results[]=[
+                    "slug"=>$row->slug,
+                    "title"=>$row->title,
+                    "type"=>$row->type,
+                    "description"=>$row->description,
+                    "image"=>$row->image,
+                    "video"=>$row->video,
+                    "status"=>$row->status,
+                    "vote"=>$vote, 
+                    "poll"=>$options,
+                    "showUrl"=>route('community.post.show',$row->slug),
+                    "createdAt"=>$row->created_at->diffInMinutes(now())>1? $row->created_at->diffForHumans(now(), true)." ago":'Just Now',
+                    "user"=>[
+                        "name"=>optional($row->user)->name
+                    ],
+                     
+                ];
+            }
+            return [ 
+                'current_page' => $posts->currentPage(),
+                'total_pages' => $posts->lastPage(),
+                'total_items' => $posts->total(),
+                'items_per_page' => $posts->perPage(),
+                'data' => $results, 
+                'prev' => $posts->previousPageUrl(),
+                'next' => $posts->nextPageUrl()
+            ];
+        } 
+        return view('user.community.posts',compact('user'));
+    }
+
+    public function index(Request $request){
+        /**
+         *  @var User
+         */
+        $user=Auth::user();
+        if($request->ajax()){   
+            $posts=Post::where('id','>',0)->where('user_id',$user->id)->orderBy('id','DESC')->paginate();
             $results=[];
             foreach ($posts->items() as $row) { 
                 $options=[];
@@ -105,7 +163,7 @@ class CommunityController extends Controller
                 ]);
             }
         }
-        return redirect()->route('community.post.show',$post->slug)->with('success',"Post published");
+        return redirect()->route('community.post.index')->with('success',"Post published");
     }
     public function pollVote(Request $request,PollOption $pollOption){ 
         /**
@@ -219,7 +277,7 @@ class CommunityController extends Controller
             }
         }
         PollOption::where('post_id',$post->id)->whereNotIn('id',$ids)->delete();
-        return redirect()->route('community.post.show',$post->slug)->with('success',"Post updated");
+        return redirect()->route('community.post.index')->with('success',"Post updated");
     }
     public function destroy(Request $request,Post $post){ 
         $post->delete();
