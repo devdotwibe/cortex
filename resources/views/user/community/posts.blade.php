772b1d66b12bdc037d9ffc21792661829d2cbb27
Akhil Kannan <dev.dotwibe@gmail.com>
2024-08-12 17:59:21 +0530

    c

--- resources/views/user/community/posts.blade.php
+++ resources/views/user/community/posts.blade.php
@@ -94,7 +94,8 @@
                             ${polloption}
                         </div>
                         <div class="post-actions">
-                            <a class="post-action-btn comment-btn" href="${v.showUrl}""><img src="{{asset('assets/images/comment.svg')}}" slt="comment"> <span>1</span></a>
+                            <a class="post-action-btn comment-btn" href="${v.showUrl}""><img src="{{asset('assets/images/comment1.svg')}}" slt="comment"> <span>${v.comments}</span></a>
+                            <a class="post-action-btn like-btn" href="${v.likeUrl}""><img src="{{asset('assets/images/${v.liked?"liked":"like"}.svg')}}" slt="comment"> <span>${v.likes}</span></a>
                         </div>
                     </div>
                 `)
