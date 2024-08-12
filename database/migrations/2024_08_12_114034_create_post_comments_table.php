4a535920494fdab1f9db04b700a479f006c768af
Akhil Kannan <dev.dotwibe@gmail.com>
2024-08-12 17:54:22 +0530

    d

---
+++ database/migrations/2024_08_12_114034_create_post_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Post;
+use App\Models\PostComment;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Post::class);
+            $table->foreignIdFor(PostComment::class)->nullable();
+            $table->text('comment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_comments');
+    }
+};
