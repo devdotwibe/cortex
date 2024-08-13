b4b8d40079a43d35939a207b1a70f2e9494df600
Akhil Kannan <dev.dotwibe@gmail.com>
2024-08-13 14:08:18 +0530

    test

--- resources/views/user/community/show.blade.php
+++ resources/views/user/community/show.blade.php
@@ -83,8 +83,8 @@
                     @endif
 
                     <div class="post-actions">
-                        <a class="post-action-btn comment-btn m-2" ><img src="{{asset('assets/images/comment1.svg')}}" slt="comment"> <span>{{$post->comments()->count()}}</span></a>
                         <a class="post-action-btn like-btn m-2"  href="{{route('community.post.like',$post->slug)}}"><img @if($post->likes()->where('user_id',auth()->id())->count()>0) src="{{asset('assets/images/liked.svg')}}" @else src="{{asset('assets/images/like.svg')}}" @endif slt="comment"> <span>{{$post->likes()->count()}}</span></a>
+                        <a class="post-action-btn comment-btn m-2" ><img src="{{asset('assets/images/comment1.svg')}}" slt="comment"> <span>{{$post->comments()->count()}}</span></a>
                     </div>
                     <div class="post-comment">
                         <div class="form">
