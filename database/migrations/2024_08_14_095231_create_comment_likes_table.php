d51031f9dc37257992c34ff309bc42df095f263c
Akhil Kannan <dev.dotwibe@gmail.com>
2024-08-14 15:28:09 +0530

    te

---
+++ database/migrations/2024_08_14_095231_create_comment_likes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Post;
+use App\Models\PostComment;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comment_likes', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Post::class);
+            $table->foreignIdFor(PostComment::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comment_likes');
+    }
+};
