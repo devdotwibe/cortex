b8f660a6126e962e393ab2bbf93c13b5ccfc2d53
Akhil Kannan <dev.dotwibe@gmail.com>
2024-08-14 18:45:34 +0530

    c

--- resources/views/admin/community/index.blade.php
+++ resources/views/admin/community/index.blade.php
@@ -43,7 +43,7 @@
                     $.each(v.poll||[],function(pk,pv){ 
                         polloption+=`
                         <div class="form-check ${v.vote.option==pv.slug?"voted":"vote"}">
-                            <input class="form-check-input" type="radio" name="${v.slug}" id="poll-${v.slug}-option-${pv.slug}" value="${pv.slug}" onchange="likevote('${pv.voteUrl}','#post-item-${v.slug}')" >
+                            <input class="form-check-input" type="radio" name="${v.slug}" id="poll-${v.slug}-option-${pv.slug}" value="${pv.slug}"  >
                             <label class="form-check-label" for="poll-${v.slug}-option-${pv.slug}">
                                 ${pv.option}
                                 <span id="poll-${v.slug}-option-${pv.slug}-percentage">(${pv.percentage}%)</span>
@@ -59,7 +59,7 @@
                     $.each(v.poll||[],function(pk,pv){
                         polloption+=`
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" name="${v.slug}" id="poll-${v.slug}-option-${pv.slug}" value="${pv.slug}" onchange="likevote('${pv.voteUrl}','#post-item-${v.slug}')">
+                            <input class="form-check-input" type="radio" name="${v.slug}" id="poll-${v.slug}-option-${pv.slug}" value="${pv.slug}" >
                             <label class="form-check-label" for="poll-${v.slug}-option-${pv.slug}">
                                 ${pv.option}
                                 <span id="poll-${v.slug}-option-${pv.slug}-percentage">(${pv.percentage}%)</span>
