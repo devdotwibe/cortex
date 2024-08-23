7c8ac1e7e9d180bff226dc07c5c50721f929ee69
Akhil Kannan <dev.dotwibe@gmail.com>
2024-08-23 20:24:49 +0530

    c

--- routes/admin.php
+++ routes/admin.php
@@ -120,7 +120,7 @@ Route::name('admin.')->prefix('admin')->group(function(){
             Route::delete('/report-post/{report_post}', [PostReportController::class,'destroy'])->name('report.destroy');
             Route::get('/report-post/{report_post}', [PostReportController::class,'show'])->name('report.show');
             Route::get('/report-post/{user}/ban-user', [PostReportController::class,'banuser'])->name('report.banuser');
-            Route::get('/report-post/{user}/block-post', [PostReportController::class,'hidepost'])->name('report.hidepost');
+            Route::get('/report-post/{post}/block-post', [PostReportController::class,'hidepost'])->name('report.hidepost');
 
             Route::get('/post/{post}/comment/{post_comment}/reply', [CommunityControllerController::class, 'postCommentReplay'])->name('post.comment.reply'); 
             Route::delete('/post/{post}/comment/{post_comment}/delete', [CommunityControllerController::class, 'commentDestroy'])->name('post.comment.destroy'); 
