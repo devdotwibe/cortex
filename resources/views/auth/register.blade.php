1b692614f8ab983bc745f7026c30ffed7b2f6112
akshaydotwibe <akshaydotwibe@outlook.com>
2024-08-26 19:38:48 +0530

    ac
    ascsa

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,7 +26,6 @@
                         <div class="form-group">
                             <div class="form-data">
                                 <div class="forms-inputs mb-4">
-                                    <span>First Name</span>
                                     <input autocomplete="off" name="first_name" type="text" placeholder="Enter your first name" value="{{old('first_name')}}" class="form-control @error('first_name') is-invalid @enderror " >
                                     @error('first_name')
                                     <div class="invalid-feedback">{{$message}}</div>
@@ -34,7 +33,6 @@
                                 </div>
 
                                 <div class="forms-inputs mb-4">
-                                    <span>Last Name</span>
                                     <input autocomplete="off" name="last_name" type="text" placeholder="Enter your last name" value="{{old('last_name')}}" class="form-control @error('last_name') is-invalid @enderror " >
                                     @error('last_name')
                                     <div class="invalid-feedback">{{$message}}</div>
@@ -42,7 +40,6 @@
                                 </div>
 
                                 <div class="forms-inputs mb-4">
-                                    <span>Email</span>
                                     <input autocomplete="off" name="email" type="text" placeholder="Enter your email or username" value="{{old('email')}}" class="form-control @error('email') is-invalid @enderror " >
                                     @error('email')
                                     <div class="invalid-feedback">{{$message}}</div>
@@ -50,7 +47,6 @@
                                 </div>
 
                                 <div class="forms-inputs mb-4">
-                                    <span>Current Grade</span>
                                     <input autocomplete="off" name="schooling_year" type="text" placeholder="Enter your Current Grade" value="{{old('schooling_year')}}" class="form-control @error('schooling_year') is-invalid @enderror " >
                                     @error('schooling_year')
                                     <div class="invalid-feedback">{{$message}}</div>
@@ -58,14 +54,12 @@
                                 </div>
 
                                 <div class="forms-inputs mb-4">
-                                    <span>Password</span>
                                     <input autocomplete="off" name="password" type="password" placeholder="Enter your password" class="form-control @error('password') is-invalid @enderror " >
                                     @error('password')
                                     <div class="invalid-feedback">{{$message}}</div>
                                     @enderror
                                 </div>
                                 <div class="forms-inputs mb-4">
-                                    <span>Confirm Password</span>
                                     <input autocomplete="off" name="re_password" type="password" placeholder="Re-enter password" class="form-control @error('re_password') is-invalid @enderror " >
                                     @error('re_password')
                                     <div class="invalid-feedback">{{$message}}</div>
