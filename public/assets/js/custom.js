7534abbd24b800c607cdcf1cc16a652ca44f6720
akshaydotwibe <akshaydotwibe@outlook.com>
2024-08-28 12:51:20 +0530

    sdsd
    sdsc

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -24,18 +24,12 @@ $(document).ready(function() {
         var $this = $(this);
         var targetId = $this.data('target');
         var $targetContent = $('#' + targetId);
-
-        // Check if the clicked h3 is already active
         if ($this.hasClass('active')) {
-            // If the clicked h3 is active, remove 'active' class from it and the target content
             $this.removeClass('active');
             $targetContent.removeClass('active');
         } else {
-            // Remove 'active' class from all content elements and all h3 elements
             $('.critical-reasoning-conten').removeClass('active');
             $('.critical-reasoning-row1 h3').removeClass('active');
-            
-            // Add 'active' class to the target content and clicked h3
             $targetContent.addClass('active');
             $this.addClass('active');
         }
