61bcb3633c903497e96a7f9c6a2ca51ed32f264d
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-09-03 20:16:17 +0530

    vsdfds

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Exam;
+use App\Trait\ResourceController;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
-    //
+    
+    use ResourceController;
+
+    function __construct()
+    {
+        self::$model=Exam::class;
+        self::$routeName="admin.course";
+    } 
+
+    public function index(Request $request)
+    {
+       
+        return view("admin.pages.course");
+    }
 }
