bfc2cdda8ea9a1b2c4ffaefa3d4ceda606f1111b
Akhil Kannan <dev.dotwibe@gmail.com>
2024-09-09 16:35:41 +0530

    c

--- resources/views/components/ajax-table.blade.php
+++ resources/views/components/ajax-table.blade.php
@@ -52,15 +52,13 @@
                             @error('bulkaction')
                                 <small class="text-danger">{{ $message }}</small>
                             @enderror
-                        </div> 
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-warning">Submit</button>
+                        </div>
                     </div>
                 </form>
-            </div>
-            <div class="delete-action">
-                <button class="btn btn-danger" name="bulkaction" value="delete">
-                    Delete
-                </button>
-            </div>
+            </div> 
         </div>
     @endif
 @endif
