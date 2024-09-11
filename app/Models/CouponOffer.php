b2829567daf616e57495cba814660cc8452427a1
Akhil Kannan <dev.dotwibe@gmail.com>
2024-09-11 17:19:59 +0530

    c

--- app/Models/CouponOffer.php
+++ app/Models/CouponOffer.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use App\Trait\ResourceModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CouponOffer extends Model
 {
-    use HasFactory;
+    use HasFactory,ResourceModel;
+    protected $fillable = ['slug','name','amount','expire'];
 }
