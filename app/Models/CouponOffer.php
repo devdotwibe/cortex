347b64c5635ec9b10b31d79c299890ba6cb310f8
Akhil Kannan <dev.dotwibe@gmail.com>
2024-09-11 17:37:34 +0530

    c

--- app/Models/CouponOffer.php
+++ app/Models/CouponOffer.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\VisibleStatus;
 use App\Trait\ResourceModel;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+#[ScopedBy([VisibleStatus::class])]
 class CouponOffer extends Model
 {
     use HasFactory,ResourceModel;
-    protected $fillable = ['slug','name','amount','expire'];
+    protected $fillable = ['slug','name','amount','expire','visible_status'];
 }
