c22953a78ab4aef7b84d83422825ee491726da7d
Akhil Kannan <dev.dotwibe@gmail.com>
2024-09-11 15:32:13 +0530

    c

--- resources/views/admin/coupon/index.blade.php
+++ resources/views/admin/coupon/index.blade.php
@@ -1,10 +1,22 @@
 @extends('layouts.admin')
-@section('title', 'Payment')
+@section('title', 'Coupon')
 @section('content')
     <section class="header_nav">
         <div class="header_wrapp">
             <div class="header_title">
-                <h2>Payment</h2>
+                <h2>Coupon</h2>
+            </div>
+        </div>
+    </section>
+    <section class="invite-wrap mt-2"> 
+        <div class="coupon-wrap">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card"> 
+                        <div class="card-body">
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
