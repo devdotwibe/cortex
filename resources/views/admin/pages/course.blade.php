97bef29e435af6e890708417ea8feeef42f9d4ec
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-09-12 13:09:19 +0530

    fdgfd

--- resources/views/admin/pages/course.blade.php
+++ resources/views/admin/pages/course.blade.php
@@ -217,7 +217,7 @@
                         @foreach($tabs1 as $k => $tab)
                             <li class="nav-item" role="presentation" data-tab-id="{{ $tab['tab_id_1'] }}">
 
-                                <a class="nav-link @if($k ==0 &&  old('sub_section')=="" old('sub_section') == $tab['tab_id_1'] || ($tab['tab_id_1'] == 'tab1' && old('sub_section')=="" ) ) active @elseif( old('sub_section') == $tab['tab_id_1'] || ($tab['tab_id_1'] == 'tab1' ) ) active @endif" 
+                                <a class="nav-link @if($k ==0 &&  old('sub_section')=="" || old('sub_section') == $tab['tab_id_1'] || ($tab['tab_id_1'] == 'tab1' && old('sub_section')=="" ) ) active @elseif( old('sub_section') == $tab['tab_id_1'] || ($tab['tab_id_1'] == 'tab1' ) ) active @endif" 
                                    id="{{ $tab['tab_id_1'] }}-tab" 
                                    data-bs-toggle="tab" 
                                    href="#{{ $tab['tab_id_1'] }}" 
