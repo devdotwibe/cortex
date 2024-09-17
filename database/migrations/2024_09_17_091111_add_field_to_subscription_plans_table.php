0a64a9bbf353764fbf85bf49ef010b406450e6b0
Akhil Kannan <dev.dotwibe@gmail.com>
2024-09-17 14:59:49 +0530

    tt

---
+++ database/migrations/2024_09_17_091111_add_field_to_subscription_plans_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('subscription_plans', function (Blueprint $table) {
+            $table->boolean('is_external')->default(false);
+            $table->string('external_link')->nullable();
+            $table->string('external_label')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('subscription_plans', function (Blueprint $table) {
+            $table->dropColumn(['is_external','external_link','external_label']);
+        });
+    }
+};
