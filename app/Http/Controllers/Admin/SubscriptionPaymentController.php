90b95d2e772410920afecfe43b4cf7bf33f40a74
Akhil Kannan <dev.dotwibe@gmail.com>
2024-09-17 15:31:24 +0530

    c

--- app/Http/Controllers/Admin/SubscriptionPaymentController.php
+++ app/Http/Controllers/Admin/SubscriptionPaymentController.php
@@ -19,54 +19,76 @@ class SubscriptionPaymentController extends Controller
         $plans=SubscriptionPlan::where('id','>',0)->get();
         return view('admin.payment-price.index',compact('plans','price'));
     }
-    public function store(Request $request){
+    public function store(Request $request){ 
+        $field='payment';
         $request->validate([
-            "payment"=>['required'],
-            "payment.basic_amount"=>['required','numeric','min:1','max:100000'],
-            "payment.combo_amount"=>['required','numeric','min:1','max:100000'],
-            'payment.title'=>['required'],
-            'payment.content'=>['nullable'],
-            'payment.icon'=>['nullable']
+            "$field"=>['required'],
+            "$field.basic_amount"=>['required','numeric','min:1','max:100000'],
+            "$field.combo_amount"=>['required','numeric','min:1','max:100000'],
+            "$field.title"=>['required'],
+            "$field.content"=>['nullable'],
+            "$field.icon"=>['nullable']
         ],[
-            "payment.required"=>"The field is required",
-            "payment.basic_amount.required"=>"This basic amount field is required",
-            "payment.combo_amount.required"=>"This combo amount field is required",
-            "payment.title.required"=>"This basic title is required",
-            "payment.content.required"=>"This content field is required",
-            "payment.icon.required"=>"This icon field is required",
+            "$field.required"=>"The field is required",
+            "$field.basic_amount.required"=>"This basic amount field is required",
+            "$field.combo_amount.required"=>"This combo amount field is required",
+            "$field.title.required"=>"This basic title is required",
+            "$field.content.required"=>"This content field is required",
+            "$field.icon.required"=>"This icon field is required",
         ]);
-        $basic_amount=$request->payment["basic_amount"];
-        $combo_amount=$request->payment["combo_amount"];
-        $title=$request->payment["title"];
-        $content=$request->payment["content"];
-        $icon=$request->payment["icon"];
-        $price1=Payment::stripe()->prices->create([
-            'currency' => config('stripe.currency'),
-            'unit_amount' => intval($basic_amount*100),
-            'product_data' => ['name' => config('app.name','Cortex').' Amount :'.(intval($basic_amount*100)/100).' For '.ucfirst($title)],
-            'metadata'=>[
-                'modify_time'=>date('Y-m-d h:i a'),
-                'title'=>$title, 
-            ]
-        ]); 
-        $price2=Payment::stripe()->prices->create([
-            'currency' => config('stripe.currency'),
-            'unit_amount' => intval($combo_amount*100),
-            'product_data' => ['name' => config('app.name','Cortex').' Amount :'.(intval($combo_amount*100)/100).' For '.ucfirst($title)],
-            'metadata'=>[
-                'modify_time'=>date('Y-m-d h:i a'),
-                'title'=>$title, 
-            ]
-        ]); 
+        $title=$request->$field["title"];
+        $content=$request->$field["content"];
+        $icon=$request->$field["icon"];
+        $basic_amount=null;
+        $combo_amount=null;
+        $external_label=null;
+        $external_link=null;
+        $basic_amount_id=null;
+        $combo_amount_id=null;
+
+        if($request->$field['is_external']){
+            $basic_amount=$request->$field["basic_amount"];
+            $combo_amount=$request->$field["combo_amount"];
+                
+            $price1=Payment::stripe()->prices->create([
+                'currency' => config('stripe.currency'),
+                'unit_amount' => intval($basic_amount*100),
+                'product_data' => ['name' => config('app.name','Cortex').' Amount :'.(intval($basic_amount*100)/100).' For '.ucfirst($title)],
+                'metadata'=>[
+                    'modify_time'=>date('Y-m-d h:i a'),
+                    'title'=>$title,
+                    "old_key"=>$subscriptionPlan->basic_amount_id??"-", 
+                ]
+            ]); 
+            $basic_amount_id=$price1->id; 
+            $price2=Payment::stripe()->prices->create([
+                'currency' => config('stripe.currency'),
+                'unit_amount' => intval($combo_amount*100),
+                'product_data' => ['name' => config('app.name','Cortex').' Amount :'.(intval($combo_amount*100)/100).' For '.ucfirst($title)],
+                'metadata'=>[
+                    'modify_time'=>date('Y-m-d h:i a'),
+                    'title'=>$title, 
+                    "old_key"=>$subscriptionPlan->combo_amount_id??"-",
+                ]
+            ]);  
+            $combo_amount_id=$price2->id;
+        }else{
+            $external_label=$request->$field["external_label"];
+            $external_link=$request->$field["external_link"];
+        }
+
         SubscriptionPlan::store([
             "name"=>Str::slug($title),
             "title"=>$title,
             "content"=>$content,
-            'basic_amount'=>$price1->unit_amount/100,
-            'basic_amount_id'=>$price1->id,
-            'combo_amount'=>$price2->unit_amount/100,
-            'combo_amount_id'=>$price2->id,
+            'basic_amount'=>$basic_amount,
+            'basic_amount_id'=>$basic_amount_id,
+            'combo_amount'=>$combo_amount,
+            'combo_amount_id'=>$combo_amount_id,
             'icon'=>$icon,
+            'external_link'=>$external_link,
+            'external_label'=>$external_label,
+            'is_external'=>false,
         ]);
 
         if($request->ajax()){
