2123d6a8cf2f451bac07e34fa4d9bcd575d11946
nikhil <nikhildotwibe@outlook.com>
2024-09-18 12:18:03 +0530

    tetfr

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,1 +1,28 @@
-<h1>Terms and conditions<h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terms and conditions</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <header>
+        <h1>Terms and conditions</h1>
+    </header>
+    
+    <main>
+        <section>
+            @if(!empty($TermsAndConditions->description))
+
+            {!! $TermsAndConditions->description !!}
+            @endif
+           
+        </section>
+
+       
+    </main>
+
+   
+</body>
+</html>
