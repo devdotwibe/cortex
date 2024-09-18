2589a588bc2405dc1f5c6675607ec916436a7800
Akhil Kannan <dev.dotwibe@gmail.com>
2024-09-18 17:58:14 +0530

    c

--- app/Http/Controllers/User/PrivateClassHomeWorkController.php
+++ app/Http/Controllers/User/PrivateClassHomeWorkController.php
@@ -25,6 +25,7 @@ class PrivateClassHomeWorkController extends Controller
          *  @var User
          */
         $user=Auth::user();
+        print_r(TermAccess::where('type','home-work')->where('user_id',$user->id)->get());
         $homeWorks=HomeWork::whereIn('id',TermAccess::where('type','home-work')->where('user_id',$user->id)->select('term_id'))->get();
         return view('user.home-work.index',compact('homeWorks','user'));
     }
