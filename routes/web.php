9e8e4a9867fda8f79df6d1f0031e4540acd540d8
Akhil Kannan <dev.dotwibe@gmail.com>
2024-09-18 13:07:31 +0530

    c

--- routes/web.php
+++ routes/web.php
@@ -207,12 +207,6 @@ Route::middleware(['auth', 'isUser'])->group(function () {
             Route::prefix('analytics')->name('analytics.')->group(function () {
                 Route::get('/', [AnalyticsController::class, 'index'])->name('index');
             });
-            Route::prefix('tipsandadvice')->name('tipsandadvise.')->group(function () {
-                Route::get('/', [TipsAndAdviceController::class, 'index'])->name('index');
-                Route::resource('/post',TipsAndAdviceController ::class);
-                Route::get('/tips-show/{id}', [TipsAndAdviceController::class, 'tip_show'])->name('tip_show');
-    
-            });
 
         });
 
@@ -228,6 +222,12 @@ Route::middleware(['auth', 'isUser'])->group(function () {
         });
 
 
+        Route::prefix('tipsandadvice')->name('tipsandadvise.')->group(function () {
+            Route::get('/', [TipsAndAdviceController::class, 'index'])->name('index');
+            Route::resource('/post',TipsAndAdviceController ::class);
+            Route::get('/tips-show/{id}', [TipsAndAdviceController::class, 'tip_show'])->name('tip_show');
+
+        });
 
     });
 
