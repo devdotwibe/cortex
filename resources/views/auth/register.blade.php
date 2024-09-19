ef40973938435215f63b32e248f11b0fe0086ce4
Akhil Kannan <dev.dotwibe@gmail.com>
2024-09-19 16:50:41 +0530

    c

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -124,20 +124,20 @@
 	        // agree: "Please accept our policy"
 	    },
 	    // errorElement: "div",
-	    errorPlacement: function ( error, element ) { 
-	        error.addClass( "invalid-feedback" );
-	        if ( element.prop( "type" ) === "checkbox" ) {
-	            error.insertAfter(element.next( "label" ));
-	        } else {
-	            error.insertAfter(element.next(".pmd-textfield-focused"));
-	        }
-	    },
-	    highlight: function ( element, errorClass, validClass ) {
-	        $( element ).addClass( "is-invalid" ).removeClass( "is-valid" );
-	    },
-	    unhighlight: function (element, errorClass, validClass) {
-	        $( element ).addClass( "is-valid" ).removeClass( "is-invalid" );
-	    }
+	    // errorPlacement: function ( error, element ) { 
+	    //     error.addClass( "invalid-feedback" );
+	    //     if ( element.prop( "type" ) === "checkbox" ) {
+	    //         error.insertAfter(element.next( "label" ));
+	    //     } else {
+	    //         error.insertAfter(element.next(".pmd-textfield-focused"));
+	    //     }
+	    // },
+	    // highlight: function ( element, errorClass, validClass ) {
+	    //     $( element ).addClass( "is-invalid" ).removeClass( "is-valid" );
+	    // },
+	    // unhighlight: function (element, errorClass, validClass) {
+	    //     $( element ).addClass( "is-valid" ).removeClass( "is-invalid" );
+	    // }
 	});
 </script>
 @endpush
