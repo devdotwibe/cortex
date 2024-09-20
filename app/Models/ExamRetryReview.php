6bdbd4f4f449d4febb5aa7edaa7aae77199ebf93
Akhil Kannan <dev.dotwibe@gmail.com>
2024-09-20 19:02:37 +0530

    ccc

--- app/Models/ExamRetryReview.php
+++ app/Models/ExamRetryReview.php
@@ -28,4 +28,19 @@ class ExamRetryReview extends Model
         'sub_category_set',
         'user_exam_review_id', 
     ];
+
+
+    public function categoryMark($id){ 
+        return ExamRetryAnswer::where('exam_retry_review_id',$this->id)->where('exam_id',$this->exam_id)->whereIn('question_id',Question::where("category_id",$id)->where('exam_id',$this->exam_id)->select('id'))->where('iscorrect',true)->where('user_answer',true)->count();
+    }
+    public function categoryCount($id){ 
+        return ExamRetryQuestion::where('exam_retry_review_id',$this->id)->where('exam_id',$this->exam_id)->whereIn('question_id',Question::where("category_id",$id)->where('exam_id',$this->exam_id)->select('id'))->count();
+    }
+    public function avgTime($id=null){ 
+        $qst=ExamRetryQuestion::where('exam_retry_review_id',$this->id)->where('exam_id',$this->exam_id);
+        if(!empty($id)){
+            $qst->whereIn('question_id',Question::where("category_id",$id)->where('exam_id',$this->exam_id)->select('id'));
+        }
+        return round($qst->whereNotNull('time_taken')->where('time_taken','>',0)->average('time_taken'),2);
+    }
 }
