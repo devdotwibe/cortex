0e96767cd9a8ce5fa11a0f482fd69c66b84b5515
Akhil Kannan <dev.dotwibe@gmail.com>
2024-09-23 15:51:30 +0530

    c

--- resources/views/admin/learn/edit.blade.php
+++ resources/views/admin/learn/edit.blade.php
@@ -8,6 +8,15 @@
         </div> 
     </div>
 </section>
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 <section class="invite-wrap mt-2">
     <div class="container">
        
