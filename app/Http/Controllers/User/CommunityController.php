0af8048b43303ba42be64199e2eb32b6548751f7
nikhil <nikhildotwibe@outlook.com>
2024-09-24 11:16:46 +0530

    fff

--- app/Http/Controllers/User/CommunityController.php
+++ app/Http/Controllers/User/CommunityController.php
@@ -88,7 +88,7 @@ class CommunityController extends Controller
         } 
         // return view('user.community.posts', compact('user','hashtags'));
         $user = Auth::user();
-        return view('user.community.index',compact('hashtags'));
+        return view('user.community.index',compact('hashtags','user'));
     }
 
     public function index(Request $request)
