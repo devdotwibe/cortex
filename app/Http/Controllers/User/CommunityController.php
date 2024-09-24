ca896d25522efa8783324a15f97d980ae236bb79
nikhil <nikhildotwibe@outlook.com>
2024-09-24 12:32:24 +0530

    dfh

--- app/Http/Controllers/User/CommunityController.php
+++ app/Http/Controllers/User/CommunityController.php
@@ -37,14 +37,10 @@ class CommunityController extends Controller
 
         if ($request->ajax()) {
             $post = Post::where('id', '>', 0);
-            // if (!empty($hashtag)) {
-            //     $post->whereIn('id', Hashtag::where('hashtag', 'like', "%$hashtag%")->select('post_id'));
-            // }
-            // $posts = $post->orderBy('id', 'DESC')->paginate();
             if (!empty($hashtag)) {
                 $post->whereIn('id', Hashtag::where('hashtag', 'like', "%$hashtag%")->select('post_id'));
             }
-            $posts = $post->where('user_id', $user->id)->orderBy('id', 'DESC')->paginate();
+            $posts = $post->orderBy('id', 'DESC')->paginate();
             $results = [];
             foreach ($posts->items() as $row) {
                 $options = [];
