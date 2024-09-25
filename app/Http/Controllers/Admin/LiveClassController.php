89385718e94314082294e7e83e04b63de6162138
nikhil <nikhildotwibe@outlook.com>
2024-09-25 15:08:15 +0530

    ee

--- app/Http/Controllers/Admin/LiveClassController.php
+++ app/Http/Controllers/Admin/LiveClassController.php
@@ -243,7 +243,7 @@ class LiveClassController extends Controller
             }
         }
 
-        $allTerms = $terms1->concat($terms2)->concat($terms3)->concat($terms4);
+        $terms[] = $terms1->concat($terms2)->concat($terms3)->concat($terms4);
 
         return view('admin.live-class.private-class-request',compact('live_class','terms'));
 
