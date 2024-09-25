9d8ba8792405676c32702c501831edbc2240cfdf
nikhil <nikhildotwibe@outlook.com>
2024-09-25 11:35:50 +0530

    fff

--- app/Http/Controllers/User/PrivateClassHomeWorkController.php
+++ app/Http/Controllers/User/PrivateClassHomeWorkController.php
@@ -29,6 +29,7 @@ class PrivateClassHomeWorkController extends Controller
         return view('user.home-work.index',compact('homeWorks','user'));
     }
     public function show(Request $request,HomeWork $homeWork){
+        print('dd');
         /**
          *  @var User
          */
