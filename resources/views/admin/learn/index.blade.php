8e9852025854a2782d161cfec1353a4312479753
Akhil Kannan <dev.dotwibe@gmail.com>
2024-09-25 12:25:15 +0530

    c

--- resources/views/admin/learn/index.blade.php
+++ resources/views/admin/learn/index.blade.php
@@ -19,10 +19,10 @@
                         <div class="card-body">
                             <div class="category">
                                 <div class="category-image">
-                                    <img src="{{asset("assets/images/User-red.png")}}">
+                                    <img src="{{$exam->examIcon($item->id,asset("assets/images/User-red.png"))}}">
                                 </div>
                                 <div class="category-content">
-                                    <h5><span id="category-content-subtitle-{{$item->id}}"> {{$exam->subtitle($item->id,"Module ".($item->getIdx()+1))}} </span> <i id="category-content-subtitle-edit-{{$item->id}}" onclick="editsubtitle(event,this)" data-title="{{$item->name}}" data-subtitle="{{$exam->subtitle($item->id,"Module ".($item->getIdx()+1))}}" data-category="{{$item->id}}"><img src="{{asset('assets/images/pen.png')}}" width="15" alt=""> </i></h5>
+                                    <h5><span id="category-content-subtitle-{{$item->id}}"> {{$exam->subtitle($item->id,"Module ".($item->getIdx()+1))}} </span> <i id="category-content-subtitle-edit-{{$item->id}}" onclick="editsubtitle(event,this)" data-title="{{$item->name}}" data-icon="{{$exam->examIcon($item->id,asset("assets/images/User-red.png"))}}" data-subtitle="{{$exam->subtitle($item->id,"Module ".($item->getIdx()+1))}}" data-category="{{$item->id}}"><img src="{{asset('assets/images/pen.png')}}" width="15" alt=""> </i></h5>
                                     <h3>{{$item->name}}</h3>
                                 </div>
                             </div>
