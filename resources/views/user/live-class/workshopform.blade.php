67551e12f3f8a4b23214178eeb475fd625ac53be
Akhil Kannan <dev.dotwibe@gmail.com>
2024-09-26 17:53:30 +0530

    te

--- resources/views/user/live-class/workshopform.blade.php
+++ resources/views/user/live-class/workshopform.blade.php
@@ -12,6 +12,11 @@
     <div class="workshop-wrap">
         <div class="row">
             <div class="col-md-12">
+                <p><strong>Note:</strong> Please Make Sure Your Google Account is Logged on this Browser to Get the Access of this Form.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
                 <div class="workshop-content">  
                     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfyk2g0VkuQshszXyye_C6GYHhsEhZWoEL8cRbs1Hv0ZPNvFw/viewform?embedded=true" width="100%" height="1084" frameborder="0" marginheight="0" marginwidth="0" allowfullscreen referrerpolicy="same-origin" allow="popups; cookies; paymentrequest; autoplay;">Loading…</iframe>
                 </div> 
