6dcfeb35d4c64a6257c5dc3ecde7345279732f44
nikhil <nikhildotwibe@outlook.com>
2024-09-26 12:46:46 +0530

    ete

--- resources/views/user/learn/show.blade.php
+++ resources/views/user/learn/show.blade.php
@@ -6,7 +6,7 @@
     <div class="header_wrapp">
         <div class="header_title"> 
             <div class="back-btn" id="back-btn" style="display: block"> <!-- Ensure proper display value -->
-                <a href="{{ route('web.learn.index') }}">
+                <a href="{{ route('learn.index') }}">
                     <img src="{{ asset('assets/images/leftarrowblack.svg') }}" alt="">
                 </a>
             </div>
