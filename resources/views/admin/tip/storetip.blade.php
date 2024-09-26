93f62746a991315231f3ac7181561c98768fd27b
nikhil <nikhildotwibe@outlook.com>
2024-09-26 12:13:40 +0530

    rttry

--- resources/views/admin/tip/storetip.blade.php
+++ resources/views/admin/tip/storetip.blade.php
@@ -6,7 +6,7 @@
     <div class="header_wrapp">
         <div class="header_title">
             <div class="back-btn" id="back-btn" style="display: block"> <!-- Ensure proper display value -->
-                <a href="{{ route('admin.{tip}/create') }}">
+                <a href="{{ route('admin.{tip}.create') }}">
                     <img src="{{ asset('assets/images/leftarrowblack.svg') }}" alt="">
                 </a>
             </div>
