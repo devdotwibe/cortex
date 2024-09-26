3e0f03874d7582e8f0d815c571971c0c1eebd0c7
nikhil <nikhildotwibe@outlook.com>
2024-09-26 15:45:00 +0530

    d

--- resources/views/user/live-class/lesson.blade.php
+++ resources/views/user/live-class/lesson.blade.php
@@ -4,6 +4,12 @@
 <section class="header_nav">
     <div class="header_wrapp">
         <div class="header_title">
+            <div class="back-btn" id="back-btn" style="display: block"> <!-- Ensure proper display value -->
+                <a href="{{ route('live-class.privateclass.lesson',$user->slug) }}">
+                  
+                    <img src="{{ asset('assets/images/exiticon.svg') }}" alt="">
+                </a>
+            </div>
             <h2>Term {{$lessonMaterial->getIdx()+1}} :Lesson Details </h2>
         </div>
     </div>
