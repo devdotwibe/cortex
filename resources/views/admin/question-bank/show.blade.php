9711c8e0dac58c255488482492acae5c208e3f87
nikhil <nikhildotwibe@outlook.com>
2024-09-26 18:19:28 +0530

    ds

--- resources/views/admin/question-bank/show.blade.php
+++ resources/views/admin/question-bank/show.blade.php
@@ -5,14 +5,14 @@
     <div class="header_wrapp">
         <div class="header_title">
 
-           
+            <h2>
                 <a onclick="pagetoggle()">{{$category->name}}<img src="{{asset('assets/images/leftarrowblack.svg')}}" alt=""></a>
 
                 <a onclick="pagetoggle()"> {{$subcategory->name}}<img src="{{asset('assets/images/leftarrowblack.svg')}}" alt=""></a>
 
                 <a onclick="pagetoggle()">{{ $setname->name }}<img src="{{asset('assets/images/leftarrowblack.svg')}}" alt=""></a>
           
-
+                Questions</h2>
             {{-- <h2>{{$category->name}} -> {{$subcategory->name}} -> {{ $setname->name }} -> Questions</h2> --}}
 
            
