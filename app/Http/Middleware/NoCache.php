6bdd0e19cffda3f540bb8b0257251343317e785b
Akhil Kannan <dev.dotwibe@gmail.com>
2024-09-26 20:13:21 +0530

    c

--- app/Http/Middleware/NoCache.php
+++ app/Http/Middleware/NoCache.php
@@ -14,15 +14,13 @@ class NoCache
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response
-    {  
-        
+    {          
         $response = $next($request);
         if ($response instanceof Response) {
             $response->headers->set('Cache-Control', 'no-cache, no-store, must-revalidate');
             $response->headers->set('Pragma', 'no-cache');
             $response->headers->set('Expires', '0');
         }
-
         return $response;
     }
 }
