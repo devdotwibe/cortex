3fd67363dc1c467f7a5040766ced48ac944ede6d
nikhil <nikhildotwibe@outlook.com>
2024-09-27 15:58:10 +0530

    f

--- resources/views/admin/report-post/show.blade.php
+++ resources/views/admin/report-post/show.blade.php
@@ -12,8 +12,9 @@
         </div> 
         <div class="header_right">
             <ul class="nav_bar"> 
-                 
-                @if($postUser->post_status=="active")
+                
+                  @if(optional($postUser)->post_status == "active")
+             
                 <li class="nav_item"><a href="{{route('admin.community.report.banuser',$postUser->slug)}}" class=" btn btn-danger">Ban User</a></li> 
                 @endif
                 <li class="nav_item"><a href="{{route('admin.community.post.edit',$post->slug)}}" class="nav_link btn">Edit Post</a></li>       
