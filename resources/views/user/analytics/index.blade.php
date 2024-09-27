526da87bcef481cecd7dfd40378f030696d5e7ff
nikhil <nikhildotwibe@outlook.com>
2024-09-27 14:55:53 +0530

    sad

--- resources/views/user/analytics/index.blade.php
+++ resources/views/user/analytics/index.blade.php
@@ -109,12 +109,16 @@
 @push('footer-script')  
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
-        function togglegrapg(v){
-            $(`.analytic-item.active,#${v}`).slideToggle().toggleClass('active');
-            if(v=="mock-exam-result"){
-                loadexamgrapg("{{url()->current()}}")
+       function togglegraph(v) {
+            $('.analytic-item.active').slideToggle().removeClass('active');
+            $(`#${v}`).slideToggle().addClass('active');
+
+            if (v === "mock-exam-result") {
+                $("#mock-exam-dropdown").show(); // Show dropdown for mock exams
+                loadexamgraph(""); // Clear previously loaded exam data
+            } else {
+                $("#mock-exam-dropdown").hide(); // Hide dropdown for other options
             }
-            
         }
         function generateRandomId(length) {
             const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
