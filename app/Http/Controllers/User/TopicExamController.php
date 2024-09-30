8c44a2e75acdaf0408bfc8c039cacaa84d4eb239
Akhil Kannan <dev.dotwibe@gmail.com>
2024-09-30 16:16:20 +0530

    c

--- app/Http/Controllers/User/TopicExamController.php
+++ app/Http/Controllers/User/TopicExamController.php
@@ -104,8 +104,7 @@ class TopicExamController extends Controller
              */
             $user = Auth::user();
             $exam = Exam::find($userExam->exam_id);
-            $category =Category::find($userExam->category_id);
-            return [$userExam,session("topic-test-attempt"),UserExam::findSlug(session("topic-test-attempt"))];
+            $category =Category::find($userExam->category_id); 
             $questions=Question::with('answers')->where('exam_id', $exam->id)->where('category_id', $category->id)->paginate(50);
             foreach ($questions as $question) {
                 $userQuestion=UserExamQuestion::store([
