46d921df239ec2ca31853328465fab1f1e7eea22
Akhil Kannan <dev.dotwibe@gmail.com>
2024-09-30 16:03:45 +0530

    c

--- database/migrations/2024_09_30_102252_add_field_to_user_exam_reviews.php
+++ database/migrations/2024_09_30_102252_add_field_to_user_exam_reviews.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('user_exam_reviews', function (Blueprint $table) {
-            //
+            $table->dropColumn(['ticket']);
         });
     }
 };
