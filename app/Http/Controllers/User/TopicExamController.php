794c5c8e788fe71f006541e5f0622bac774bc416
Akhil Kannan <dev.dotwibe@gmail.com>
2024-09-30 15:07:57 +0530

    c

--- app/Http/Controllers/User/TopicExamController.php
+++ app/Http/Controllers/User/TopicExamController.php
@@ -228,7 +228,7 @@ class TopicExamController extends Controller
                 $user->setProgress('exam-' . $exam->id . '-topic-' . $category->id . '-complete-date', date('Y-m-d H:i:s'));
             }
             $user->setProgress("exam-" . $exam->id . "-topic-" . $category->id . "-complete-review", 'yes');
-            dispatch(new SubmitReview($review));
+            dispatch(new SubmitReview($review,$attemt));
             Session::remove("topic-test-attempt");
             if ($questioncnt > $passed) {
                 $key = md5("exam-retry-" . $review->id);
