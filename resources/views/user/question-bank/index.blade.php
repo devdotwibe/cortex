6efb726fc5c079b730fc38559838c881c444c6c3
nikhil <nikhildotwibe@outlook.com>
2024-10-01 11:56:26 +0530

    gfj

--- resources/views/user/question-bank/index.blade.php
+++ resources/views/user/question-bank/index.blade.php
@@ -13,11 +13,12 @@
         <div class="row">
             @foreach ($categorys as $k=> $item)
             <div class="col-md-6">
-                @if ($user->is_free_access||(optional($user->subscription())->status??"")=="subscribed"||$k == 0) 
-                <a href="{{route('question-bank.show',$item->slug)}}">
-                @else
-                <a href="{{route('pricing.index')}}">
-                @endif
+                <a href="javascript:void(0);" 
+                    @if ($user->is_free_access || (optional($user->subscription())->status ?? "") == "subscribed" || $k == 0)
+                        onclick="window.location.href='{{ route('question-bank.show', $item->slug) }}'"
+                    @else
+                        onclick="showSubscriptionNotification()"
+                    @endif>
                     <div class="card">
                         <div class="card-body">
                             <div class="category">
@@ -43,9 +44,33 @@
     </div>
 </section> 
 @endsection
+@push('modals') 
 
+<!-- Notification Modal -->
+<div class="modal fade" id="subscriptionModal" tabindex="-1" aria-labelledby="subscriptionModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="subscriptionModalLabel">Subscription Required</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Candidate Not Subscriber Plan
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endpush
 @push('footer-script')
-    <script>
-         
-    </script>
+<script>
+    function showSubscriptionNotification() {
+        $('#subscriptionModal').modal('show'); // Show the modal using Bootstrap
+    }
+</script>
+
+    
 @endpush
