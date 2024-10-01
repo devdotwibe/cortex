a82dc8942441ee8af08492470f5e7c62b79f2e79
Akhil Kannan <dev.dotwibe@gmail.com>
2024-10-01 16:30:41 +0530

    c

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-// Schedule::job(new ExpireSubscription)->yearlyOn(5, 31, '00:15');
+Schedule::job(new ExpireSubscription)->yearlyOn(5, 31, '00:15');
