6fb76149f0d4f6adfdc51cd7277b7e7de33cce2c
nikhil <nikhildotwibe@outlook.com>
2024-10-02 14:52:30 +0530

    w

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -42,7 +42,7 @@
 
 
         
-<div class="modal fade" id="import_user_modal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+<div class="header_right" id="import_user_modal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content" >
             <div class="modal-header">
