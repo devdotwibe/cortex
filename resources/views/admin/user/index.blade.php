6cb9f47f5707feb511891789a753b7d50f7486df
nikhil <nikhildotwibe@outlook.com>
2024-10-02 15:49:24 +0530

    gfg

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -52,7 +52,12 @@
                             </form> --}}
                         </div>
 
-                        
+                        <div class="text-fields">
+                            <label>Name : </label>
+                            <select class="form-control import-fields" name="name" id="name" data-value="name" >
+                                <option value="">--Select--</option>
+                            </select>
+                         </div>    
 
                    <div class="text-fields">
                    <label>First Name : </label>
