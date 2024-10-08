3f194ffceb396c189dcc980f5ae6d3e0500c5963
nikhil <nikhildotwibe@outlook.com>
2024-10-08 12:10:35 +0530

    ty

--- resources/views/user/learn/index.blade.php
+++ resources/views/user/learn/index.blade.php
@@ -4,7 +4,7 @@
 <section class="header_nav">
     <div class="header_wrapp">
         <div class="header_title">
-            <h2>Learn</h2>
+            <h2>Learn1</h2>
         </div>
     </div>
 </section>
