4b23adb604906f878eac8dc7b26129d9861b66ab
nikhil <nikhildotwibe@outlook.com>
2024-10-08 14:09:04 +0530

    dd

--- routes/web.php
+++ routes/web.php
@@ -175,6 +175,7 @@ Route::middleware(['auth', 'isUser'])->group(function () {
         Route::prefix('live-class')->name('live-class.')->group(function () {
             Route::get('/', [LiveClassController::class, 'index'])->name('index');
             Route::get('/{live}', [LiveClassController::class, 'show'])->name('show');
+            Route::get('/{live}/workshop', [LiveClassController::class, 'workshop'])->name('workshop');
         });
 
         Route::middleware('subscription')->group(function () {
@@ -182,7 +183,7 @@ Route::middleware(['auth', 'isUser'])->group(function () {
             Route::prefix('live-class')->name('live-class.')->group(function () {
                 // Route::get('/', [LiveClassController::class, 'index'])->name('index');
                 // Route::get('/{live}', [LiveClassController::class, 'show'])->name('show');
-                Route::get('/{live}/workshop', [LiveClassController::class, 'workshop'])->name('workshop');
+                // Route::get('/{live}/workshop', [LiveClassController::class, 'workshop'])->name('workshop');
                 Route::get('/{live}/workshop/form', [LiveClassController::class, 'workshopform'])->name('workshop.form');
                 Route::get('/{live}/private-class', [LiveClassController::class, 'privateclass'])->name('privateclass');
 
