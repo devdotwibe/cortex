a4f7b30fcc64263218073fe51cf5481398933891
nikhil <nikhildotwibe@outlook.com>
2024-10-08 10:49:33 +0530

    ddt

--- resources/views/admin/full-mock-exam/index.blade.php
+++ resources/views/admin/full-mock-exam/index.blade.php
@@ -4,6 +4,11 @@
 <section class="header_nav">
     <div class="header_wrapp">
         <div class="header_title">
+            <div class="back-btn" id="back-btn" style="display: block"> <!-- Ensure proper display value -->
+                <a href="{{ route('admin.exam') }}">
+                    <img src="{{ asset('assets/images/leftarrowblack.svg') }}" alt="">
+                </a>
+            </div>
             <h2>{{$exam->title}} - Questions</h2>
         </div>
         <div class="header_content">
