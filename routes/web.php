df86ae0fad95e878a1c23bcb44dd9387eb89dc10
nikhil <nikhildotwibe@outlook.com>
2024-10-08 14:08:23 +0530

    gg

--- routes/web.php
+++ routes/web.php
@@ -174,13 +174,14 @@ Route::middleware(['auth', 'isUser'])->group(function () {
 
         Route::prefix('live-class')->name('live-class.')->group(function () {
             Route::get('/', [LiveClassController::class, 'index'])->name('index');
+            Route::get('/{live}', [LiveClassController::class, 'show'])->name('show');
         });
 
         Route::middleware('subscription')->group(function () {
 
             Route::prefix('live-class')->name('live-class.')->group(function () {
                 // Route::get('/', [LiveClassController::class, 'index'])->name('index');
-                Route::get('/{live}', [LiveClassController::class, 'show'])->name('show');
+                // Route::get('/{live}', [LiveClassController::class, 'show'])->name('show');
                 Route::get('/{live}/workshop', [LiveClassController::class, 'workshop'])->name('workshop');
                 Route::get('/{live}/workshop/form', [LiveClassController::class, 'workshopform'])->name('workshop.form');
                 Route::get('/{live}/private-class', [LiveClassController::class, 'privateclass'])->name('privateclass');
