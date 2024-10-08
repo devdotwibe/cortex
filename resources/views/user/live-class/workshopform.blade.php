bd91433bd150630311c3dfcdf6da501ca9f6dd97
nikhil <nikhildotwibe@outlook.com>
2024-10-08 11:03:38 +0530

    gf

--- resources/views/user/live-class/workshopform.blade.php
+++ resources/views/user/live-class/workshopform.blade.php
@@ -4,10 +4,8 @@
 <section class="header_nav">
     <div class="header_wrapp">
         <div class="header_title">
-            <div class="back-btn" id="back-btn" style="display: block"> <!-- Ensure proper display value -->
-                <a href="{{ route('live-class.workshop') }}">
-                    <img src="{{ asset('assets/images/leftarrowblack.svg') }}" alt="">
-                </a>
+            <div class="back-btn" >
+                <a href="{{route('live-class.workshop')}}"><img src="{{asset('assets/images/leftarrowblack.svg')}}" alt=""></a>
             </div>
             <h2>Live Class - {{($live_class->class_title_2??' Intensive Workshop ')}}</h2>
         </div>
