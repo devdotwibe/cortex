3e4bf6f9db298a9000179314e20a601a41e60737
jerindotwibe <jerindotwibe@outlook.com>
2024-10-09 19:17:12 +0530

    hgtr

--- resources/views/admin/live-class/index.blade.php
+++ resources/views/admin/live-class/index.blade.php
@@ -8,7 +8,7 @@
             </div>
         </div>
     </section>
-    <section class="content_section admin_section">
+    <section class="content_section admin_section private_section">
         <div class="container">
             <div class="row">
 
