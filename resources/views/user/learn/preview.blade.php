de0612438235ad15ae6e4a0fe97f4a8091011d29
jerindotwibe <jerindotwibe@outlook.com>
2024-10-09 19:27:24 +0530

    4567hgj

--- resources/views/user/learn/preview.blade.php
+++ resources/views/user/learn/preview.blade.php
@@ -118,16 +118,6 @@
                 }) 
                 if (res.total > 1) {
             $.each(res.links, function(k, v) {
-                let linkstatus = "";
-
-               
-                // Removing 'useranswers' logic, setting default classes
-                if (k != 0 && k != res.links.length) {
-                    linkstatus = 'status-bad';  // Default status as 'status-bad'
-                    if (v.iscorrect) {
-                        linkstatus = "status-good";  // If correct, change to 'status-good'
-                    }
-                }
                         if(v.active||!v.url){
                             $('#lesson-footer-pagination').append(`
                                 <button class="btn btn-secondary  ${linkstatus} ${v.active?"active":""}" disabled  >${v.label}</button>
