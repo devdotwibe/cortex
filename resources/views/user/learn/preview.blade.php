ac87b05bf70131d054168460ceec4e0b02a38ba4
jerindotwibe <jerindotwibe@outlook.com>
2024-10-10 14:04:54 +0530

    le

--- resources/views/user/learn/preview.blade.php
+++ resources/views/user/learn/preview.blade.php
@@ -117,6 +117,8 @@
                         },'json')
                     }
                 }) 
+                console.log(res);
+                console.log(useranswers);
                 if (res.total > 1) {
             $.each(res.links, function(k, v) {
                 let linkstatus="";
