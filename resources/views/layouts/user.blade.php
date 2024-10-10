e11b31decc5289c32c3e360f5832d5132898bb4e
jerindotwibe <jerindotwibe@outlook.com>
2024-10-10 16:45:32 +0530

    fg

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -537,15 +537,15 @@ if (!allowedTypes.includes(file.type)) {
     <script>
 
 $(document).ready(function() {
-       
-       console.log('test');
-       if ($('.side-dropdown-menu .side-item.active').length) {
-
-           console.log('active');
-           $('.side-dropdown-menu').css('display', 'block');
-
-       }
-   });
+    // Loop through each .side-dropdown-menu to check if it has an active item
+    $('.side-dropdown-menu').each(function() {
+        if ($(this).find('.side-item.active').length) {
+            $(this).css('display', 'block'); // Show only this dropdown menu if it has an active item
+        } else {
+            $(this).css('display', 'none');  // Hide other dropdown menus
+        }
+    });
+});
 
 
     </script>
