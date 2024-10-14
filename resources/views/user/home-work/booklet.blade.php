317aa307e3ec910cfeeed24abae321cbdc39c0be
jerindotwibe <jerindotwibe@outlook.com>
2024-10-14 15:47:36 +0530

    bookmark.png

--- resources/views/user/home-work/booklet.blade.php
+++ resources/views/user/home-work/booklet.blade.php
@@ -32,8 +32,10 @@
                 <div class="progress-main">
                     <div class="bookmark">
                         <a class="" id="bookmark-current" >
-                            <img class="active-img" src="{{asset("assets/images/bookmark.png")}}" alt="bookmark">
-                            <img class="inactive-img" src="{{asset("assets/images/bookmarkfill.png")}}" alt="bookmark">
+                            <span id="flagtext">Flag</span>
+                            <img class="active-img" src="{{asset("assets/images/flag-blue.svg")}}" alt="bookmark">
+                         
+                            <img class="inactive-img" src="{{asset("assets/images/flag-red.svg")}}" alt="bookmark">
                         </a>
                     </div>
                 </div>
@@ -365,8 +367,10 @@
                 refreshstatus(summery.cudx,'not-answered');
                 if(summery.flagdx[summery.cudx]){
                     $("#bookmark-current").addClass('active');
+                    $("#flagtext").text('Unflag');
                 }else{
                     $("#bookmark-current").removeClass('active');
+                    $("#flagtext").text('Flag');
                 }
                 summery.save()
                 $.each(res.data,function(k,v){ 
@@ -615,6 +619,7 @@
                     summery.flagcurrent[summery.currentSlug]=true;
                     summery.save();
                     $("#bookmark-current").removeClass('active');
+                    $("#flagtext").text('flag')
                     $(`#show-all .question-item[data-idx="${summery.cudx}"]`).removeClass('status-flag')
                     $(`#flagged .question-item[data-idx="${summery.cudx}"]`).removeClass('status-flag')
                 }else{
@@ -622,6 +627,7 @@
                     summery.flagcurrent[summery.currentSlug]=true;
                     summery.save();
                     $("#bookmark-current").addClass('active')
+                    $("#flagtext").text('Unflag')
                     $(`#show-all .question-item[data-idx="${summery.cudx}"]`).addClass('status-flag')
                     $(`#flagged .question-item[data-idx="${summery.cudx}"]`).addClass('status-flag')
                 } 
