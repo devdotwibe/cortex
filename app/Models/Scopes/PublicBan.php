7b6e7a2f22ab66ea914f17a83e8709b3471d32e8
jerindotwibe <jerindotwibe@outlook.com>
2024-10-14 15:51:10 +0530

    ssw

--- app/Models/Scopes/PublicBan.php
+++ app/Models/Scopes/PublicBan.php
@@ -14,11 +14,8 @@ class PublicBan implements Scope
      * Apply the scope to a given Eloquent query builder.
      */
     public function apply(Builder $builder, Model $model): void
-    {       
-        // Apply the condition for checking posts with admin_id not null
-        $adminpostExists = $builder->whereNotNull('admin_id')->exists();
-
-        if (!Auth::guard('admin')->check() || session('is.logined.as') === 'user' || $adminpostExists) {
+    {        
+        if(!Auth::guard('admin')->check()||session('is.logined.as','admin')=="user"){
             $builder->where(function($qry){
                 $qry->where(function($iqry){
                     $iqry->where('visible_status','show')->whereIn('user_id',User::where('post_status','active')->select('id'));
