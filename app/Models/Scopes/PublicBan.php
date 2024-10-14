b00cd372a859569b795ed7a03b9cd87d4965bc95
jerindotwibe <jerindotwibe@outlook.com>
2024-10-14 14:35:28 +0530

    rre

--- app/Models/Scopes/PublicBan.php
+++ app/Models/Scopes/PublicBan.php
@@ -15,10 +15,10 @@ class PublicBan implements Scope
      */
     public function apply(Builder $builder, Model $model): void
     {       
-        $adminbuilder =   $builder;
-        $adminpost = $adminbuilder->whereNotNull('admin_id');
+        // Apply the condition for checking posts with admin_id not null
+        $adminpostExists = $builder->whereNotNull('admin_id')->exists();
 
-        if(!Auth::guard('admin')->check()||session('is.logined.as','admin')=="user" || (!empty($adminpost))){
+        if (!Auth::guard('admin')->check() || session('is.logined.as') === 'user' || !$adminpostExists) {
             $builder->where(function($qry){
                 $qry->where(function($iqry){
                     $iqry->where('visible_status','show')->whereIn('user_id',User::where('post_status','active')->select('id'));
