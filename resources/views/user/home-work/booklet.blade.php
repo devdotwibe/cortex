9030393c86911243b938b4b838bdb41a5b3f1193
jerindotwibe <jerindotwibe@outlook.com>
2024-10-14 19:49:46 +0530

    wwe

--- resources/views/user/home-work/booklet.blade.php
+++ resources/views/user/home-work/booklet.blade.php
@@ -28,18 +28,7 @@
                     </div>
                 </div>
             </div>
-            <div class="bookmark">
-                <a class="" id="bookmark-current" >
-                    
-                    
-                    <span id="flagtext" class="flagclass">Flag</span>
-                    <span id="flagimages" class="flagclass" >
-                    <img class="active-img" src="{{asset("assets/images/flag-blue.svg")}}" alt="bookmark">
-                 
-                    <img class="inactive-img" src="{{asset("assets/images/flag-red.svg")}}" alt="bookmark">
-                    </span>
-                </a>
-            </div>
+           
         </div>
         
     </div>
@@ -63,6 +52,19 @@
         <div class="lesson-left pagination-arrow" style="display: none" >
             <button class="button left-btn"><img src="{{asset('assets/images/leftarrow.svg')}}" alt="<"> Back </button>
         </div>
+        <div class="bookmark">
+            <a class="" id="bookmark-current" >
+                
+                
+                <span id="flagtext" class="flagclass">Flag</span>
+                <span id="flagimages" class="flagclass" >
+                <img class="active-img" src="{{asset("assets/images/flag-blue.svg")}}" alt="bookmark">
+             
+                <img class="inactive-img" src="{{asset("assets/images/flag-red.svg")}}" alt="bookmark">
+                </span>
+            </a>
+        </div>
+        
         <div class="lesson-right pagination-arrow" style="display:none">
             <button class="button right-btn"> Next <img src="{{asset('assets/images/rightarrow.svg')}}" alt=">"></button>
         </div>
