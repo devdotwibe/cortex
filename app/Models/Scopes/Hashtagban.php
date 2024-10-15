14413aa2dc156d1ed0f4442d5d9b11609d79e0f6
jerindotwibe <jerindotwibe@outlook.com>
2024-10-15 16:55:44 +0530

    ww

--- app/Models/Scopes/Hashtagban.php
+++ app/Models/Scopes/Hashtagban.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Scopes;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
@@ -18,10 +19,8 @@ class Hashtagban implements Scope
         if(!Auth::guard('admin')->check()||session('is.logined.as','admin')=="user"){
             $builder->where(function($qry){
                 $qry->where(function($iqry){
-                    $iqry->where('visible_status','show')->whereIn('user_id',User::where('post_status','active')->select('id'));
+                    $iqry->where('post_id',Post::whereIn('user_id',User::whereIn('post_status','active')->select('id'))->select('id'));
                 });
-                $qry->orWhere('user_id',Auth::id());
-                $qry->orWhereNotNull('admin_id');
             });
         }
     }
