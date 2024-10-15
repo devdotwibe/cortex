4f92e5c1ff1c3f064b11b9ad9df5fa7ba787850c
jerindotwibe <jerindotwibe@outlook.com>
2024-10-15 16:00:27 +0530

    ttUYTU

--- app/Models/Hashtag.php
+++ app/Models/Hashtag.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\Hashtagban;
+use App\Trait\ResourceModel;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+#[ScopedBy([Hashtagban::class])]
 class Hashtag extends Model
 {
     use HasFactory;
