04731ce7962b5822e60d57b6fab59b28e0f5f58b
jerindotwibe <jerindotwibe@outlook.com>
2024-10-15 15:31:33 +0530

    22

--- app/Http/Controllers/User/CommunityController.php
+++ app/Http/Controllers/User/CommunityController.php
@@ -22,7 +22,12 @@ class CommunityController extends Controller
     {
 
 
-        $hashtags = Hashtag::groupBy('hashtag')->pluck('hashtag');
+        // $hashtags = Hashtag::groupBy('hashtag')->pluck('hashtag');
+
+        $hashtags = Hashtag::where('hashtag', 'LIKE', '#%')
+        ->groupBy('hashtag')
+        ->pluck('hashtag');
+       
 
 
         $hashtag = $request->input('hashtag');
