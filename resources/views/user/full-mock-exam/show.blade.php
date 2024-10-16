cfba3d6e3585194b25f9e8b9f6372264d38a3cf7
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-10-16 15:57:12 +0530

    span addeed

--- resources/views/user/full-mock-exam/show.blade.php
+++ resources/views/user/full-mock-exam/show.blade.php
@@ -65,10 +65,13 @@
             <div class="progress-main">
                 <div class="bookmark">
                     <a class="" id="bookmark-current" >
-                        <span id="flagtext">Flag</span>
+                        <span id="flagtext"  class="flagclass">Flag</span>
+
+                        <span id="flagimages" class="flagclass">
                         <img class="active-img" src="{{asset("assets/images/flag-blue.svg")}}" alt="bookmark">
                      
                         <img class="inactive-img" src="{{asset("assets/images/flag-red.svg")}}" alt="bookmark">
+                        </span>
                     </a>
                 </div>
             </div>
