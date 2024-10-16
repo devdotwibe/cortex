81dc44554080125435954646838b3305d0a91f1c
Akhil Kannan <dev.dotwibe@gmail.com>
2024-10-16 10:20:45 +0530

    sdfsd

--- app/Http/Controllers/User/LearnTopicController.php
+++ app/Http/Controllers/User/LearnTopicController.php
@@ -127,6 +127,7 @@ class LearnTopicController extends Controller
         $learncount=Learn::where('category_id',$category->id)->where('sub_category_id',$subCategory->id)->count();
         return view("user.learn.lesson",compact('category','exam','subCategory','user','learncount'));
     }
+    
 
     public function preview(Request $request,UserExamReview $userExamReview){
         $category=Category::find($userExamReview->category_id);
