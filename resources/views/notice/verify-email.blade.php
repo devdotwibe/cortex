2c70dcbad8a2076c4f6053af890e9cba178a2211
jerindotwibe <jerindotwibe@outlook.com>
2024-10-17 11:46:07 +0530

    wwu

--- resources/views/notice/verify-email.blade.php
+++ resources/views/notice/verify-email.blade.php
@@ -1,28 +1,40 @@
-@extends('layouts.auth')
-@section('title', 'Login')
-@section('content')  
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, 
-                    <a href="{{ route('verification.resend') }}" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</a>.
-
-                    <br><br>
-                    <a href="{{ route('dashboard') }}" class="btn btn-primary">{{ __('Back to Dashboard') }}</a>
+@extends('layouts.exam')
+@section('title', $exam->subtitle($category->id,"Module ".($category->getIdx()+1)).':'.$category->name)
+@section('content')
+<section class="exam-container" id="exam-container">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+    
+                    <div class="card-body">
+                        @if (session('resent'))
+                            <div class="alert alert-success" role="alert">
+                                {{ __('A fresh verification link has been sent to your email address.') }}
+                            </div>
+                        @endif
+    
+                        {{ __('Before proceeding, please check your email for a verification link.') }}
+                        {{ __('If you did not receive the email') }}, 
+                        <a href="{{ route('verification.resend') }}" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</a>.
+    
+                        <br><br>
+                        <a href="{{ route('dashboard') }}" class="btn btn-primary">{{ __('Back to Dashboard') }}</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</section>
+
 @endsection
+
+@push('modals')
+
+
+@push('footer-script')
+
+
+   
+@endpush
