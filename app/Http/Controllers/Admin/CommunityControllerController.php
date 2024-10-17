0d15a230b70230324d3a40c0d49390631e1111b7
jerindotwibe <jerindotwibe@outlook.com>
2024-10-17 15:59:20 +0530

    ww3

--- app/Http/Controllers/Admin/CommunityControllerController.php
+++ app/Http/Controllers/Admin/CommunityControllerController.php
@@ -358,31 +358,7 @@ foreach ($extractedHashtags as $hashtag) {
 
 
 
-
-//     public function store2(Request $request)
-// {
-//     $post = Post::create([
-//         'description' => $request->description,
-//         'hashtags' => json_encode($this->extractHashtags($request->description)),
-//         // Other fields
-//     ]);
-
-//     // Update or create hashtags
-//     foreach ($post->hashtags as $hashtag) {
-//         Hashtag::firstOrCreate(['hashtag' => $hashtag]);
-//     }
-
-//     // Redirect or return response
-// }
-
-// private function extractHashtags($text)
-// {
-//     preg_match_all('/#\w+/', $text, $matches);
-//     return array_unique($matches[0]);
-// }
-
-
-public function search(Request $request)
+    public function search(Request $request)
 {
     $query = $request->input('query');
 
@@ -397,4 +373,32 @@ public function search(Request $request)
 }
 
     
+
+
+
+
+    public function store2(Request $request)
+{
+    $post = Post::create([
+        'description' => $request->description,
+        'hashtags' => json_encode($this->extractHashtags($request->description)),
+        // Other fields
+    ]);
+
+    // Update or create hashtags
+    foreach ($post->hashtags as $hashtag) {
+        Hashtag::firstOrCreate(['hashtag' => $hashtag]);
+    }
+
+    // Redirect or return response
+}
+
+private function extractHashtags($text)
+{
+    preg_match_all('/#\w+/', $text, $matches);
+    return array_unique($matches[0]);
+}
+
+
+
 }
