50469dcce18986c3e376ab9dfd7499f7f9ddd691
jerindotwibe <jerindotwibe@outlook.com>
2024-10-17 11:52:37 +0530

    34

--- resources/views/notice/verify-email.blade.php
+++ resources/views/notice/verify-email.blade.php
@@ -2,22 +2,7 @@
 @section('title', 'Login')
 @section('content')
 <section class="exam-container" id="exam-container">
-    <div class="container-wrap">
-        <div class="lesson">
-            <a class="lesson-exit float-start" href="{{route('learn.show',$category->slug)}}" title="Exit" data-title="Exit" aria-label="Exit" data-toggle="tooltip" >
-                <img src="{{asset("assets/images/exiticon.svg")}}" alt="exiticon">
-            </a>
-            <div class="lesson-title">
-                <h5><span>{{$exam->subtitle($category->id,"Module ".($category->getIdx()+1))}}</span><span> : </span><span>{{$category->name}}</span></h5>
-            </div>
-            <div class="lesson-body">
-                <div class="row" id="lesson-questionlist-list" style="display: none">
-                </div>
-            </div>
-            <div class="lesson-footer" id="lesson-footer-pagination">
-            </div>
-        </div>
-    </div>
+   
 </section>
 
 @endsection
