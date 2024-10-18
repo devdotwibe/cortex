b171748d31b4ba0763d46ca76da676480af7fd9b
jerindotwibe <jerindotwibe@outlook.com>
2024-10-18 16:59:39 +0530

    ww1

--- resources/views/admin/pages/course.blade.php
+++ resources/views/admin/pages/course.blade.php
@@ -748,7 +748,7 @@
                                             <input type="file" class="form-control"  style="display: none;" name="fullmockimage" id="fullmockimage">
                                         </div>
                                     </div>
-                                </div>
+                                
 
                              
 
@@ -773,7 +773,7 @@
                                 </div>
 
                             </div>
-
+                        </div>
                                 <button type="submit" class="btn btn-dark fullmock" name="sub_section"
                                     value="tab4_save">Save</button>
                             </form>
