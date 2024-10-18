57ea68a89fa49a662dbd4b13f5e78a46feb964b1
jerindotwibe <jerindotwibe@outlook.com>
2024-10-18 16:35:18 +0530

    33e

--- resources/views/admin/pages/course.blade.php
+++ resources/views/admin/pages/course.blade.php
@@ -104,12 +104,17 @@
 
                                       
 
-                                            <div class="col-md-12">
-                                                <div class="form-group">
-                                                    <label for="privateimage">Private Image</label>
-                                                    <input type="file" class="form-control" name="privateimage">
-                                                </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label for="image" class="file-upload">Image  <br>
+                                                    <img src="{{ asset('assets/images/upfile.svg') }}"
+                                                        alt="Upload Icon"> </label>
+                                                <input type="file" class="form-control" style="display: none;" name="image" id="imageInput" onchange="previewImage()">
+                                                @error('image')
+                                                <div class="text-danger">{{ $message }}</div>
+                                            @enderror
                                             </div>
+                                        </div>
 
 
 
