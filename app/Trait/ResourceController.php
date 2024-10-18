67374fd9b7ce167249083088fcaea9f120afd08e
jerindotwibe <jerindotwibe@outlook.com>
2024-10-18 15:21:13 +0530

    qqw

--- app/Trait/ResourceController.php
+++ app/Trait/ResourceController.php
@@ -171,7 +171,7 @@ trait ResourceController
         <img src="' . asset("assets/images/icons/iconamoon_edit.svg") . '" alt="Edit">
     </span>
     <span class="adminactive-icon">
-        <img src="' . asset("assets/images/iconshover/-iconamoon_edit-yellow.svg") . '" alt="Edit Active">
+        <img src="' . asset("assets/images/iconshover/iconamoon_edit-yellow.svg") . '" alt="Edit Active">
     </span>
 </a>
 
