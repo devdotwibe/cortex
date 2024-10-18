f316befcbb001c716276bbf611e97df76791b5ad
jerindotwibe <jerindotwibe@outlook.com>
2024-10-18 12:01:37 +0530

    eew

--- resources/views/user/community/posts.blade.php
+++ resources/views/user/community/posts.blade.php
@@ -104,7 +104,7 @@
               const userName = user.name;
               const userID = user.id;
   
-              const url = "{{ route('admin.community.index', ['user_id' => '__userID__']) }}".replace('__userID__', userID);
+              const url = "{{ route('community.index', ['user_id' => '__userID__']) }}".replace('__userID__', userID);
   
               // Append unique user names to the search results
               $('#searchResults').append(`
