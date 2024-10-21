611123f8a5699e8f65b00917a50b49d48775959e
jerindotwibe <jerindotwibe@outlook.com>
2024-10-21 18:49:27 +0530

    www

--- resources/views/user/question-bank/set.blade.php
+++ resources/views/user/question-bank/set.blade.php
@@ -2,7 +2,7 @@
 @section('headerclass','header-class')
 @section('title', $exam->subtitle($category->id,"Topic ".($category->getIdx()+1)).':'.$category->name)
 @section('content')
-<section class="exam-container questionclass">
+<section class="exam-container questionclass answerclass">
     <div class="exam-progress">
         <div class="exam-progress-inner">
             <div class="exam-progress-inner-item exam-left">
