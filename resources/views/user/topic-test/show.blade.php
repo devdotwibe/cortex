8584b26c544553182d113c0768e17bda736b6b56
jerindotwibe <jerindotwibe@outlook.com>
2024-10-21 18:16:30 +0530

    wwW

--- resources/views/user/topic-test/show.blade.php
+++ resources/views/user/topic-test/show.blade.php
@@ -290,9 +290,9 @@
             timerDiv.slideToggle(300, function() {
 
                 if (timerDiv.is(':visible')) {
-                    button.text('Hide time');
+                    button.text('Hide Time');
                 } else {
-                    button.text('Show time');
+                    button.text('Show Time');
                     button.insertAfter(timerDiv);
                 }
             });
