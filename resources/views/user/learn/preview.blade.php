4d50d5188b5fb379d0ef3ce833837e1b5fddd9a5
jerindotwibe <jerindotwibe@outlook.com>
2024-10-21 18:04:54 +0530

    rr5

--- resources/views/user/learn/preview.blade.php
+++ resources/views/user/learn/preview.blade.php
@@ -61,6 +61,49 @@
         </div>
     </div> 
 </section> 
+
+<section class="exam-footer"> 
+    <div class="lesson-pagination">
+        <div class="lesson-left pagination-arrow" style="display: none" >
+            <button class="button left-btn"><img src="{{asset('assets/images/leftarrow.svg')}}" alt="<"> Back </button>
+        </div>
+
+
+        <div class="exam-right exam-progress-inner-item">
+
+            <div class="progress-main">
+
+                
+                {{-- <div class="bookmark">
+                    
+                    <a class="" id="bookmark-current" >
+                        
+                        <span id="flagtext" class="flagclass">Flag</span>
+                        <span id="flagimages" class="flagclass" >
+                        <img class="active-img" src="{{asset("assets/images/flag-blue.svg")}}" alt="bookmark">
+                    
+                        <img class="inactive-img" src="{{asset("assets/images/flag-red.svg")}}" alt="bookmark">
+                        </span>
+                    </a>
+                </div> --}}
+            </div>
+        </div>
+        
+
+
+      
+
+
+        <div class="lesson-right pagination-arrow" style="display:none">
+            <button class="button right-btn"> Next <img src="{{asset('assets/images/rightarrow.svg')}}" alt=">"></button>
+        </div>
+        <div class="lesson-finish pagination-arrow" style="display:none">
+            <button class="button finish-btn" > Finish Set <img src="{{asset('assets/images/rightarrow.svg')}}" alt=">"></button>
+        </div>  
+    </div> 
+</section>
+
+
 @endsection
 
 @push('footer-script') 
