73c35d2cf8014506b415d30c5e9764ff9a597670
jerindotwibe <jerindotwibe@outlook.com>
2024-10-22 14:03:18 +0530

    yyt

--- resources/views/user/live-class/private.blade.php
+++ resources/views/user/live-class/private.blade.php
@@ -50,7 +50,7 @@
                   
                   @endguest --}}
                   @guest('admin')  
-                  <a class="btn btn-outline-warning m-2" href="#" onclick="showLockedModal()">Register</a>
+                  <a class="btn btn-outline-warning m-2" href="#" onclick="showLockedModal()">Register1</a>
               @endguest
                     @elseif($user->privateClass->status!="approved")
                     @if($user->privateClass->status=="pending") <p class="text-warning"> You are under verification, Please wait.</p> @elseif($user->privateClass->status=="rejected") <p class="text-danger" >Your are rejected by admin, Please <a @if(!empty(optional($setting)->emailaddress)) href="mailto:{{optional($setting)->emailaddress}}" @endif >contact Admin {{ optional($setting)->emailaddress }}</a> for further details.</p> @else <span class="btn btn-outline-warning"> {{ucfirst($user->privateClass->status)}} </span> @endif
