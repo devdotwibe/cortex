897240f044871f7260dc2b50d2fa67a1d3b477f2
jerindotwibe <jerindotwibe@outlook.com>
2024-10-22 12:25:52 +0530

    ttu

--- routes/web.php
+++ routes/web.php
@@ -236,7 +236,14 @@ Route::middleware(['auth', 'isUser'])->group(function () {
         });
 
 
-
+        Route::prefix('tipsandadvice')->name('tipsandadvise.')->group(function () {
+            Route::get('/', [TipsAndAdviceController::class, 'index'])->name('index');
+            Route::resource('/post',TipsAndAdviceController ::class);
+            Route::get('/tips-show/{id}', [TipsAndAdviceController::class, 'tip_show'])->name('tip_show');
+    
+        });
+    
+    
 
 
 
@@ -337,14 +344,7 @@ Route::middleware(['auth', 'isUser'])->group(function () {
 
     });
 
-    Route::prefix('tipsandadvice')->name('tipsandadvise.')->group(function () {
-        Route::get('/', [TipsAndAdviceController::class, 'index'])->name('index');
-        Route::resource('/post',TipsAndAdviceController ::class);
-        Route::get('/tips-show/{id}', [TipsAndAdviceController::class, 'tip_show'])->name('tip_show');
-
-    });
-
-
+  
 
 });
 
