1f5e3a30894e120d2b9656657b2afd93cf8d441b
Bloves <bijujoseph666@gmail.com>
2024-10-22 21:36:47 +0530

    exam retry values

--- app/Models/ExamRetryReview.php
+++ app/Models/ExamRetryReview.php
@@ -51,9 +51,13 @@ class ExamRetryReview extends Model
                                 ->count();
     }
     public function avgTime($id=null){ 
-        $qst=ExamRetryQuestion::where('exam_retry_review_id',$this->id)->where('exam_id',$this->exam_id);
+        $qst=ExamRetryQuestion::where('exam_retry_review_id',$this->id)
+                                ->where('exam_id',$this->exam_id);
         if(!empty($id)){
-            $qst->whereIn('question_id',Question::where("category_id",$id)->where('exam_id',$this->exam_id)->select('id'));
+            $qst->whereIn('question_id',
+                        Question::where("category_id",$id)
+                                        ->where('exam_id',$this->exam_id)
+                                        ->select('id'));
         }
         return round($qst->whereNotNull('time_taken')->where('time_taken','>',0)->average('time_taken'),2);
     }
@@ -67,12 +71,37 @@ class ExamRetryReview extends Model
         $exmcnt=0; 
         switch ($this->name) {
             case 'full-mock-exam':
-                $anscnt = ExamRetryAnswer::where('exam_retry_review_id','<=',$userExamReviewId)->where('exam_id',$this->exam_id)->whereIn('exam_retry_review_id',ExamRetryReview::where('name','full-mock-exam')->where('id','<=',$userExamReviewId)->groupBy('user_id')->select(DB::raw('MAX(id)')))->where('iscorrect',true)->where('user_answer',true)->count();
-                $exmcnt = ExamRetryReview::where('id','<=',$userExamReviewId)->where('exam_id',$this->exam_id)->whereIn('id',ExamRetryReview::where('name','full-mock-exam')->where('id','<=',$userExamReviewId)->groupBy('user_id')->select(DB::raw('MAX(id)')))->count();
-                break; 
+                $anscnt = ExamRetryAnswer::where('exam_retry_review_id', '=', $userExamReviewId)
+                                            ->where('exam_id', $this->exam_id)
+                                            // ->whereIn('exam_retry_review_id', 
+                                            //                 ExamRetryReview::where('name', 'full-mock-exam')
+                                            //                                         ->where('id', '<=', $userExamReviewId)
+                                            //                                         ->groupBy('user_id')
+                                            //                                         ->select(DB::raw('MAX(id)')))
+                                            ->where('iscorrect', true)
+                                            ->where('user_answer', true)
+                                            ->count();
+                $exmcnt = ExamRetryReview::where('id', '<=', $userExamReviewId)
+                                            ->where('exam_id', $this->exam_id)
+                                            // ->whereIn('id', 
+                                            //                     ExamRetryReview::where('name', 'full-mock-exam')
+                                            //                     ->where('id', '<=', $userExamReviewId)
+                                            //                     ->groupBy('user_id')
+                                            //                     ->select(DB::raw('MAX(id)')))
+                                            ->count();
+                break;
             case 'topic-test':
-                $anscnt = ExamRetryAnswer::where('exam_retry_review_id','<=',$userExamReviewId)->where('exam_id',$this->exam_id)->whereIn('exam_retry_review_id',ExamRetryReview::where('name','topic-test')->where('category_id',$categoryId)->where('id','<=',$userExamReviewId)->groupBy('user_id')->select(DB::raw('MAX(id)')))->where('iscorrect',true)->where('user_answer',true)->count();
-                $exmcnt = ExamRetryReview::where('id','<=',$userExamReviewId)->where('exam_id',$this->exam_id)->where('category_id',$categoryId)->whereIn('id',ExamRetryReview::where('name','topic-test')->where('category_id',$categoryId)->where('id','<=',$userExamReviewId)->groupBy('user_id')->select(DB::raw('MAX(id)')))->count();
+                $anscnt = ExamRetryAnswer::where('exam_retry_review_id', '<=', $userExamReviewId)
+                                            ->where('exam_id', $this->exam_id)
+                                            ->whereIn('exam_retry_review_id', ExamRetryReview::where('name', 'topic-test')
+                                            ->where('category_id', $categoryId)
+                                            ->where('id', '<=', $userExamReviewId)
+                                            ->groupBy('user_id')
+                                            ->select(DB::raw('MAX(id)')))
+                                            ->where('iscorrect', true)
+                                            ->where('user_answer', true)
+                                            ->count();
+                $exmcnt = ExamRetryReview::where('id', '<=', $userExamReviewId)->where('exam_id', $this->exam_id)->where('category_id', $categoryId)->whereIn('id', ExamRetryReview::where('name', 'topic-test')->where('category_id', $categoryId)->where('id', '<=', $userExamReviewId)->groupBy('user_id')->select(DB::raw('MAX(id)')))->count();
                 break; 
             // case 'learn':
             //     $anscnt = ExamRetryAnswer::where('exam_retry_review_id','<=',$userExamReviewId)->where('exam_id',$this->exam_id)->whereIn('exam_retry_review_id',ExamRetryReview::where('name','learn')->where('category_id',$categoryId)->where('sub_category_id',$subCategoryId)->where('id','<=',$userExamReviewId)->groupBy('user_id')->select(DB::raw('MAX(id)')))->where('iscorrect',true)->where('user_answer',true)->count();
