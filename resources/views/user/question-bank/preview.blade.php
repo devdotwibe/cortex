091d720c1e6b2fc2b7d74525a3147f10902cce0f
jerindotwibe <jerindotwibe@outlook.com>
2024-10-23 16:47:52 +0530

    e

--- resources/views/user/question-bank/preview.blade.php
+++ resources/views/user/question-bank/preview.blade.php
@@ -66,18 +66,7 @@
         </div>
     </div>
     <!-- ............................................................................................................. -->
-    <div class="lesson-left lesson-pagination">
-            <!-- <button class="button left-btn"><img src="{{asset('assets/images/leftarrow.svg')}}" alt="<"> Back </button> -->
-            <a class="lesson-exit float-start" href="{{route('question-bank.set.complete',$userExamReview->slug)}}"  title="Exit" data-title="Exit" aria-label="Exit" data-toggle="tooltip">
-                Back
-        </a>
-    </div>
-
-    <div class="lesson-right">
-        <a class="lesson-exit float-start" href="{{route('question-bank.show',$category->slug)}}"  title="Exit" data-title="Exit" aria-label="Exit" data-toggle="tooltip">
-Exit Review
-    </a>
-    </div>
+   
             <!-- .............................................................................................................. -->
 </section> 
 
@@ -107,6 +96,19 @@ Exit Review
                 </div> --}}
             </div>
         </div>
+
+        <div class="lesson-left lesson-pagination">
+            <!-- <button class="button left-btn"><img src="{{asset('assets/images/leftarrow.svg')}}" alt="<"> Back </button> -->
+            <a class="lesson-exit float-start" href="{{route('question-bank.set.complete',$userExamReview->slug)}}"  title="Exit" data-title="Exit" aria-label="Exit" data-toggle="tooltip">
+                Back
+        </a>
+    </div>
+
+    <div class="lesson-right">
+        <a class="lesson-exit float-start" href="{{route('question-bank.show',$category->slug)}}"  title="Exit" data-title="Exit" aria-label="Exit" data-toggle="tooltip">
+Exit Review
+    </a>
+    </div>
         
 
 
