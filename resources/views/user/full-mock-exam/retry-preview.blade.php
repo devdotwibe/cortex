1ff5b16098aa2223a4f86e9910945fd86a661136
jerindotwibe <jerindotwibe@outlook.com>
2024-10-23 20:19:02 +0530

    uu

--- resources/views/user/full-mock-exam/retry-preview.blade.php
+++ resources/views/user/full-mock-exam/retry-preview.blade.php
@@ -8,11 +8,11 @@
             <div class="exam-progress-inner-item exam-left">
                 <div class="progress-main">
 
-                    <div class="exam-exit ">
+                    {{-- <div class="exam-exit ">
                         <a  href="{{route('full-mock-exam.index')}}" title="Exit" data-title="Exit" aria-label="Exit" data-toggle="tooltip">
                             <img src="{{asset("assets/images/exiticon-wht.svg")}}" alt="exiticon">
                         </a>
-                    </div>
+                    </div> --}}
 
                     {{-- <div class="question-number">
                         <span>Question: </span>
