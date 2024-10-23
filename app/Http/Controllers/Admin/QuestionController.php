220d361a3e52a9b076fb49608b7f35c493d7d283
jerindotwibe <jerindotwibe@outlook.com>
2024-10-23 16:16:22 +0530

    66e

--- app/Http/Controllers/Admin/QuestionController.php
+++ app/Http/Controllers/Admin/QuestionController.php
@@ -85,17 +85,15 @@ class QuestionController extends Controller
 
        
 
-        $files = $request->file('answer'); // Assuming 'answer' contains the file input
+        $files = $request->file_answer;
 
         $question = Question::store($questiondat);
-        foreach ($request->answer as $k => $ans) {
-            $imageName = "";
-            // Check if a file is present in the specific answer
-            if ($request->hasFile("answer.$k.image")) {
-                // Store the file and get the hashed filename
-                $imageName = "questionimages/" . $request->file("answer.$k.image")->hashName();
-                $request->file("answer.$k.image")->storeAs('questionimages', $request->file("answer.$k.image")->hashName());
-            }
+foreach ($request->answer as $k => $ans) {
+    $imageName = "";
+    if ($request->hasFile($files[$k])) {
+        $imageName = "questionimages/" . $request->file("answer.$k.image")->hashName();
+        $request->file("answer.$k.image")->storeAs('questionimages', $imageName);
+    }
     $answer = Answer::store([
         "exam_id" => $question->exam_id,
         "question_id" => $question->id,
