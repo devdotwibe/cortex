f95b71df2431c3392d6e2791b7e86d34c2ed108d
jerindotwibe <jerindotwibe@outlook.com>
2024-10-23 14:11:54 +0530

    ttt

--- routes/web.php
+++ routes/web.php
@@ -255,6 +255,8 @@ Route::middleware(['auth', 'isUser'])->group(function () {
 
                 Route::get('/{live}/private-class/form', [LiveClassController::class, 'privateclassform'])->name('privateclass.form');
                 Route::post('/{live}/private-class/form', [LiveClassController::class, 'privateclassformsubmit']);
+
+                
                 Route::get('/{live}/private-class/room', [LiveClassController::class, 'privateclassroom'])->name('privateclass.room');
                 Route::middleware('hasPrivateClass')->group(function () {
                    
