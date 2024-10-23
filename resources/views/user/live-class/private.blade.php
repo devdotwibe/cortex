1e0ec68f0425622e0df00694a08bd74cc8fbccdb
jerindotwibe <jerindotwibe@outlook.com>
2024-10-23 11:03:52 +0530

    eeu

--- resources/views/user/live-class/private.blade.php
+++ resources/views/user/live-class/private.blade.php
@@ -50,8 +50,11 @@
                   
                   @endguest --}}
                   @guest('admin')  
+                  @if(!(auth('web')->user()->is_free_access) && (optional(auth('web')->user()->subscription())->status ?? "") !== "subscribed")
 
                   <a class="btn btn-warning m-2" href="{{route('live-class.privateclass.room',$user->slug)}}">Enter</a>
+
+                    @else
                   <a class="btn btn-outline-warning m-2" href="#" onclick="showLockedModal()">Register</a>
               @endguest
                     @elseif($user->privateClass->status!="approved")
