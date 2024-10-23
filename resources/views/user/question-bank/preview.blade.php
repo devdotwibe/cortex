d88cacac082bd515d19e804971c6bb5ba67f8b6e
jerindotwibe <jerindotwibe@outlook.com>
2024-10-23 16:58:08 +0530

    uuu

--- resources/views/user/question-bank/preview.blade.php
+++ resources/views/user/question-bank/preview.blade.php
@@ -84,7 +84,7 @@ Exit Review
         {{-- <div class="lesson-left pagination-arrow" style="display: none" >
             <button class="button left-btn"><img src="{{asset('assets/images/leftarrow.svg')}}" alt="<"> Back </button>
         </div> --}}
-        <a class="lesson-exit float-start button left-btn " href="{{route('question-bank.set.complete',$userExamReview->slug)}}"  title="Exit" data-title="Exit" aria-label="Exit" data-toggle="tooltip">
+        <a class="lesson-exit float-start button left-btn" > <img src="{{asset('assets/images/leftarrow.svg')}}" href="{{route('question-bank.set.complete',$userExamReview->slug)}}"  title="Exit" data-title="Exit" aria-label="Exit" data-toggle="tooltip">
             Back
     </a>
 
