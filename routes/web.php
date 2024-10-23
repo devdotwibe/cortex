b1ccd7667ed75ce8978dc0db5876a94e37f3cc9b
jerindotwibe <jerindotwibe@outlook.com>
2024-10-23 11:38:39 +0530

    rr2

--- routes/web.php
+++ routes/web.php
@@ -175,6 +175,17 @@ Route::middleware(['auth', 'isUser'])->group(function () {
         
 
 
+        Route::prefix('live-class')->name('live-class.')->group(function () {
+            Route::get('/', [LiveClassController::class, 'index'])->name('index');
+            // Route::get('/{live}', [LiveClassController::class, 'show'])->name('show');
+            Route::get('/{live}/workshop', [LiveClassController::class, 'workshop'])->name('workshop');
+            // Route::get('/{live}/workshop/form', [LiveClassController::class, 'workshopform'])->name('workshop.form');
+            Route::get('/{live}/private-class', [LiveClassController::class, 'privateclass'])->name('privateclass');
+
+
+           
+            });
+       
 
 
 
@@ -235,11 +246,11 @@ Route::middleware(['auth', 'isUser'])->group(function () {
         Route::middleware('subscription')->group(function () {
 
             Route::prefix('live-class')->name('live-class.')->group(function () {
-                Route::get('/', [LiveClassController::class, 'index'])->name('index');
+        //         Route::get('/', [LiveClasszController::class, 'index'])->name('index');
                 Route::get('/{live}', [LiveClassController::class, 'show'])->name('show');
-                Route::get('/{live}/workshop', [LiveClassController::class, 'workshop'])->name('workshop');
+                // Route::get('/{live}/workshop', [LiveClassController::class, 'workshop'])->name('workshop');
                 Route::get('/{live}/workshop/form', [LiveClassController::class, 'workshopform'])->name('workshop.form');
-                Route::get('/{live}/private-class', [LiveClassController::class, 'privateclass'])->name('privateclass');
+        //         Route::get('/{live}/private-class', [LiveClassController::class, 'privateclass'])->name('privateclass');
 
 
                 Route::get('/{live}/private-class/form', [LiveClassController::class, 'privateclassform'])->name('privateclass.form');
