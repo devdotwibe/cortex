89d24a1aadb65905a5b449d16d5ec60d56346efb
jerindotwibe <jerindotwibe@outlook.com>
2024-10-23 17:03:26 +0530

    uuw

--- resources/views/user/question-bank/preview.blade.php
+++ resources/views/user/question-bank/preview.blade.php
@@ -116,9 +116,15 @@ Exit Review
       
 
 
-        <div class="lesson-right pagination-arrow" style="display:none">
+        {{-- <div class="lesson-right pagination-arrow" style="display:none">
             <button class="button right-btn"> Next <img src="{{asset('assets/images/rightarrow.svg')}}" alt=">"></button>
+        </div> --}}
+        <div class="lesson-right pagination-arrow" style="display:none">
+            <a href="{{ route('question-bank.show', $category->slug) }}" class="button right-btn" title="Next">
+                Finish Set <img src="{{ asset('assets/images/rightarrow.svg') }}" alt=">">
+            </a>
         </div>
+        
         <div class="lesson-finish pagination-arrow" style="display:none">
             <button class="button finish-btn" onclick="window.location.href='{{ route('question-bank.show',$category->slug) }}'"> Finish Set <img src="{{asset('assets/images/rightarrow.svg')}}" alt=">"></button>
         </div>  
