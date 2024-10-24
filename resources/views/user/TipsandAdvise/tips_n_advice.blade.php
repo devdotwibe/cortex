9fd4af5d1a1267f334e3f47eb69a0a080f6465f6
jerindotwibe <jerindotwibe@outlook.com>
2024-10-24 17:01:45 +0530

    rrw

--- resources/views/user/TipsandAdvise/tips_n_advice.blade.php
+++ resources/views/user/TipsandAdvise/tips_n_advice.blade.php
@@ -3,12 +3,14 @@
 @section('content')
 
 <div class="critical-reasoning">
+    <div class="backtitleclass">
     <div class="back-btn" id="back-btn" style="display: block"> <!-- Ensure proper display value -->
         <a href="{{ route('tipsandadvise.index') }}">
             <img src="{{ asset('assets/images/exiticon.svg') }}" alt="">
         </a>
     </div>
     <h2>Tips And Advice</h2>
+</div>
     <div class="critical-reasoning-row">
         <!-- Left Column: List of Tips -->
         <div class="critical-reasoning-col1">
