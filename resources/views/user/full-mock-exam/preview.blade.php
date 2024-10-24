8e37142396bc4441f68715760a8b1ba927a0bfe4
jerindotwibe <jerindotwibe@outlook.com>
2024-10-24 11:24:31 +0530

    33e

--- resources/views/user/full-mock-exam/preview.blade.php
+++ resources/views/user/full-mock-exam/preview.blade.php
@@ -1,55 +1,13 @@
 @extends('layouts.exam')
-
+@section('headerclass','header-class')
 @section('title', $exam->title)
 @section('content')
-<section class="exam-container questionclass answerclass">
-    <div class="exam-progress quest-progress">
-        <div class="exam-progress-inner">
-            <div class="exam-progress-inner-item exam-left">
-                <div class="progress-main">
-
-                    <div class="exam-exit ">
-                        <a  href="{{route('full-mock-exam.index')}}" title="Exit" data-title="Exit" aria-label="Exit" data-toggle="tooltip">
-                            <img src="{{asset("assets/images/exiticon-wht.svg")}}" alt="exiticon">
-                        </a>
-                    </div>
-
-                    {{-- <div class="question-number">
-                        <span>Question: </span>
-                    </div> --}}
-    
-                   
-
-                    
-                </div>
-            </div>
-           
-            <div class="question-header question-number">
-                <div class="progress-menus">
-                    <div class="menu-text">
-                        <span id="menu-text" >Question <span> 0 </span>   <span>0 </span> </span>
-                      
-                    </div>
-                    <div class="menu-icon"> 
-                        <a onclick="toglepreviewpage()" >
-                            {{-- <img src="{{asset("assets/images/menu.svg")}}" alt="exiticon"> --}}
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="Review-mode">
-                <span>Review Mode </span>
-            </div>
-           
-        </div>
-        
-    </div>
+<section class="exam-container">
     <div class="container-wrap mcq-container-wrap full-mock-exam-review">
         <div class="lesson">            
-            {{-- <a class="lesson-exit float-start" href="{{route('full-mock-exam.index')}}"  title="Exit" data-title="Exit" aria-label="Exit" data-toggle="tooltip">
+            <a class="lesson-exit float-start" href="{{route('full-mock-exam.index')}}"  title="Exit" data-title="Exit" aria-label="Exit" data-toggle="tooltip">
                 <img src="{{asset("assets/images/exiticon.svg")}}" alt="exiticon">
-            </a>  --}}
+            </a> 
             <div class="lesson-body"> 
                 <div class="row" id="lesson-questionlist-list" style="display: none">
                 </div>
@@ -59,63 +17,6 @@
         </div>
     </div> 
 </section> 
-<section class="exam-footer"> 
-    <div class="lesson-pagination">
-      
-
-        <div class="lesson-left ">
-            <a href="{{ route('full-mock-exam.complete', $userExamReview->slug) }}" class="button left-btn" title="Back">
-                <img src="{{ asset('assets/images/leftarrow.svg') }}" alt="<"> Back 
-            </a>
-        </div>
-
-
-        <div class="exam-right exam-progress-inner-item">
-
-            <div class="progress-main">
-
-                
-                {{-- <div class="bookmark">
-                    
-                    <a class="" id="bookmark-current" >
-                        
-                        <span id="flagtext" class="flagclass">Flag</span>
-                        <span id="flagimages" class="flagclass" >
-                        <img class="active-img" src="{{asset("assets/images/flag-blue.svg")}}" alt="bookmark">
-                    
-                        <img class="inactive-img" src="{{asset("assets/images/flag-red.svg")}}" alt="bookmark">
-                        </span>
-                    </a>
-                </div> --}}
-            </div>
-        </div>
-        
-
-
-      
-
-{{-- 
-        <div class="lesson-right pagination-arrow" style="display:none">
-            <button class="button right-btn"> Next <img src="{{asset('assets/images/rightarrow.svg')}}" alt=">"></button>
-        </div>
-        <div class="lesson-finish pagination-arrow" style="display:none">
-            <button class="button finish-btn" onclick="window.location.href='{{ route('full-mock-exam.index') }}'"> Finish Set <img src="{{asset('assets/images/rightarrow.svg')}}" alt=">"></button>
-        </div>  --}}
-        
-
-
-        <div class="finish-btn">
-            <a href="{{ route('full-mock-exam.index') }}" class="button right-btn" title="Next">
-                Finish Set <img src="{{ asset('assets/images/rightarrow.svg') }}" alt=">">
-            </a>
-        </div>
-
-
-
-        
-
-    </div> 
-</section>
 @endsection
 
 @push('footer-script') 
@@ -134,7 +35,7 @@
             return result;
         }
         function loadlessonreview(reviewurl){ 
-            $.get(reviewurl||"{{ route('full-mock-exam.preview',$userExamReview->slug) }}",function(res){
+            $.get(reviewurl||"{{ route('full-mock-exam.retry.preview', ['user_exam_review' => $userExamReview->slug, 'exam_retry_review' => $examRetryReview->slug]) }}",function(res){
                 $('.pagination-arrow').hide();
                 $('#lesson-footer-pagination').html('')
                 const lesseonId=generateRandomId(10); 
@@ -147,7 +48,7 @@
                                 </div>
                                 <div class="mcq-container">
                                     <div class="mcq-group">
-                                        
+                                        <h5><span>{{$exam->title}}</span></h5>
                                         <div class="mcq-title-text" ${v.title_text?"":'style="display:none"'}>
                                             ${v.title_text||""}
                                         </div>
@@ -158,7 +59,7 @@
                                     <div class="mcq-answer mcq-group-right">
                                         <div  class="mcq-description">
                                             ${v.sub_question||""}
-                                        </div> 
+                                        </div>
                                         <div id="mcq-${lesseonId}-ans" class="form-group">
                                             <div class="form-data" >
                                                 <div class="forms-inputs mb-4" id="mcq-${lesseonId}-list"> 
@@ -171,7 +72,7 @@
                                             ${v.explanation||''}
                                         </div>
 
-                                        <div id="mcq-${lesseonId}-ans-progress" class="form-group">
+                                        {{-- <div id="mcq-${lesseonId}-ans-progress" class="form-group">
                                             <div class="form-data" >
                                                 <div class="forms-inputs mb-4" id="mcq-${lesseonId}-list-progress"> 
                                                     
@@ -180,13 +81,13 @@
                                             <div>
                                                 <p>You spent ${v.time_taken||0} seconds on this question. The average student spent ${v.total_user_taken_time||0} seconds on this question<p>
                                             </div>
-                                        </div>
+                                        </div> --}}
                                     </div>
                                 </div>
                             </div>
                         </div>
                     `).fadeIn();
-                    $.get("{{ route('full-mock-exam.preview',$userExamReview->slug) }}",{question:v.slug},function(ans){
+                    $.get("{{ route('full-mock-exam.retry.preview', ['user_exam_review' => $userExamReview->slug, 'exam_retry_review' => $examRetryReview->slug]) }}",{question:v.slug},function(ans){
                         $(`#mcq-${lesseonId}-list`).html('')
                         $(`#mcq-${lesseonId}-list-progress`).html('')
                         $.each(ans,function(ai,av){
@@ -238,39 +139,16 @@
                         }
                      })
                 }
+ 
                 $('.lesson-end').show();
+            },'json')
 
+         }
 
-if (res.next_page_url) { 
-$('.lesson-right').show()
-.find('button.right-btn')
-.data('pageurl', res.next_page_url)
-.attr('onclick', `loadlessonreview('${res.next_page_url}')`); // Adding onclick event
-} else {
-$('.lesson-finish').show();
-}
+         $(function(){
+            loadlessonreview()
+         })
 
-if (res.prev_page_url) {
-$('.lesson-left').show()
-.find('button.left-btn')
-.data('pageurl', res.prev_page_url)
-.attr('onclick', `loadlessonreview('${res.prev_page_url}')`); // Adding onclick event
-}
-
-$('#menu-text').html(`Question <span> ${res.current_page} </span> `)
-
-},'json')
-
-}
-
-$(function(){
-loadlessonreview()
-})
-function toglepreviewpage(){
-// timerActive=!timerActive; 
-$('#question-preview-page').slideToggle()
-$('#question-answer-page').fadeToggle()
-}
 </script>
 
 @endpush
