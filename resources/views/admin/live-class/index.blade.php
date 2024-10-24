10958cc9be478b4563bbc44b0b3fd91a32dc9555
jerindotwibe <jerindotwibe@outlook.com>
2024-10-24 15:13:27 +0530

    66e

--- resources/views/admin/live-class/index.blade.php
+++ resources/views/admin/live-class/index.blade.php
@@ -373,16 +373,19 @@ These open group sessions condense the entire Thinking Skills curriculum into te
 
 
 
-                                    <div class="text-field">
+                                    <div class="text-field starttimeclass">
                                         <label for="starttime">Start Time:</label>
                                         <input type="text" name="starttime" id="starttime" class="form-control"
                                             placeholder="HH : MM" data-mask="^(0[0-9]|1[0-9]|2[0-4]) : [0-5][0-9]$"
                                             required>
+                                    </div>
+                                    <div class="starttimeampm">
                                         <select name="starttime_am_pm" id="starttime_am_pm" class="form-control"
                                             required>
                                             <option value="AM">AM</option>
                                             <option value="PM">PM</option>
                                         </select>
+                                    </div>
                                     </div>
                                 </div>
                                 <div class="startdayclass">
