d90c42f8710227e6092479d4e4229ed18d89e57a
jerindotwibe <jerindotwibe@outlook.com>
2024-10-25 15:52:41 +0530

    ww

--- resources/views/user/question-bank/preview.blade.php
+++ resources/views/user/question-bank/preview.blade.php
@@ -136,7 +136,6 @@
             $.get(reviewurl || "{{ route('question-bank.preview', $userExamReview->slug) }}", function(res) {
                 $('.pagination-arrow').hide();
                 $('#lesson-footer-pagination').html('')
-                $('#lesson-footer-pagination1').html('')
                 const lesseonId = generateRandomId(10);
                 $.each(res.data, function(k, v) {
                     $('#lesson-questionlist-list').html(`
@@ -252,14 +251,6 @@
                                 <button class="${linkstatus} btn btn-secondary" onclick="loadlessonreview('${v.url}')" >${v.label}</button>
                             `)
                         }
-                        $('#lesson-footer-pagination1').append(`
-                                <button class="${linkstatus} btn btn-secondary ${v.active?"active":""}" disabled  >${label_name}</button>
-                            `)
-                        } else {
-                            $('#lesson-footer-pagination1').append(`
-                                <button class="${linkstatus} btn btn-secondary" onclick="loadlessonreview('${v.url}')" >${v.label}</button>
-                            `)
-                        }
                     })
                 }
 
@@ -303,4 +294,185 @@
             $('#question-answer-page').fadeToggle()
         }
     </script>
+
+
+
+<script>
+    var useranswers = @json($useranswer);
+    var timelist = @json(json_decode($user->progress('exam-reviewed-' . $userExamReview->id . '-times', '[]')));
+
+    function generateRandomId(length) {
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let result = '';
+        const charactersLength = characters.length;
+
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+
+        return result;
+    }
+
+    function loadlessonreview(reviewurl) {
+        $.get(reviewurl || "{{ route('question-bank.preview', $userExamReview->slug) }}", function(res) {
+            $('.pagination-arrow').hide();
+            $('#lesson-footer-pagination1').html('')
+            const lesseonId = generateRandomId(10);
+            $.each(res.data, function(k, v) {
+                $('#lesson-questionlist-list').html(`
+                    <div class="col-md-12">
+                        <div class="mcq-row" >
+                            <div class="mcq-title">
+                                <span>${v.title||""}</span>
+                            </div>
+                            <div class="mcq-container">
+                                <div class="mcq-group">
+                                 
+                                    <div class="mcq-title-text" ${v.title_text?"":'style="display:none"'}>
+                                        ${v.title_text||""}
+                                    </div>
+                                    <div id="mcq-${lesseonId}">
+                                        ${v.note||""}
+                                    </div>
+                                </div>
+                                <div class="mcq-answer mcq-group-right">
+                                    <div  class="mcq-description">
+                                        ${v.sub_question||""}
+                                    </div>
+                                    <div id="mcq-${lesseonId}-ans" class="form-group">
+                                        <div class="form-data" >
+                                            <div class="forms-inputs mb-4" id="mcq-${lesseonId}-list"> 
+                                                
+                                            </div> 
+                                        </div>
+                                    </div>
+                                    <div id="mcq-${lesseonId}-explanation" class="mcq-explanation"> 
+                                        <label>Correct Answer <span id="mcq-${lesseonId}-correct"></span></label>
+                                        ${v.explanation||''}
+                                    </div>
+
+                                    <div id="mcq-${lesseonId}-ans-progress" class="form-group">
+                                        <div class="form-data" >
+                                            <div class="forms-inputs mb-4" id="mcq-${lesseonId}-list-progress"> 
+                                                
+                                            </div> 
+                                        </div>
+                                        <div>
+                                            <p>You spent <strong>${v.time_taken||0} seconds</strong> on this question. The average student spent <strong>${v.total_user_taken_time||0} seconds</strong> on this question<p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                `).fadeIn();
+
+                $.get("{{ route('question-bank.preview', $userExamReview->slug) }}", {
+                    question: v.slug
+                }, function(ans) {
+                    $(`#mcq-${lesseonId}-list`).html('')
+                    $(`#mcq-${lesseonId}-list-progress`).html('')
+                    $.each(ans, function(ai, av) {
+                        const letter = String.fromCharCode(ai + 'A'.charCodeAt(0))
+                        $(`#mcq-${lesseonId}-list`).append(`
+                        <div class="form-check-ans">
+                            <span class="question-user-ans ${av.iscorrect?"correct":"wrong"}" data-ans="${av.slug}"></span>
+                            <div class="form-check">
+                                <input type="radio" disabled name="answer" data-question="${v.slug}" id="user-answer-${lesseonId}-ans-item-${ai}" value="${av.slug}" class="form-check-input" ${av.user_answer?"checked":""}  >        
+                                <label for="user-answer-${lesseonId}-ans-item-${ai}" >${ letter }. ${av.title}</label>
+                            </div>  
+                        </div>
+                        `)
+                        $(`#mcq-${lesseonId}-list-progress`).append(`
+                            <div class="form-progress-ans ans-${av.iscorrect?"select":"no-select"}"> 
+                                <div class="form-progress">       
+                                    <label for="user-answer-${lesseonId}-ans-progress-item-${ai}" >${ letter }</label>
+                                    <progress id="user-answer-${lesseonId}-ans-progress-item-${ai}" max="100" value="${av.total_user_answered||0}"></progress> <span>${((av.total_user_answered||0)*1).toFixed(2)}%</span>
+                                </div>  
+                            </div>
+                        `)
+                        if (av.iscorrect) {
+                            $(`#mcq-${lesseonId}-correct`).text(`: ${ letter } `)
+                        }
+                    })
+                }, 'json')
+
+            })
+
+
+            if (res.total > 1) {
+                $.each(res.links, function(k, v) {
+                    let linkstatus = "";
+                    if (k != 0 && k != res.links.length && useranswers[k - 1]) {
+                        linkstatus = 'status-bad';
+                        if (useranswers[k - 1].iscorrect) {
+
+
+                            linkstatus = "status-good";
+
+
+                            if (useranswers[k - 1].time_taken < {{ $examtime }}) {
+                                linkstatus = "status-exelent";
+                            }
+                        }
+                    }
+                    if (v.active || !v.url) {
+
+                        var label_name = v.label;
+
+                        if (v.label == '« Previous') {
+                            var label_name = "<";
+                        }
+                        $('#lesson-footer-pagination1').append(`
+                            <button class="${linkstatus} btn btn-secondary ${v.active?"active":""}" disabled  >${label_name}</button>
+                        `)
+                    } else {
+                        $('#lesson-footer-pagination1').append(`
+                            <button class="${linkstatus} btn btn-secondary" onclick="loadlessonreview('${v.url}')" >${v.label}</button>
+                        `)
+                    }
+                })
+            }
+
+            $('.lesson-end').show();
+
+
+            if (res.next_page_url) {
+                $('.lesson-right').show()
+                    .find('button.right-btn')
+                    .data('pageurl', res.next_page_url)
+                    .attr('onclick', `loadlessonreview('${res.next_page_url}')`); // Adding onclick event
+            } else {
+                $('.lesson-finish').show();
+            }
+
+            if (res.prev_page_url) {
+                $('.lesson-left').show()
+                    .find('button.left-btn')
+                    .data('pageurl', res.prev_page_url)
+                    .attr('onclick', `loadlessonreview('${res.prev_page_url}')`); // Adding onclick event
+            }
+
+            $('#menu-text').html(`Question <span> ${res.current_page} </span> `)
+
+        }, 'json')
+
+    }
+
+    $(function() {
+        loadlessonreview()
+        $('.lesson-left button.left-btn,.lesson-right button.right-btn').click(function() {
+            const pageurl = $(this).data();
+
+            loadlessonreview(pageurl)
+        });
+    })
+
+    function toglepreviewpage() {
+        // timerActive=!timerActive; 
+        $('#question-preview-page').slideToggle()
+        $('#question-answer-page').fadeToggle()
+    }
+</script>
 @endpush
