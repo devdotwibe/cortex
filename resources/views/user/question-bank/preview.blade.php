a5580b87fbd9f4ce81dcb67456c873b71e48f691
jerindotwibe <jerindotwibe@outlook.com>
2024-10-25 16:17:23 +0530

    s1

--- resources/views/user/question-bank/preview.blade.php
+++ resources/views/user/question-bank/preview.blade.php
@@ -293,7 +293,30 @@
         }
 
 
-        
+                $('.lesson-end').show();
+
+
+                if (res.next_page_url) {
+                    $('.lesson-right').show()
+                        .find('button.right-btn')
+                        .data('pageurl', res.next_page_url)
+                        .attr('onclick', `loadlessonreview('${res.next_page_url}')`); // Adding onclick event
+                } else {
+                    $('.lesson-finish').show();
+                }
+
+                if (res.prev_page_url) {
+                    $('.lesson-left').show()
+                        .find('button.left-btn')
+                        .data('pageurl', res.prev_page_url)
+                        .attr('onclick', `loadlessonreview('${res.prev_page_url}')`); // Adding onclick event
+                }
+
+                $('#menu-text').html(`Question <span> ${res.current_page} </span> `)
+
+            }, 'json')
+
+        }
 
         $(function() {
             loadlessonreview()
