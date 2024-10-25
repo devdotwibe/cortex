81b660a695d8e7b7d64206d285eb80c69a03006e
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-10-25 21:20:13 +0530

    up

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -14,6 +14,7 @@
                 @if(!empty($TermsAndConditions->description))
 
                 {!! $TermsAndConditions->description !!}
+                
                 @endif
             </div>
 
