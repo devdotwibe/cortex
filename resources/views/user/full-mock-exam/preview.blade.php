6d0c0c97450d542dc425c76472040ae17a55b221
jerindotwibe <jerindotwibe@outlook.com>
2024-10-25 20:16:42 +0530

    qqe

--- resources/views/user/full-mock-exam/preview.blade.php
+++ resources/views/user/full-mock-exam/preview.blade.php
@@ -147,6 +147,7 @@
             $.get(reviewurl || "{{ route('full-mock-exam.preview', $userExamReview->slug) }}", function(res) {
                 $('.pagination-arrow').hide();
                 $('#lesson-footer-pagination').html('')
+                $('#lesson-footer-paginationmobile').html('')
                 const lesseonId = generateRandomId(10);
                 $.each(res.data, function(k, v) {
                     $('#lesson-questionlist-list').html(`
@@ -250,6 +251,48 @@
                         }
                     })
                 }
+
+                
+                if (res.total > 1) {
+                    $.each(res.links, function(k, v) {
+                        let linkstatus = "";
+                        if (k != 0 && k != res.links.length && useranswers[k - 1]) {
+                            linkstatus = 'status-bad';
+                            if (useranswers[k - 1].iscorrect) {
+
+
+                                linkstatus = "status-good";
+
+
+                                if (useranswers[k - 1].time_taken < {{ $examtime }}) {
+                                    linkstatus = "status-exelent";
+                                }
+                            }
+                        }
+                        if (v.active || !v.url) {
+
+                            var label_name = v.label;
+
+                            if (v.label == '« Previous') {
+                                var label_name = "<";
+                            }
+
+                            var preclass = "";
+                            if (k == 0) {
+                                preclass = "preclass";
+                            }
+                            $('#lesson-footer-paginationmobile').append(`
+    <button class="${linkstatus} btn btn-secondary  {$preclass} ${v.active?"active":""}" disabled   >${label_name}</button>
+`)
+                        } else {
+                            $('#lesson-footer-paginationmobile').append(`
+    <button class="${linkstatus} btn btn-secondary " onclick="loadlessonreview('${v.url}')" >${v.label}</button>
+`)
+                        }
+
+                    })
+                }
+
                 $('.lesson-end').show();
 
 
