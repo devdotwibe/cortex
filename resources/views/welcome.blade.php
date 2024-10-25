4c445b5d8956486d050528cf84713faf56ce06af
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-10-25 21:23:27 +0530

    update last 25

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,6 @@
         <div class="container">
             <div class="banner-row">
                 <div class="banner-col1">
-                    <h1>hoemn</h1>
                     @if (!empty($banner->title))
                         <h3 class="highlight">{{ $banner->title }}</h3>
                     @endif
