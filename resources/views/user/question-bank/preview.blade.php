9bddf0f5bda1441c9f0a6432dc34e68f45b48c03
jerindotwibe <jerindotwibe@outlook.com>
2024-10-25 14:48:28 +0530

    eeq

--- resources/views/user/question-bank/preview.blade.php
+++ resources/views/user/question-bank/preview.blade.php
@@ -26,19 +26,22 @@
                     </div>
                 </div>
 
-                {{-- <div class="question-header question-number">
+                <div class="question-header question-number">
                     <div class="progress-menus">
                         <div class="menu-text">
                             <span id="menu-text">Question <span> 0 </span> <span>0 </span> </span>
 
                         </div>
-                        <div class="menu-icon">
-                            <a onclick="toglepreviewpage()">
-                                <img src="{{asset("assets/images/menu.svg")}}" alt="exiticon">
-                            </a>
-                        </div>
+                        
                     </div>
-                </div> --}}
+                </div>
+
+
+                <div class="menu-icon">
+                    <a onclick="toglepreviewpage()">
+                        <img src="{{asset("assets/images/menu.svg")}}" alt="exiticon">
+                    </a>
+                </div>
 
                 <div class="Review-mode">
                     <span>Review Mode </span>
