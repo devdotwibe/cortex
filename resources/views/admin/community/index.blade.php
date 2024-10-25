6504e1383097c94b6ad9464c19509ea6fd0072c3
jerindotwibe <jerindotwibe@outlook.com>
2024-10-25 18:06:03 +0530

    5fgh

--- resources/views/admin/community/index.blade.php
+++ resources/views/admin/community/index.blade.php
@@ -61,7 +61,7 @@
                 <form id="searchForm" action="">
                     <div class="text-field">
                         <input type="search" id="searchInput" placeholder="Search for Posts" aria-label="Search for Posts" oninput="performSearch()">
-                        <button type="submit" class="search-btn" onclick="toggleSearchResults()"><img src="{{ asset('assets/images/searc-icon.svg') }}" alt=""></button>
+                        <button type="button" class="search-btn" onclick="toggleSearchResults()"><img src="{{ asset('assets/images/searc-icon.svg') }}" alt=""></button>
                     </div>
                 </form>
                 <div class="searchclass">
