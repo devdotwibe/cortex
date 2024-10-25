696ad0ea928de1ca8c94a9353cb01c4bfce98467
Akshay dotwibe <akshaydotwibe@outlook.com>
2024-10-25 15:40:29 +0530

    sdsd

--- resources/views/admin/live-class/private-class.blade.php
+++ resources/views/admin/live-class/private-class.blade.php
@@ -49,7 +49,7 @@
                                 </div>
                             </div>
 
-                            <div class="category" id="category-content-class-detail">
+                            <div class="category1" id="category-content-class-detail">
 
                             </div>
 
