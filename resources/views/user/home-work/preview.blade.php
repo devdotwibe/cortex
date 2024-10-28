f0e6827b8fb24412e58427e3624d216a62e8a87d
jerindotwibe <jerindotwibe@outlook.com>
2024-10-28 12:30:11 +0530

    wt

--- resources/views/user/home-work/preview.blade.php
+++ resources/views/user/home-work/preview.blade.php
@@ -231,7 +231,7 @@
 
 
                         let linkstatuss = "";
-                        if (k != 0 && k != res.links.length) {
+                        if (k != 0 && k != res.links.length && useranswers[k - 1]) {
 
                             linkstatuss = 'mob-view';
                         }
