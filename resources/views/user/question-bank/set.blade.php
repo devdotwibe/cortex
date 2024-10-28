318b29beb0562c2a9070ad0e1be892f8e6735656
jerindotwibe <jerindotwibe@outlook.com>
2024-10-28 14:41:03 +0530

    e

--- resources/views/user/question-bank/set.blade.php
+++ resources/views/user/question-bank/set.blade.php
@@ -298,21 +298,19 @@
 
     <script> 
 
-        function HideTime() {
-            const timerDiv = $('#exam_timer');
-            const button = $('#hide_button');
+function HideTime() {
+        const timerDiv = $('#exam_timer');
+        const button = $('#hide_button');
 
-            timerDiv.slideToggle(300, function() {
-
-                // if (timerDiv.is(':visible')) {
-                //     button.text('Hide Time');
-                // } else {
-                //     button.text('Show Time');
-                //     button.insertAfter(timerDiv);
-                // }
-            });
-        }
-
+        timerDiv.slideToggle(300, function() {
+            if (timerDiv.is(':visible')) {
+                button.html('Hide Time');
+            } else {
+                button.html('<img src="{{ asset("assets/images/flat-color-icons_clock.svg") }}" alt="Show Time Icon">');
+                button.insertAfter(timerDiv);
+            }
+        });
+    }
          
         var totalcount={{$questioncount??0}};
         var questionids=[];
