4a571d656439d7b3d09000c1fd1b6b71ae6b18ae
jerindotwibe <jerindotwibe@outlook.com>
2024-10-28 14:45:49 +0530

    Hidet

--- resources/views/user/full-mock-exam/show.blade.php
+++ resources/views/user/full-mock-exam/show.blade.php
@@ -30,7 +30,7 @@
                     </div> 
 
                     
-                    <button class="btn hide-btn" id="hide_button" onclick="HideTime()"><img src="{{asset("assets/images/flat-color-icons_clock.svg")}}"></button>
+                    <button class="btn hide-btn" id="hide_button" onclick="HideTime()">Hide Time</button>
 
                 </div>
             </div>
