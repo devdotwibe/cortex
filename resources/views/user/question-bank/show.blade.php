c6086c1bdf53ef2fe92b800aa0d293c49f47e5b3
jerindotwibe <jerindotwibe@outlook.com>
2024-10-29 18:07:43 +0530

    yyw

--- resources/views/user/question-bank/show.blade.php
+++ resources/views/user/question-bank/show.blade.php
@@ -3,6 +3,9 @@
 @section('content')
 <section class="container set-container">
     <div class="container-wrap">
+        <div class="header_title">
+            <h2>Learn</h2>
+        </div>
         <div class="lesson">                  
             <a class="lesson-exit float-start" href="{{route('question-bank.index')}}"  title="Exit" data-title="Exit" aria-label="Exit" data-toggle="tooltip">
                 <img src="{{asset("assets/images/exiticon.svg")}}" alt="exiticon">
