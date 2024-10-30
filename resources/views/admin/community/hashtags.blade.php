38898afe3d5c1bb2d5f7bd70ec5cfd8f80f64370
jerindotwibe <jerindotwibe@outlook.com>
2024-10-30 11:43:47 +0530

    yy5

--- resources/views/admin/community/hashtags.blade.php
+++ resources/views/admin/community/hashtags.blade.php
@@ -53,20 +53,7 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @foreach ($hashtags as $index => $hashtag)
-                                        <tr>
-                                            <td>{{ $index + 1 }}</td>
-                                            <td class="limit-text">{{ $hashtag->name }}</td>
-                                            <td>
-                                                <a href="{{ route('admin.community.hashtags.edit', $hashtag->id) }}" class="btn btn-warning">Edit</a>
-                                                <form action="{{ route('admin.community.hashtags.destroy', $hashtag->id) }}" method="POST" style="display:inline;">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger">Delete</button>
-                                                </form>
-                                            </td>
-                                        </tr>
-                                    @endforeach
+                                   
                                 </tbody>
                             </table>
                         </div>
