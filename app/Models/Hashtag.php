38c3ac9dd19aee61d257eb4f03ee6be7db6e62f3
jerindotwibe <jerindotwibe@outlook.com>
2024-11-01 16:13:49 +0530

    eee

--- app/Models/Hashtag.php
+++ app/Models/Hashtag.php
@@ -13,7 +13,7 @@ class Hashtag extends Model
     use HasFactory;
     protected $table = 'hashtags';
 
-    // protected $fillable = ['hashtag', 'post_id'];
+    protected $fillable = ['hashtag'];
 
     public function post()
     {
