d055d6851f49ab2ee789d5207eb7c7b8f530beae
jerindotwibe <jerindotwibe@outlook.com>
2024-11-01 12:00:11 +0530

    yy2

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -423,7 +423,7 @@ jQuery(document).on("change", "#file_upload", function() {
 
                          setTimeout(function() {
                             location.reload();
-                         }, 60000); 
+                         }, 10000); 
         
        
                         usertablefilter();
