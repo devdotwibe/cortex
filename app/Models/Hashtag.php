deac395e48a96f29337488ed12073d493b66ecef
jerindotwibe <jerindotwibe@outlook.com>
2024-11-01 15:47:25 +0530

    226

--- app/Models/Hashtag.php
+++ app/Models/Hashtag.php
@@ -11,6 +11,7 @@ use Illuminate\Database\Eloquent\Model;
 class Hashtag extends Model
 {
     use HasFactory;
+    protected $table = 'hashtags';
 
     protected $fillable = ['hashtag', 'post_id'];
 
