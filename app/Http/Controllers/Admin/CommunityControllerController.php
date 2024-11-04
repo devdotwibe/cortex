92f4c4fa64a1bfa36be239aed90d58f3ddf5321c
jerindotwibe <jerindotwibe@outlook.com>
2024-11-04 18:12:56 +0530

    wwe

--- app/Http/Controllers/Admin/CommunityControllerController.php
+++ app/Http/Controllers/Admin/CommunityControllerController.php
@@ -64,7 +64,7 @@ class CommunityControllerController extends Controller
                     "title"=>$row->title,
                     "type"=>$row->type,
                     "description"=>$row->description,
-                   "hashtags"=>$row->hashtaglist->hashtag,
+                   "hashtags"=>$row->hashtaglist()->pluck('hashtag'),
                     "likes"=>$row->likes()->count(),
                     "comments"=>$row->comments()->whereNull('post_comment_id')->count(),
                     "image"=>$row->image,
