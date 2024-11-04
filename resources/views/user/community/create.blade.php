01649a90fd25244ae4181532f1a0d0f04d9d6537
jerindotwibe <jerindotwibe@outlook.com>
2024-11-04 11:59:40 +0530

    ww

--- resources/views/user/community/create.blade.php
+++ resources/views/user/community/create.blade.php
@@ -80,7 +80,7 @@
 
                             <div class="form-group">
                                 <label for="hashtag-select">Select Hashtag</label>
-                                <select id="hashtag-select" name="hashtag_id" class="form-control" required>
+                                <select id="hashtag-select" name="hashtag" class="form-control" required>
                                     <option value="">Select a hashtag</option>
                                     @foreach($hashtags as $hashtag)
                                         <option value="{{ $hashtag->id }}">{{ $hashtag->hashtag }}</option>
