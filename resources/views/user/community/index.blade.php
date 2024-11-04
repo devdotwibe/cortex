3a0fcc57c33b62aaf65b4fc9e8fd6d74b4ff3b13
jerindotwibe <jerindotwibe@outlook.com>
2024-11-04 17:49:34 +0530

    eeeY

--- resources/views/user/community/index.blade.php
+++ resources/views/user/community/index.blade.php
@@ -233,7 +233,7 @@
                         <div class="post-content">
                             ${v.description||""}
                         </div>
-                         <div class="post-content">
+                         <div class="post-content 22">
                             ${v.hashtags||""}
                         </div>
                         <div class="poll-options">
@@ -320,7 +320,7 @@
             <div class="post-title">
                 ${v.title||""}
             </div>
-            <div class="post-content">
+            <div class="post-content 25">
                 ${v.description||""}
             </div>
              <div class="post-content">
