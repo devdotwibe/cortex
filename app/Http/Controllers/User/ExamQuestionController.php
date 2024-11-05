5faf35d1ee69337bc973436627a4aa2324504b9b
jerindotwibe <jerindotwibe@outlook.com>
2024-11-05 18:41:04 +0530

    44

--- app/Http/Controllers/User/ExamQuestionController.php
+++ app/Http/Controllers/User/ExamQuestionController.php
@@ -246,7 +246,6 @@ class ExamQuestionController extends Controller
                 return UserReviewAnswer::where('user_review_question_id',$question->id)->get();
             }
             $page_count = UserReviewQuestion::whereIn('review_type',['mcq'])->where('user_id',$user->id)->where('user_exam_review_id',$userExamReview->id)->count();
-dd( $page_count);
             return UserReviewQuestion::whereIn('review_type',['mcq'])->where('user_id',$user->id)->where('user_exam_review_id',$userExamReview->id)->paginate(100);
         }
         $useranswer=UserReviewQuestion::leftJoin('user_review_answers','user_review_answers.user_review_question_id','user_review_questions.id')
