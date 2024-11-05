22a6e610d8da77746aee0f6cd0ec6ea81d63b8dd
jerindotwibe <jerindotwibe@outlook.com>
2024-11-05 16:51:41 +0530

    rrr

--- app/Http/Controllers/Admin/QuestionBankController.php
+++ app/Http/Controllers/Admin/QuestionBankController.php
@@ -230,7 +230,7 @@ class QuestionBankController extends Controller
             if ($request->ajax()) {
                 return response()->json(["success" => "Questions update success1"]);
             }
-            return redirect()->route('admin.question-bank.show')->with("success", "Questions update success");
+            return redirect()->route('admin.question-bank.show')->with("success", "Questions update success2");
         }
     }
 
