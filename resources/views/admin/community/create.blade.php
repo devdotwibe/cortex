05a4ba82aec8f70855567cf40024e33abd3e8947
jerindotwibe <jerindotwibe@outlook.com>
2024-11-06 11:14:15 +0530

    e

--- resources/views/admin/community/create.blade.php
+++ resources/views/admin/community/create.blade.php
@@ -89,14 +89,14 @@
                                 </select>
                             </div> --}}
 
-                            <div class="form-group">
+                            {{-- <div class="form-group">
                                 <label for="hashtag-select">Select Hashtags</label>
                                 <select id="hashtag-select" name="hashtag[]" class="form-control" multiple>
                                     @foreach($hashtags as $hashtag)
                                         <option value="{{ $hashtag->id }}">{{ $hashtag->hashtag }}</option>
                                     @endforeach
                                 </select>
-                            </div>
+                            </div> --}}
                             
 
 
