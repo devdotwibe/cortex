f1fc7da60cfef2eb8088bc24e9fde85305ec1e7d
jerindotwibe <jerindotwibe@outlook.com>
2024-11-06 11:47:13 +0530

    eee

--- app/Http/Controllers/User/CommunityController.php
+++ app/Http/Controllers/User/CommunityController.php
@@ -248,7 +248,7 @@ class CommunityController extends Controller
 
          // Attach selected hashtags to the post
     if ($request->has('hashtags')) {
-        $post->hashtags()->sync($request->hashtags); // Using many-to-many relationship
+        $post->hashtag()->sync($request->hashtags); // Using many-to-many relationship
     }
 
 
