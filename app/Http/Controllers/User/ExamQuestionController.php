e079edf23c363e4c295ad5f19ec6b70ca43b6420
jerindotwibe <jerindotwibe@outlook.com>
2024-11-07 18:15:57 +0530

    444ee

--- app/Http/Controllers/User/ExamQuestionController.php
+++ app/Http/Controllers/User/ExamQuestionController.php
@@ -71,7 +71,7 @@ class ExamQuestionController extends Controller
                 $qry->where('exam_id',$exam->id);
             });
         })
-        ->orderBy('time_of_exam', 'desc')
+        ->orderBy('created_at', 'desc')
         ->get();
 
         /**
