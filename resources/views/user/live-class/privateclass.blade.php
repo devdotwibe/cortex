cbcadce282b4c81bf6a5749fcaec81e0e1e80e3f
jerindotwibe <jerindotwibe@outlook.com>
2024-11-07 20:45:22 +0530

    yyu

--- resources/views/user/live-class/privateclass.blade.php
+++ resources/views/user/live-class/privateclass.blade.php
@@ -40,7 +40,7 @@
                 <div class="col-md-6">
                     <div class="card mb-3">
                         <div class="card-body">
-                            <a href="{{ route('live-class.privateclass.lesson', $user->slug) }}">
+                            <a>
                                 <div class="category">
                                     <div class="category-image">
                                         <img src="{{ asset('assets/images/lessonmaterial.svg') }}">
--- routes/web.php
+++ routes/web.php
@@ -296,7 +296,7 @@ Route::middleware(['auth', 'isUser'])->group(function () {
                     // Route::get('/{live}/private-class/details', [LiveClassController::class, 'privateclassdetails'])->name('privateclass.details');
                     Route::get('/{live}/private-class/{class_detail}/term', [LiveClassController::class, 'privateclassterm'])->name('privateclass.term');
 
-                    Route::get('/{live}/private-class/lesson', [LiveClassController::class, 'privateclasslesson'])->name('privateclass.lesson');
+                    // Route::get('/{live}/private-class/lesson', [LiveClassController::class, 'privateclasslesson'])->name('privateclass.lesson');
                     Route::get('/{live}/private-class/lesson/{lesson_material}/show', [LiveClassController::class, 'privateclasslessonshow'])->name('privateclass.lessonshow');
 
                     Route::get('/{live}/private-class/lesson/{sub_lesson_material}.pdf', [LiveClassController::class, 'privateclasslessonpdf'])->name('privateclass.lessonpdf');
