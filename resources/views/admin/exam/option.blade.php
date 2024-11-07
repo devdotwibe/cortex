c603e585ba044cc8db6051535b5d9b80a452b43b
jerindotwibe <jerindotwibe@outlook.com>
2024-11-07 16:29:10 +0530

    44r

--- resources/views/admin/exam/option.blade.php
+++ resources/views/admin/exam/option.blade.php
@@ -12,7 +12,9 @@
 <section class="invite-wrap mt-2">
 
     <div class="container">
+        <div class="header_title">
         <h2>Topic Test </h2>
+    </div> 
         <x-general-form :url="route('admin.exam.options')"   btnsubmit="Save" :fields='[ 
             ["name"=>"title","label"=>"Title","placeholder"=>"Title","size"=>12,"type"=>"text" ,"value"=>get_option("exam_simulator_title") ],
              ["name"=>"description","label"=>"Description","placeholder"=>"Description","size"=>12,"type"=>"editor" ,"value"=>get_option("exam_simulator_description") ],
