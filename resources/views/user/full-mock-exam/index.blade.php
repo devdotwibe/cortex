b0c21f0ab69395da972506481817fe9c89ed271a
jerindotwibe <jerindotwibe@outlook.com>
2024-11-08 12:52:45 +0530

    664

--- resources/views/user/full-mock-exam/index.blade.php
+++ resources/views/user/full-mock-exam/index.blade.php
@@ -169,7 +169,7 @@
                 url:url
             },
             order: [
-                [2, 'ASC']
+                [2, 'DESC']
             ],
             initComplete: function() {
                 var info = this.api().page.info(); 
