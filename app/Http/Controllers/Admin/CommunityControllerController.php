1460bc408ab267b3b43915ea2ae3a04fd5cfcd81
jerindotwibe <jerindotwibe@outlook.com>
2024-11-08 15:48:11 +0530

    rr3

--- app/Http/Controllers/Admin/CommunityControllerController.php
+++ app/Http/Controllers/Admin/CommunityControllerController.php
@@ -338,8 +338,8 @@ class CommunityControllerController extends Controller
 
         // Update or associate the selected hashtag with the post
         if ($request->filled('hashtag')) {
-            Hashtag::where('post_id', $post->id)->update(['post_id' => null]); // Remove previous association
-            $hashtag = Hashtag::find($request->hashtag);
+            Hashtagstore::where('post_id', $post->id)->update(['post_id' => null]); // Remove previous association
+            $hashtag = Hashtagstore::find($request->hashtag);
             $hashtag->post_id = $post->id;
             $hashtag->save();
         }
