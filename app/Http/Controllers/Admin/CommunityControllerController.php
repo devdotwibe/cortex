9b5c21347afd445b0a98f76c3b2903a0272fa749
jerindotwibe <jerindotwibe@outlook.com>
2024-11-08 14:41:18 +0530

    ee

--- app/Http/Controllers/Admin/CommunityControllerController.php
+++ app/Http/Controllers/Admin/CommunityControllerController.php
@@ -10,6 +10,7 @@ use App\Models\Poll;
 use App\Models\Hashtag;
 use App\Models\AdminPost;
 use App\Models\AdminPoll;
+use App\Models\Hashtagstore;
 use App\Models\PollOption;
 use App\Models\Post;
 use App\Models\PostComment;
@@ -168,14 +169,25 @@ class CommunityControllerController extends Controller
         // }
 
         // Attempt to find the hashtag and associate it with the post
-    if ($request->has('hashtag') && $hashtag = Hashtag::find($request->hashtag)) {
-        $hashtag->post_id = $post->id;
-        $hashtag->save();
-    } else {
-        // Handle case if the hashtag was not found, if needed
-        // Example: log an error or ignore
-    }
+    // if ($request->has('hashtag') && $hashtag = Hashtag::find($request->hashtag)) {
+    //     $hashtag->post_id = $post->id;
+    //     $hashtag->save();
+    // } else {
+    //     // Handle case if the hashtag was not found, if needed
+    //     // Example: log an error or ignore
+    // }
 
+
+
+    $extractedHashtags = $request->input('hashtags',[]);
+
+    // dd($extractedHashtags);
+    foreach ($extractedHashtags as $hashtag) {
+        if (!empty($hashtag)) {
+            $hash_value = Hashtagstore::find($hashtag);
+            Hashtag::firstOrCreate(['hashtag'=>$hash_value->hashtag,'hashtagstore_id' => $hashtag, 'post_id' => $post->id]);
+        }
+    }
         return redirect()->route('admin.community.index')->with('success', "Post published");
     }
 
