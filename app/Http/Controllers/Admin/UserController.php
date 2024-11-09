ac4f217d3f0f570abcb13c555d8009c00ceaffdf
jerindotwibe <jerindotwibe@outlook.com>
2024-11-09 10:30:46 +0530

    ww5

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -333,7 +333,7 @@ class UserController extends Controller
         $request->validate([
             'first_name' => 'required|string|max:255', 
          
-        'email' => 'required|max:255',       
+         'email' => 'required|email|max:255',
         'expiry_date' => 'required|date',  
          
             
