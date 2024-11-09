3f4b355cc232e6be64e5d63932246e566b4fde7e
jerindotwibe <jerindotwibe@outlook.com>
2024-11-09 16:51:10 +0530

    444

--- resources/views/user/full-mock-exam/preview.blade.php
+++ resources/views/user/full-mock-exam/preview.blade.php
@@ -263,7 +263,7 @@
                         }
                         if (v.active || !v.url) {
                             var preclass = "";
-                            if (k == 0  && k == res.links.length-1) {
+                            if (k == 0 ) {
                                 preclass = "preclass";
                             }
                             $('#lesson-footer-pagination').append(`
