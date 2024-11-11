f36050ee696740ba84c2c24b18d7e09febb63826
jerindotwibe <jerindotwibe@outlook.com>
2024-11-11 19:19:08 +0530

    tt

--- app/Http/Controllers/Admin/SubFaqController.php
+++ app/Http/Controllers/Admin/SubFaqController.php
@@ -87,7 +87,7 @@ class SubFaqController extends Controller
         $faq->save();
     
         
-        return redirect()->back()->with(["success"=>"Faq added success","modal_id"=>"sub-category-modal-content"]);
+        return redirect()->route('admin.faq.index')->with(["success"=>"Faq added success","modal_id"=>"sub-category-modal-content"]);
         
     }
     
