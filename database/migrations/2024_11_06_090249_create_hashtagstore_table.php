a4ba8bb613c537c885008d7bb42b5876747fdb42
jerindotwibe <jerindotwibe@outlook.com>
2024-11-11 12:47:43 +0530

    ee

--- database/migrations/2024_11_06_090249_create_hashtagstore_table.php
+++ database/migrations/2024_11_06_090249_create_hashtagstore_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('hashtagstore', function (Blueprint $table) {
             $table->id(); 
-            $table->string('name')->nullable(); 
+            $table->string('hashtag')->nullable(); 
             $table->timestamps(); 
         });
     }
