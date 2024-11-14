d0f37ba441e6328c8f963349d0af65b3ad8525ae
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-11-14 11:09:41 +0530

    update code

--- app/Http/Controllers/Admin/AdminUserController.php
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -24,8 +24,6 @@ class AdminUserController extends Controller
     public function index (Request $request)
     {
         if($request->ajax()){
-
-            self::$defaultActions=["edit","delete"]; 
             return $this->addAction(function($data){
                 return '
                
