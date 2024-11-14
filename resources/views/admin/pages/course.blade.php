ab9769862b50acac92379e43219b5df105832ffa
jerindotwibe <jerindotwibe@outlook.com>
2024-11-14 20:22:18 +0530

    up

--- resources/views/admin/pages/course.blade.php
+++ resources/views/admin/pages/course.blade.php
@@ -150,9 +150,13 @@
                                     
                                         <div class="form-group imgid1" id="imgid1">
                                             <label for="imagePreview">Image Preview</label>
-                                            <div id="imagePreview" class="numericalclass"
-                                            style="border: 1px solid #ddd; padding: 10px; width: 132px; height: 150px; position: relative;">
-                                                    
+                                            <div id="imagePreviewContainer" class="numericalclass">
+                                                @if (isset($course) && $course->image)
+                                                    <!-- Display saved image and delete button -->
+                                                    <img id="imagePreview-save" src="{{ url('d0/' . $course->image) }}" alt="Image Preview"
+                                                        style="width: 100%; height: auto;">
+                                                    <!-- Delete button for saved image -->
+                                                    {{-- <button type="button" class="btn btn-danger" id="icondelete1" onclick="removeImage()"  style="position: absolute; top: 5px; right: 5px; {{ isset($course) && $course->image ? 'display: block;' : 'display: none;' }}">X</button> --}}
 
 
 
@@ -179,7 +183,7 @@
 
 
 
-                                               
+                                                @endif
                                     
                                               <!-- Dynamic image preview -->
                                               <img id="imagePreview" src="#" alt="Image Preview" style="display: none; width: 100%; height: auto;">
