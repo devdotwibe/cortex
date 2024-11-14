5d966ec46a504f518edb25516bad0e935f9affcb
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-11-14 11:59:53 +0530

    update code

--- resources/views/admin/admin_users/index.blade.php
+++ resources/views/admin/admin_users/index.blade.php
@@ -150,7 +150,7 @@
                            </div>
 
                             <div class="col-md-4 pt-4">  
-                                <button type="submit" class="btn btn-dark" id="table-subcategory-form-submit"> Add + </button>  
+                                <button type="button" class="btn btn-dark" id="table-subcategory-form-submit"> Save </button>  
                                 <button type="button" class="btn btn-secondary" style="display: none" id="table-subcategory-form-clear" >Cancel</button>               
                             </div>
                         </div> 
