719dfe2781c14583bf5f71befb3ee63a9949d0ce
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-11-14 16:21:14 +0530

    update permission code

--- app/Http/Middleware/AdminPermission.php
+++ app/Http/Middleware/AdminPermission.php
@@ -20,14 +20,90 @@ class AdminPermission
          * @var Admin
          */
 
-         dd($opt);
-
         $admin = Auth::guard('admin')->user();
 
-         if ($admin->role !='master') {
+        if ($admin->role !='master') {
           
+            $admin_permission = AdminPermission::where('admin_id',$admin->id)->first();
+
+            if($admin_permission)
+            {
+                switch ($opt) {
+
+                    case 'users':
+                       
+                        if ($admin_permission->users != 'Y') {
+
+                            return $next($request);
+                        }
+                        break;
+                    
+                    case 'learn':
+                     
+                        if ($admin_permission->learn != 'Y') {
+
+                            return $next($request);
+                        }
+                        break;
+        
+                    case 'options':
+                     
+                        if ($admin_permission->options != 'Y') {
+
+                            return $next($request);
+                        }
+                        break;
+
+                    case 'question_bank':
+                    
+                        if ($admin_permission->question_bank != 'Y') {
+
+                            return $next($request);
+                        }
+                        break;
+                    
+                    case 'exam_simulator':
+                
+                        if ($admin_permission->exam_simulator != 'Y') {
+
+                            return $next($request);
+                        }
+                        break;
+
+                    case 'live_teaching':
+                
+                        if ($admin_permission->live_teaching != 'Y') {
+
+                            return $next($request);
+                        }
+                        break;
+
+                    case 'community':
+                
+                        if ($admin_permission->community != 'Y') {
+
+                            return $next($request);
+                        }
+                        break;
+
+                    case 'pages':
+            
+                        if ($admin_permission->pages != 'Y') {
+
+                            return $next($request);
+                        }
+                        break;
+        
+                    default:
+                      
+                        return redirect()->back();
+                }
+            }
+            else
+            {
+                return redirect()->back();
+            }
           
-            return $next($request);
 
         } else {
           
