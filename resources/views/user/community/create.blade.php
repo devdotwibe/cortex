99ddc4d9bac81c837d7ed14b2bd4e467234a824c
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-11-15 19:10:23 +0530

    update code

--- resources/views/user/community/create.blade.php
+++ resources/views/user/community/create.blade.php
@@ -92,7 +92,7 @@
 
                             <div class="form-group">
                                 <label for="hashtag-select">Select Channel</label>
-                                <select id="hashtag-select" name="hashtags[]" class="form-control">
+                                <select id="hashtag-select" name="hashtags" class="form-control">
                                     @foreach($hashtags as $hashtag)
                                         <option value="{{ $hashtag->id }}">{{ $hashtag->hashtag }}</option>
                                     @endforeach
