266637161b5c39d29f0da43b55445cee4e150d5d
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-11-18 14:02:16 +0530

    update code

--- resources/views/user/question-bank/preview.blade.php
+++ resources/views/user/question-bank/preview.blade.php
@@ -441,11 +441,11 @@
                                 preclass = "preclass";
                             }
                             $('#lesson-footer-paginationmobile').append(`
-                                <button class="${linkstatus} btn btn-secondary  ${preclass} ${v.active?"active":""}" disabled   >${label_name}</button>
+                                <button class="${linkstatus} btn btn-secondary ${useranswers[k - 1].time_taken}  ${preclass} ${v.active?"active":""}" disabled   >${label_name}</button>
                             `)
                         } else {
                             $('#lesson-footer-paginationmobile').append(`
-                                <button class="${linkstatus} btn btn-secondary " onclick="loadlessonreview('${v.url}')" >${v.label}</button>
+                                <button class="${linkstatus} btn btn-secondary ${useranswers[k - 1].time_taken} " onclick="loadlessonreview('${v.url}')" >${v.label}</button>
                             `)
                         }
 
