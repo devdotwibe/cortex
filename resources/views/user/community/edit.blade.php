ee2a9b05aa43d7c36f558f226d8f447e77bd6f7a
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-11-18 11:22:39 +0530

    update code

--- resources/views/user/community/edit.blade.php
+++ resources/views/user/community/edit.blade.php
@@ -80,7 +80,7 @@
                             </div> --}}
                             <div class="form-group">
                                 <label for="hashtag-select">Select Hashtag</label>
-                                <select id="hashtag-select" name="hashtag[]" class="form-control" multiple>
+                                <select  name="hashtag[]" class="form-control">
                                     <option value="">Select a hashtag</option>
                                     @foreach($hashtags as $hashtag)
                                         <option value="{{ $hashtag->id }}" 
