8cdd4483becd8481dd4508bef038f8b6baacca43
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-11-19 14:26:36 +0530

    update search

--- app/Http/Controllers/Admin/PaymentController.php
+++ app/Http/Controllers/Admin/PaymentController.php
@@ -19,12 +19,14 @@ class PaymentController extends Controller
             self::$model=PaymentTransation::class;
             self::$defaultActions=[""];
 
-            $search = $request->search['value'];
+            if(!empty($request->search['value']))
+            {
+                $search = $request->search['value'];
+                self::$model->whereHas('user', function($query) use ($search) {
+                    $query->where('name', 'like', "%{$search}%");
+                });
+            }
             
-            self::$model->whereHas('user', function($query) use ($search) {
-                $query->where('name', 'like', "%{$search}%");
-            });
-
             return  $this->with('user')->whereHas('user')->addColumn('username',function($data){
                 return optional($data->user)->name;
             })->buildTable();
