3f6b8987ec82670103cc70e34d687de92bdd0d50
jerindotwibe <jerindotwibe@outlook.com>
2024-11-20 16:45:07 +0530

    up

--- resources/views/user/live-class/workshopform.blade.php
+++ resources/views/user/live-class/workshopform.blade.php
@@ -5,7 +5,7 @@
     <div class="header_wrapp">
         <div class="header_title">
             <div class="back-btn" id="back-btn" style="display: block"> <!-- Ensure proper display value -->
-                <a href="{{ route('live-class.workshop',$live_class->live) }}">
+                <a href="{{ route('live-class.workshop',$live_class->slug) }}">
                     <img src="{{ asset('assets/images/leftarrowblack.svg') }}" alt="">
                 </a>
             </div>
