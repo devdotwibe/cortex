257eed2b77e3ef39298194b4ada74ac86316ba4f
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-11-21 12:19:17 +0530

    update code

--- resources/views/admin/home-work/create.blade.php
+++ resources/views/admin/home-work/create.blade.php
@@ -12,7 +12,9 @@
 
 <section class="invite-wrap mt-2">
     <div class="container">
-        <x-create-form 
+
+
+        {{-- <x-create-form 
             name="admin.home-work" 
             :params='[
                 "home_work"=>$homeWork->slug
@@ -27,7 +29,30 @@
                 ["name"=>"answer","label"=>"answer" ,"type"=>"choice" ,"size"=>12],
                 ["name"=>"explanation","label"=>"Explanation","size"=>12,"type"=>"editor" ],
             ]' 
-        />  
+        />   --}}
+
+        <x-create-form name="admin.learn" :params="[$category->slug]" :cancel="route('admin.learn.show',$category->slug)" frmID="learnForm" btnsubmit="Save" :fields='[
+            ["name"=>"category_id", "value"=>$category->id,"type"=>"hidden"],
+            ["name"=>"redirect", "value"=>route("admin.learn.show",$category->slug),"type"=>"hidden"],
+            ["name"=>"sub_category_id" ,"label"=>"Sub Category","ajaxurl"=>route("admin.learn.show",$category->slug),"type"=>"select","child"=>"sub_category_set","size"=>4],
+             ["name"=>"title", "placeholder"=>"Title","label"=>"Title","size"=>4,"type"=>"text"], 
+            ["name"=>"learn_type","event"=>["change"=>"cclickback"] ,"label"=>"Learn Type","placeholder"=>"Select Learn Type","type"=>"select","size"=>4,"options"=>[["value"=>"video","text"=>"Video"],["value"=>"notes","text"=>"Note"],["value"=>"short_notes","text"=>"Short Note Questions"],["value"=>"mcq","text"=>"MCQs Questions"]]],
+             
+            ["name"=>"video_url", "addclass"=>"video_section" ,"display"=>"none" , "placeholder"=>"Video url","label"=>"Vimeo Video","size"=>12,"type"=>"text"], 
+           
+            ["name"=>"mcq_question", "addclass"=>"mcq_section","display"=>"none" , "label"=>"Question","size"=>12,"type"=>"editor"], 
+            ["name"=>"mcq_answer", "addclass"=>"mcq_section" , "display"=>"none", "label"=>"answer" ,"type"=>"choice" ,"size"=>6],
+            ["name"=>"explanation", "addclass"=>"mcq_section","display"=>"none" , "label"=>"Explanation","size"=>12,"type"=>"editor" ],
+             ["name"=>"short_question", "addclass"=>"short_section","display"=>"none" , "label"=>"Question","size"=>12,"type"=>"editor"],
+
+            ["name"=>"short_answer", "addclass"=>"short_section" ,"display"=>"none" , "placeholder"=>"Type Answer Here","label"=>"Answer","size"=>12,"type"=>"editor"], 
+
+            ["name"=>"note", "addclass"=>"note_section","display"=>"none" , "label"=>"Note","size"=>12,"type"=>"editor"],
+
+
+        ]'  /> 
+
+
     </div>
 </section> 
 @endsection
