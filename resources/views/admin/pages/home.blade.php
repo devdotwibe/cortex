b3bba8e464986fe331ea7c1f42eb080d41a1fa91
jerindotwibe <jerindotwibe@outlook.com>
2024-11-21 18:21:48 +0530

    up

--- resources/views/admin/pages/home.blade.php
+++ resources/views/admin/pages/home.blade.php
@@ -2620,6 +2620,11 @@
                         <input type="file" name="featureimageupdate[]" onchange="previewFeatureImagefea(event, 'text${featureIndex}')" id="featureimage_text${featureIndex}" class="form-control"  style="display: none;">
                          <div id="preview-container-text${featureIndex}" style="margin-top: 10px;">
                         <img id="preview-image-text${featureIndex}" src="" alt="Image Preview" style="max-width: 100px; display: none;">
+
+                        <!-- Remove Button -->
+    <button type="button" id="deleteicon-text${featureIndex}" class="btn btn-danger" style="display: none; margin-top: 10px;" onclick="removerepimg('text${featureIndex}')">Delete</button>
+
+    
                          </div>
                     </div>
                 </div>
