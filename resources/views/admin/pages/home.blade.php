34d44ddf9d157b668f306602175b76c4ae71213b
jerindotwibe <jerindotwibe@outlook.com>
2024-11-21 18:16:25 +0530

    up

--- resources/views/admin/pages/home.blade.php
+++ resources/views/admin/pages/home.blade.php
@@ -859,15 +859,15 @@
                                                                         onclick="removeImagedelete()">Delete</button> --}}
 
                                                                          <!-- Delete button for preview (before saving) -->
-                                                                    {{-- <button type="button" class="btn btn-danger imgid{{ $item->id }}"
+                                                                    <button type="button" class="btn btn-danger imgid{{ $item->id }}"
                                                                         id="deleteicon-{{ $item->id }}"
                                                                         style="position: absolute; top: 5px; right: 5px; display: none;"
-                                                                        onclick="removerepimg(this)">Delete</button> --}}
+                                                                        onclick="removerepimg(this)">Delete</button>
 
                                                                         <button type="button" class="btn btn-danger imgid{{ $item->id }}"
                                                                             id="deleteicon-{{ $item->id }}"
                                                                             style="position: absolute; top: 5px; right: 5px; display: none;"
-                                                                            onclick="removerepimg(this); hideFeatureImagePreview('{{ $item->id }}')">Delete</button>
+                                                                            onclick="hideFeatureImagePreview('{{ $item->id }}')">Delete</button>
                                                                         
 
 
