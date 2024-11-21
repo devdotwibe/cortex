24a395642975c50bb2de825a4a097b111fe7908c
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-11-21 19:26:23 +0530

    update code

--- resources/views/admin/home-work/edit.blade.php
+++ resources/views/admin/home-work/edit.blade.php
@@ -25,29 +25,14 @@
             ];
             }
         @endphp
-        <x-edit-form 
-            name="admin.home-work" 
-            :id="$homeWorkQuestion->slug"
-            :params='["home_work"=>$homeWork->slug,"home_work_question"=>$homeWorkQuestion->slug]' 
-            :cancel="route('admin.home-work.show',$homeWork->slug)"  
-            btnsubmit="Save" 
-            :fields='[  
-                ["name"=>"redirect", "value"=>route("admin.home-work.show",$homeWork->slug),"type"=>"hidden"],
-                ["name"=>"home_work_book_id" ,"label"=>"Week Booklet","ajaxurl"=>route("admin.home-work.create",$homeWork->slug),"type"=>"select","value"=>$homeWorkQuestion->home_work_book_id,"valuetext"=>optional($homeWorkQuestion->homeWorkBook)->title,"size"=>4],
-                
-                ["name"=>"description","label"=>"Question","size"=>12,"type"=>"editor","value"=>$homeWorkQuestion->description], 
-                ["name"=>"answer","label"=>"answer" ,"type"=>"choice" ,"size"=>12,"value"=>$choices ],
-                ["name"=>"explanation","label"=>"Explanation","size"=>12,"type"=>"editor","value"=>$homeWorkQuestion->explanation ],
-            ]' 
-        />
-        
+      
         <x-edit-form  :id="$homeWorkQuestion->slug" name="admin.home-work" :params='["home_work"=>$homeWork->slug,"home_work_question"=>$homeWorkQuestion->slug]'  :cancel="route('admin.home-work.show',$homeWork->slug)"   frmID="learnForm" btnsubmit="Save" :fields='[
             ["name"=>"redirect", "value"=>route("admin.home-work.show",$homeWork->slug),"type"=>"hidden"],
              ["name"=>"home_work_book_id" ,"label"=>"Week Booklet","ajaxurl"=>route("admin.home-work.create",$homeWork->slug),"type"=>"select","size"=>4, ,"value"=>$homeWorkQuestion->home_work_book_id],
             ["name"=>"home_work_type","event"=>["change"=>"cclickback"] ,"label"=>"Home Work Type","placeholder"=>"Select Home Work Type","type"=>"select","size"=>4,"options"=>[["value"=>"short_notes","text"=>"Short Note Questions"],["value"=>"mcq","text"=>"MCQs Questions"]]],
              
            
-             ["name"=>"description", "addclass"=>"mcq_question","display"=>"block" , "label"=>"Question","size"=>12,"type"=>"editor" "value"=>$homeWorkQuestion->description], 
+             ["name"=>"description", "addclass"=>"mcq_question","display"=>"block" , "label"=>"Question","size"=>12,"type"=>"editor", "value"=>$homeWorkQuestion->description], 
              ["name"=>"answer", "addclass"=>"mcq_question","display"=>"block" ,"label"=>"answer" ,"type"=>"choice" ,"size"=>12 ,"value"=>$choices],
              ["name"=>"explanation", "addclass"=>"mcq_question","display"=>"block" , "label"=>"Explanation","size"=>12,"type"=>"editor" ,"value"=>$homeWorkQuestion->explanation ],
 
