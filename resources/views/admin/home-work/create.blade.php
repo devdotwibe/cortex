a0a6d7992e81c44edaddf3d0800d40c8a4aa6d0e
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-11-21 14:06:11 +0530

    update codde

--- resources/views/admin/home-work/create.blade.php
+++ resources/views/admin/home-work/create.blade.php
@@ -13,24 +13,6 @@
 <section class="invite-wrap mt-2">
     <div class="container">
 
-
-        <x-create-form 
-            name="admin.home-work" 
-            :params='[
-                "home_work"=>$homeWork->slug
-            ]' 
-            :cancel="route('admin.home-work.show',$homeWork->slug)"  
-            btnsubmit="Save" 
-            :fields='[  
-                ["name"=>"redirect", "value"=>route("admin.home-work.show",$homeWork->slug),"type"=>"hidden"],
-                ["name"=>"home_work_book_id" ,"label"=>"Week Booklet","ajaxurl"=>route("admin.home-work.create",$homeWork->slug),"type"=>"select","size"=>4],
-                
-                ["name"=>"description","label"=>"Question","size"=>12,"type"=>"editor"], 
-                ["name"=>"answer","label"=>"answer" ,"type"=>"choice" ,"size"=>12],
-                ["name"=>"explanation","label"=>"Explanation","size"=>12,"type"=>"editor" ],
-            ]' 
-        />  
-
         <x-create-form name="admin.home-work" :params='["home_work"=>$homeWork->slug]'  :cancel="route('admin.home-work.show',$homeWork->slug)" frmID="learnForm" btnsubmit="Save" :fields='[
             ["name"=>"redirect", "value"=>route("admin.home-work.show",$homeWork->slug),"type"=>"hidden"],
              ["name"=>"home_work_book_id" ,"label"=>"Week Booklet","ajaxurl"=>route("admin.home-work.create",$homeWork->slug),"type"=>"select","size"=>4],
