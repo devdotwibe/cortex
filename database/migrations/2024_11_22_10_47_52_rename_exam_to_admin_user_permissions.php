0f7cd6d959b443e9d1c5a1811f631ef965ac369b
ArjunlalDotwibe <arjundotwibe@outlook.com>
2024-11-22 11:46:58 +0530

    update code

---
+++ database/migrations/2024_11_22_10_47_52_rename_exam_to_admin_user_permissions.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('admin_permissions', function (Blueprint $table) {
+            $table->string('topic_exam')->default('N');
+            $table->string('full_mock_exam')->default('N');
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('admins', function (Blueprint $table) {
+            $table->dropColumn('topic_exam');
+            $table->dropColumn('full_mock_exam');
+        });
+    }
+};
