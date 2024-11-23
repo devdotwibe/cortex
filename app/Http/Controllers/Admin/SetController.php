5dd3f603a7b78fc87be65e7bf519d5d1dad80a61
jerindotwibe <jerindotwibe@outlook.com>
2024-11-23 12:32:13 +0530

    up

--- app/Http/Controllers/Admin/SetController.php
+++ app/Http/Controllers/Admin/SetController.php
@@ -30,7 +30,7 @@ class SetController extends Controller
                                     $validTimeFormat = '/^(0[0-9]|1[0-9]|2[0-3]) ?: ?[0-5][0-9]$/';
 
                                     if (!preg_match($validTimeFormat, $value) || $value === '00:00' || $value === '00 : 00') {
-                                        $fail('The time of exam must not be 00:00.');
+                                        $fail('The time of exam must not be 23:59.');
                                     }
                                 },
                             ],
@@ -124,7 +124,7 @@ class SetController extends Controller
                     $validTimeFormat = '/^(0[0-9]|1[0-9]|2[0-3]) ?: ?[0-5][0-9]$/';
 
                     if (!preg_match($validTimeFormat, $value) || $value === '00:00' || $value === '00 : 00') {
-                        $fail('The time of exam must not be 00:00.');
+                        $fail('The time of exam must not be 23:58.');
                     }
                 },
             ],
