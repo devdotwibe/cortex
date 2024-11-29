9044fe4701b861fbd07b8d943d8edabc8dfc4f52
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-11-29 15:55:25 +0530

    update code

--- app/Http/Controllers/Admin/HomeWorkController.php
+++ app/Http/Controllers/Admin/HomeWorkController.php
@@ -108,9 +108,8 @@ class HomeWorkController extends Controller
                     "home_work_book_id" => ['required'],
                     "description" => ['required'],
                     "answer" => ['required'],
-                    "answer.*" => ["required_without:choice_answer_image", 'string', 'max:150', 'nullable'],
-                    "choice_answer_image.*" => ["required_without:file_answer"],
-                    "file_answer.*" => ["required_without:answer", 'image', 'mimes:jpeg,png,jpg,gif', 'max:2048'],
+                    "answer.*" => ["required_without:choice_answer_image,file_answer", 'string', 'max:150', 'nullable'],
+                    "file_answer.*" => ["required_without:answer,choice_answer_image", 'image', 'mimes:jpeg,png,jpg,gif', 'max:2048'],
                     "explanation" => ['nullable']
                 ], [
                     'answer.*.required_without' => 'The answer field is required when file answer is not provided.',
