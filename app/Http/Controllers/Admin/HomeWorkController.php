c3bc0af2ed43016af25f8b550d2f9b24e41cef14
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-11-29 15:58:38 +0530

    update code

--- app/Http/Controllers/Admin/HomeWorkController.php
+++ app/Http/Controllers/Admin/HomeWorkController.php
@@ -108,12 +108,12 @@ class HomeWorkController extends Controller
                     "home_work_book_id" => ['required'],
                     "description" => ['required'],
                     "answer" => ['required'],
-                    "answer.*" => ["required_without:choice_answer_image,file_answer", 'string', 'max:150', 'nullable'],
-                    "file_answer.*" => ["required_without:answer,choice_answer_image", 'image', 'mimes:jpeg,png,jpg,gif', 'max:2048'],
+                   "answer.*" => ["required_without_all:choice_answer_image,file_answer", 'string', 'max:150', 'nullable'],
+                    "file_answer.*" => ["required_without_all:answer,choice_answer_image", 'image', 'mimes:jpeg,png,jpg,gif', 'max:2048'],
                     "explanation" => ['nullable']
                 ], [
-                    'answer.*.required_without' => 'The answer field is required when file answer is not provided.',
-                    'file_answer.*.required_without' => 'The file answer is required when answer is not provided.',
+                    'answer.*.required_without_all' => 'The answer field is required when file answer is not provided.',
+                    'file_answer.*.required_without_all' => 'The file answer is required when answer is not provided.',
                     'file_answer.*.mimes' => 'Each file answer must be an image (jpeg, png, jpg, gif).',
                 ]);
                 break;
