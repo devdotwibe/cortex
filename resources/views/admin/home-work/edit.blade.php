44d66b99e4d3ac96b87dcb637866186c2fa6f106
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-11-29 16:22:15 +0530

    update code

--- resources/views/admin/home-work/edit.blade.php
+++ resources/views/admin/home-work/edit.blade.php
@@ -18,16 +18,6 @@
 <section class="invite-wrap mt-2">
     <div class="container">
 
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-
         @php
             $choices=[];
             if(!empty($homeWorkQuestion->answers) && isset($homeWorkQuestion->answers))
