c2efa49a8aa94db778adc02c52becc8b62281c75
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-12-02 14:24:54 +0530

    update code

--- database/migrations/2024_12_02_063921_orders_to_question.php
+++ database/migrations/2024_12_02_063921_orders_to_question.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('questions', function (Blueprint $table) {
-            $table->string('order')->default('0')->change();
+            $table->string('order')->default('0');
         });
     }
 
