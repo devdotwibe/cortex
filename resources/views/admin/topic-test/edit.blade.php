6363c7519910d4f4812936be620bb7bae5266787
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-12-02 11:49:05 +0530

    update code

--- resources/views/admin/topic-test/edit.blade.php
+++ resources/views/admin/topic-test/edit.blade.php
@@ -26,7 +26,7 @@
             ["name"=>"category_id", "value"=>$category->id,"type"=>"hidden"],
             ["name"=>"redirect", "value"=>route("admin.topic-test.show",$category->slug),"type"=>"hidden"],
             
-           ["name"=>"order" ,"label"=>"Order","ajaxurl"=>route("admin.exam.create",$exam->slug),"type"=>"select","value"=>$exam->order,"valuetext"=>$exam->id,"size"=>4],  
+           ["name"=>"order" ,"label"=>"Order","ajaxurl"=>route("admin.exam.create",$exam->slug),"type"=>"select","value"=>$exam->order,"valuetext"=>$exam->order,"size"=>4],  
 
             ["name"=>"title_text","label"=>"Title Text","size"=>12,"type"=>"editor","value"=>$question->title_text],
             ["name"=>"description","label"=>"Left Question","size"=>12,"type"=>"editor","value"=>$question->description], 
