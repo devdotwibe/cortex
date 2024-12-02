0294c132d7a4536a9ff4903f4c0b616d47b8dcf6
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-12-02 13:46:47 +0530

    update code

--- resources/views/admin/topic-test/show.blade.php
+++ resources/views/admin/topic-test/show.blade.php
@@ -69,7 +69,7 @@
                             </form>
                         </div>
 
-                        <x-ajax-table :bulkaction="true" :order="true" bulkactionlink="{{ route('admin.topic-test.bulkaction', ['category' => $category->slug]) }}"  tableid="categoryquestiontable"  
+                        <x-ajax-table :bulkaction="true" :order="false" bulkactionlink="{{ route('admin.topic-test.bulkaction', ['category' => $category->slug]) }}"  tableid="categoryquestiontable"  
                         
                         :bulkotheraction='[
                             ["name"=>"Enable Visible Access","value"=>"visible_status"],
