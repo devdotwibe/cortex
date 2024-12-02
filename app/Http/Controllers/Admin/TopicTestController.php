710d9087b882cbc4c51a4707f51be5ba13cc856a
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-12-02 14:27:19 +0530

    update code

--- app/Http/Controllers/Admin/TopicTestController.php
+++ app/Http/Controllers/Admin/TopicTestController.php
@@ -50,7 +50,7 @@ class TopicTestController extends Controller
             if(!empty($request->sub_category_id)){
                 $this->where('sub_category_id',$request->sub_category_id);
             }
-            $this->orderBy('order', 'DESC');
+            $this->orderBy('order', 'ASC');
            
 
             return $this->where('exam_id',$exam->id)
