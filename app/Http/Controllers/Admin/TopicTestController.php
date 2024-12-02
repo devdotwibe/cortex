66816d0edd3d484d6880cf39b5e83c30b88d8d62
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-12-02 19:56:06 +0530

    update code

--- app/Http/Controllers/Admin/TopicTestController.php
+++ app/Http/Controllers/Admin/TopicTestController.php
@@ -122,7 +122,7 @@ class TopicTestController extends Controller
                 self::$model = Setname::class; 
                 return $this->where('sub_category_id',$request->parent_id??0)/*->where('category_id',$category->id)*/->buildSelectOption();
             }
-            elseif($name=="order"){
+            elseif($name=="order_no"){
                 self::reset();
                 self::$model = Question::class; 
 
@@ -157,9 +157,9 @@ class TopicTestController extends Controller
             ]);
             $exam=Exam::find( $exam->id );
         } 
-        if($question->order === '9999999999')
+        if($question->order_no === '9999999999')
         {
-            $question->order="";
+            $question->order_no="";
         }
         return view("admin.topic-test.edit",compact('category','exam','question'));
     }
