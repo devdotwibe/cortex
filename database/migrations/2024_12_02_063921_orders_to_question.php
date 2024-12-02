f1afd9a52f2d1d0f9ef06780c7bbe9317c08801d
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-12-02 12:17:47 +0530

    update code

--- database/migrations/2024_12_02_063921_orders_to_question.php
+++ database/migrations/2024_12_02_063921_orders_to_question.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('question', function (Blueprint $table) {
+        Schema::table('questions', function (Blueprint $table) {
             $table->string('order')->nullable();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('question', function (Blueprint $table) {
+        Schema::table('questions', function (Blueprint $table) {
             $table->dropColumn('order');
         });
     }
