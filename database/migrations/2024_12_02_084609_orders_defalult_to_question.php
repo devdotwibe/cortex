d1d28c99f705ce9e379ec111b4f244a44115d278
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-12-02 14:18:04 +0530

    update colmn

--- database/migrations/2024_12_02_084609_orders_defalult_to_question.php
+++ database/migrations/2024_12_02_084609_orders_defalult_to_question.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('question', function (Blueprint $table) {
+        Schema::table('questions', function (Blueprint $table) {
             $table->string('order')->default(0)->change();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('question', function (Blueprint $table) {
+        Schema::table('questions', function (Blueprint $table) {
             $table->dropColumn('order');
         });
     }
