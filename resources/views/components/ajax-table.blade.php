289aefde49e8236facea03129c061a38ecd45424
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-12-02 20:52:46 +0530

    update code

--- resources/views/components/ajax-table.blade.php
+++ resources/views/components/ajax-table.blade.php
@@ -227,6 +227,7 @@
             table_{{ $tableid }}=$('#table-{{ $tableid }}').DataTable({
                 processing: true,
                 serverSide: true,
+                ordering: true,
             
                 ajax: {
                     url: "{{ $url }}",
