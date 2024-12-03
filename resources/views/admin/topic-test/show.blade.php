a7a306cac3969f7f8a58b47393a1be1259f37a71
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-12-03 13:03:08 +0530

    update code

--- resources/views/admin/topic-test/show.blade.php
+++ resources/views/admin/topic-test/show.blade.php
@@ -131,6 +131,7 @@ $(function() {
                 success: function(res) {
 
                     console.log(res);
+                    $('#table-categoryquestiontable').DataTable().ajax.reload(); 
 
                 }
 
