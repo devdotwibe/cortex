0ba65d5c35be079ef6ec4d3385d4f25c5ae97ea7
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-12-04 19:54:49 +0530

    update code

--- app/Http/Controllers/User/LearnTopicController.php
+++ app/Http/Controllers/User/LearnTopicController.php
@@ -167,7 +167,7 @@ class LearnTopicController extends Controller
                 $question = UserReviewQuestion::findSlug($request->question);
                 return UserReviewAnswer::where('user_review_question_id', $question->id)->get(['slug', 'title', 'user_answer', 'iscorrect', 'description', 'image']);
             }
-            $data = UserReviewQuestion::whereIn('review_type', ['mcq', 'short_notes'])->where('user_id', $user->id)->where('user_exam_review_id', $userExamReview->id)->paginate(1, ['title', 'note', 'slug', 'review_type', 'user_answer', 'currect_answer', 'explanation']);
+            $data = UserReviewQuestion::whereIn('review_type', ['mcq', 'short_notes'])->where('user_id', $user->id)->where('user_exam_review_id', $userExamReview->id)->orderBy('order_no')->paginate(1, ['title', 'note', 'slug', 'review_type', 'user_answer', 'currect_answer', 'explanation']);
 
             $data_questions = UserReviewQuestion::whereIn('review_type', ['mcq', 'short_notes'])->where('user_id', $user->id)->where('user_exam_review_id', $userExamReview->id)->get();
 
