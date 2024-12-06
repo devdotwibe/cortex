23cf8526878de34cc54879ba1fd8c80ac87eb041
jerindotwibe <jerindotwibe@outlook.com>
2024-12-06 15:15:20 +0530

    up

--- resources/views/admin/question-bank/show.blade.php
+++ resources/views/admin/question-bank/show.blade.php
@@ -10,9 +10,7 @@
     <div class="header_wrapp">
         <div class="header_title">
             <div class="back-btn" id="back-btn" style="display: block"> <!-- Ensure proper display value -->
-                {{-- <a href="{{ route('admin.question-bank.subcategoryset') }}"> --}}
-                    <a href="{{ route('admin.question-bank.subcategoryset', [ 'setname' => $setName]) }}">
-
+                <a href="{{ route('admin.question-bank.index') }}">
                     <img src="{{ asset('assets/images/leftarrowblack.svg') }}" alt="">
                 </a>
             </div>
