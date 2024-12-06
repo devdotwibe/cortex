34b8f8221eafbc9aea4d2761ebf1a31bf01bc384
jerindotwibe <jerindotwibe@outlook.com>
2024-12-06 12:06:55 +0530

    up

--- resources/views/admin/question-bank/show.blade.php
+++ resources/views/admin/question-bank/show.blade.php
@@ -13,12 +13,17 @@
             
             {{-- <h2>{{$category->name}} -> {{$subcategory->name}} -> {{ $setname->name }} -> Questions</h2> --}}
 
-            <h2>
+            {{-- <h2>
                 <a href="{{ route('admin.question-bank.index') }}">{{$category->name}}</a> -> 
                 <a href="{{ route('admin.question-bank.index', ['id' => $category->slug,'type' =>'subcategory']) }}">{{$subcategory->name}}</a> -> 
                 {{ $setname->name }} -> Questions
-            </h2>
-            
+            </h2> --}}
+
+             <h2>
+                <a href="{{ route() }}">{{$category->name}}</a> -> 
+                <a href="{{ route() }}">{{$subcategory->name}}</a> -> 
+                {{ $setname->name }} -> Questions
+            </h2> 
 
         </div>
          
