ae6d230d111e61baadcc7c1efd4dc4bb0bea34cb
jerindotwibe <jerindotwibe@outlook.com>
2024-12-09 14:27:10 +0530

    up

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -593,13 +593,34 @@ if (isCollapsed) {
             const isCollapsed1 = localStorage.getItem('sidebarCollapsed1') === 'true';
             
 
-            // Apply the class based on stored state
-            if (isCollapsed) {
-                $('.side_bar').addClass('slider-btn');
-            } else {
+            // // Apply the class based on stored state
+            // if (isCollapsed) {
+            //     $('.side_bar').addClass('slider-btn');
+            // } else {
+            //     $('.side_bar').removeClass('slider-btn');
+            // }
+
+
+            if (!isCollapsed) {
                 $('.side_bar').removeClass('slider-btn');
+                
+                $('.titledisplay').removeAttr('title');
+            } else {
+              
+                
+                $('.side_bar').addClass('slider-btn');
+                
+                
+                $('.titledisplay').each(function () {
+                    // Retrieve the data-title attribute value
+                    var title = $(this).data('title');
+
+                    // Set the title attribute with the value
+                    $(this).attr('title', title);
+             });
             }
 
+            
             if (isCollapsed1) {
                 $('.sliderbody').addClass('slider-active');
             } else {
