5ebe93f6002b2fb79f0f5e5a7318405d12f88ae0
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-12-10 16:24:09 +0530

    updsate code

--- resources/views/user/topic-test/index.blade.php
+++ resources/views/user/topic-test/index.blade.php
@@ -47,7 +47,7 @@
                         </div>
                     </div>
             </div>
-            @endif
+            {{-- @endif --}}
             @endforeach
         </div>
     </div>
