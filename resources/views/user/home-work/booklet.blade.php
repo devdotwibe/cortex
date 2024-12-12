520aac5205e886019d1bce45f367737e992251f4
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-12-12 18:25:26 +0530

    update code

--- resources/views/user/home-work/booklet.blade.php
+++ resources/views/user/home-work/booklet.blade.php
@@ -441,9 +441,9 @@
                                                 ${v.short_question}
                                             </div>
                                             <div id="note-${lesseonId}-ans" class="form-group">
-                                                <div class="form-data">
+                                                <div class="form-data answer_type">
                                                     <div class="forms-inputs mb-4">
-                                                        <input type="text" name="answer" data-question="${v.slug}" id="user-answer-${lesseonId}" value="" class="form-control answer_type" placeholder="Write your answer here" aria-placeholder="Write your answer here" autocomplete="off" >
+                                                        <input type="text" name="answer" data-question="${v.slug}" id="user-answer-${lesseonId}" value="" class="form-control" placeholder="Write your answer here" aria-placeholder="Write your answer here" autocomplete="off" >
                                                         <div class="invalid-feedback" id="error-answer-field" >The field is required</div>
                                                     </div>
                                                 </div>
