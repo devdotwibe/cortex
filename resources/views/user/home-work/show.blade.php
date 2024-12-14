8308f93d6387f1b4e17d422df9350661b7b889a4
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-12-14 13:14:51 +0530

    update code

--- resources/views/user/home-work/show.blade.php
+++ resources/views/user/home-work/show.blade.php
@@ -27,7 +27,7 @@
                         @elseif($user->progress("home-work-{$homeWork->id}-booklet-{$item->id}-complete-date", '') == '')
                         @guest('admin')  <a  onclick="confimbooklet('{{route('home-work.booklet',['home_work'=>$homeWork->slug,'home_work_book'=>$item->slug])}}','{{$item->title}}')"> @endguest
                         @else
-                        <a  onclick="loadbooklethistory('{{route('home-work.booklet',['home_work'=>$homeWork->slug,'home_work_book'=>$item->slug])}}','{{$item->title}}')">
+                        <a  onclick="loadbooklethistory('{{route('home-work.history',['home_work'=>$homeWork->slug,'home_work_book'=>$item->slug])}}','{{$item->title}}')">
                         @endif
                             <div class="category">
                                 <div class="category-content"> 
@@ -117,7 +117,9 @@
                     var info = this.api().page.info(); 
                     var json = this.api().ajax.json();
                     console.log(json);
-                    $('#restart-btn').attr('href', json.url);
+                    // $('#restart-btn').attr('href', json.url);
+                     $('#restart-btn').attr('href','{{route('home-work.history',['home_work'=>$homeWork->slug,'home_work_book'=>$item->slug])}}','{{$item->title}}');
+                    
                     $('#review-history-label').html(` ${json.name} `)
                     if (info.pages > 1) {
                         $("#attemt-list-table_wrapper .pagination").show();
