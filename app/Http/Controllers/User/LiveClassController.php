339226531901bc375fba0fbb0b383787db750dc2
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2024-12-17 16:16:21 +0530

    up

--- app/Http/Controllers/User/LiveClassController.php
+++ app/Http/Controllers/User/LiveClassController.php
@@ -195,7 +195,7 @@ class LiveClassController extends Controller
          
         //  $jobStatus = Cache::get("job_status_{$process->jobIdentifier}");
 
-         if ($subLessonMaterial->status !== 'completed' && $subLessonMaterial->status !== 'failled') {
+         if ($subLessonMaterial->status !== 'completed' && $subLessonMaterial->status !== 'failled' || $subLessonMaterial->status === '') {
       
             // $process =ProcessFile::dispatch($filepath,$user,$subLessonMaterial,$cachepath);
             // ProcessFile::dispatchNow($filepath, $user, $subLessonMaterial, $cachepath);
