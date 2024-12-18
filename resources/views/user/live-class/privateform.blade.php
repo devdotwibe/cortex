e766bde85a5ae1a18b0a74bc65e9cb0cc52cdf5a
bijubmuse <biju.joseph@bmuse.in>
2024-12-18 13:12:10 +0530

    required fields

--- resources/views/user/live-class/privateform.blade.php
+++ resources/views/user/live-class/privateform.blade.php
@@ -18,11 +18,11 @@
                         :url="route('live-class.privateclass.form',$user->slug)" 
                         :cancel="route('live-class.privateclass', $user->slug)"
                         :fields='[
-                            ["name"=>"email","label"=>"Email","placeholder"=>"Email","type"=>"text","size"=>12],
-                            ["name"=>"full_name","label"=>"Student Full Name","placeholder"=>"Student Full Name","type"=>"text","size"=>12],
-                            ["name"=>"parent_name","label"=>"Parent Name","placeholder"=>"Parent Name","type"=>"text","size"=>12],
-                            ["name"=>"phone","label"=>"* Phone Number","placeholder"=>"Phone Number","type"=>"text","size"=>12],
-                            ["name"=>"timeslot","label"=>"* Select your available timeslot (you can choose more than one)","options"=>[["text"=>"Saturday 9:30 - 11:30 a.m (Online)","value"=>"Saturday 9:30 - 11:30 a.m (Online)"],["text"=>"Saturday 12 - 2 p.m","value"=>"Saturday 12 - 2 p.m"],["text"=>"Sunday 9:30 - 11:30 a.m","value"=>"Sunday 9:30 - 11:30 a.m"],["text"=>"Sunday 12 - 2 p.m","value"=>"Sunday 12 - 2 p.m"]],"type"=>"checkboxgroup","size"=>12],
+                            ["name"=>"email","label"=>"Email *","placeholder"=>"Email","type"=>"text","size"=>12],
+                            ["name"=>"full_name","label"=>"Student Full Name *","placeholder"=>"Student Full Name","type"=>"text","size"=>12],
+                            ["name"=>"parent_name","label"=>"Parent Name *","placeholder"=>"Parent Name","type"=>"text","size"=>12],
+                            ["name"=>"phone","label"=>"Phone Number *","placeholder"=>"Phone Number","type"=>"text","size"=>12],
+                            ["name"=>"timeslot","label"=>"Select your available timeslot (you can choose more than one) *","options"=>[["text"=>"Saturday 9:30 - 11:30 a.m (Online)","value"=>"Saturday 9:30 - 11:30 a.m (Online)"],["text"=>"Saturday 12 - 2 p.m","value"=>"Saturday 12 - 2 p.m"],["text"=>"Sunday 9:30 - 11:30 a.m","value"=>"Sunday 9:30 - 11:30 a.m"],["text"=>"Sunday 12 - 2 p.m","value"=>"Sunday 12 - 2 p.m"]],"type"=>"checkboxgroup","size"=>12],
                         ]' 
                         btnsubmit="Register"
                     ></x-general-form>
