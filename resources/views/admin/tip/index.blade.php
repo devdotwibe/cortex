5f055815c0f996632355853de19e5d6a09aebf90
jerindotwibe <jerindotwibe@outlook.com>
2025-01-02 16:49:54 +0530

    up

--- resources/views/admin/tip/index.blade.php
+++ resources/views/admin/tip/index.blade.php
@@ -14,41 +14,32 @@
 <section class="content_section admin_section" id="category-content-section">
     <div class="container">
         <div class="row">
-            @foreach ($categorys as $k => $item)
+            @foreach ($categorys as $k=> $item)
             <div class="col-md-6">
-                <a href="{{route('admin.tip.create', $item->id)}}">
+
+                <a href="{{route('admin.tip.create',$item->id)}}">
                     <div class="card mt-4">
                         <div class="card-body">
+
                             <div class="category">
                                 <div class="category-image">
-                                    <!-- Edit icon for the category image -->
-                                    <img src="{{ asset('assets/images/User-red.png') }}" id="category-content-icon-{{$item->id}}">
-                                    <i id="category-content-icon-edit-{{$item->id}}" onclick="editIcon(event, this)" data-category="{{$item->id}}">
-                                        <img src="{{ asset('assets/images/pen.png') }}" width="15" alt="">
-                                    </i>
+                                    <img src="{{asset('assets/images/User-red.png')}}">
                                 </div>
 
                                 <div class="category-content">
-                                    <!-- Edit icon for the subtitle -->
-                                    <h5>
-                                        <span id="category-content-subtitle-{{$item->id}}"> Topic {{$k+1}} </span> 
-                                        <i id="category-content-subtitle-edit-{{$item->id}}" onclick="editSubtitle(event, this)" 
-                                            data-title="{{$item->name}}" 
-                                            data-subtitle="Topic {{$k+1}}" 
-                                            data-category="{{$item->id}}">
-                                            <img src="{{ asset('assets/images/pen.png') }}" width="15" alt="">
-                                        </i>
-                                    </h5>
+                                    <h5><span id="category-content-subtitle-3"> Topic {{$k+1}} </span> <i id="category-content-subtitle-edit-3" onclick="editsubtitle(event,this)" data-title="a" data-subtitle="Topic 1" data-category="3"><img src="http://localhost:8000/assets/images/pen.png" width="15" alt=""> </i></h5>
                                     <h3>{{$item->name}}</h3>
                                 </div>
 
                             </div>
+
                         </div>
                     </div>
                 </a>
+
             </div>
             @endforeach
-        </div>
+
     </div>
 </section>
 
