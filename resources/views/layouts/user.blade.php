4bf6287a43b869c55d3055b6583369f1ec4c4a83
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2025-01-06 18:22:03 +0530

    udpate code

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -526,13 +526,13 @@ if (!allowedTypes.includes(file.type)) {
 
             const isCollapsed = $('.side_bar').hasClass('slider-btn');
 
-            const collapsed = false;
+            const collapsed = 'false';
 
             if(isCollapsed)
             {
-                const collapsed = true;
+                const collapsed = 'true';
             }
-            
+
             const isCollapsed1 = $('.sliderbody').hasClass('slider-active');
 
                     $.ajax({
