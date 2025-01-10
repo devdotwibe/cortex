4f6edc922ff28728395877842a579db66f349434
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2025-01-10 11:20:22 +0530

    update code

--- routes/web.php
+++ routes/web.php
@@ -30,24 +30,24 @@ use Illuminate\Support\Facades\Route;
  * Development
  */
 
-Route::get('/rollback', function () {
-    Artisan::call('migrate:rollback');
-    dd(Artisan::output());
-});
+// Route::get('/rollback', function () {
+//     Artisan::call('migrate:rollback');
+//     dd(Artisan::output());
+// });
 
-Route::get('/migrate', function () {
-    Artisan::call('migrate');
-    dd(Artisan::output());
-});
-Route::get('/fresh', function () {
-    Artisan::call('migrate:fresh');
-    dd(Artisan::output());
-});
+// Route::get('/migrate', function () {
+//     Artisan::call('migrate');
+//     dd(Artisan::output());
+// });
+// Route::get('/fresh', function () {
+//     Artisan::call('migrate:fresh');
+//     dd(Artisan::output());
+// });
 
-Route::get('/db-seed', function () {
-    Artisan::call('db:seed');
-    dd(Artisan::output());
-});
+// Route::get('/db-seed', function () {
+//     Artisan::call('db:seed');
+//     dd(Artisan::output());
+// });
 
 Route::get('/', [HomeController::class, 'index']);
 
