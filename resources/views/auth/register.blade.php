def73c8ad15954ae7152bba2b597cdee5d659020
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2025-01-13 14:26:00 +0530

    up

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -45,7 +45,7 @@
                                     <div id="email-error" class="error invalid-feedback">{{$message}}</div>
                                     @enderror
 
-                                    <span>Note:We are currently facing issues with yahoo, outlook and hotmail and gmail is the preferred email address for accounts.</span>
+                                    <div>Note:We are currently facing issues with yahoo, outlook and hotmail and gmail is the preferred email address for accounts.</div>
 
                                 </div>
 
