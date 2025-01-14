d8de807d45cd0f05c75440c2a9dab3e17bb84ccf
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2025-01-14 11:47:21 +0530

    update code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,12 +82,12 @@
                     @endif
                     
 
-
-
-
-
-
-
+                    <div class="sticky-box">
+                        <p>
+                            New: Cortex Online's platform is updated to simulate the latest Online Selective Test 
+                            <a class="take-test" href="{{route('full-mock-exam.index')}}"> Take a free diagnostic test --> </a>
+                        </p>
+                    </div>
 
                     </ul>
 
