634f838a19dcac42b20c67606d7cbe38b6ebf060
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2025-01-17 10:58:01 +0530

    udparte code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
         @endif
     </title>
     
-    <meta name="title" content="Cortex Online">
+    <meta name="title" content="Cortex Online: Selective Test Preparation - Thinking Skills">
     <meta name="keywords" content="Cortex Online">
     <meta name="description" content="Cortex Online has video lessons that teach the theory for different Thinking Skills topics, a question bank with over 1500 questions for practice, and an exam simulator to help students with trial test preparation.">
 
@@ -191,8 +191,9 @@
 
                         <p><a href="https://www.cortexacademy.com.au" target="_blank">www.cortexacademy.com.au</a></p>
                         
-                        <p><a href="">St Hudson Street
-                                <span>Australia</span></a>
+                        <p><a href=""> Farnell Avenue
+                                {{-- <span>Australia</span>  --}}
+                            </a>
                         </p>
                         <p><a href="">Open 9am to 5pm
                                 <span>Monday to Friday</span></a>
