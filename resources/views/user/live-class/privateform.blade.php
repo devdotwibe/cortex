3269b2df15957e43f431f74d85ec9acf9b7b79a5
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2025-01-21 16:16:34 +0530

    udpate code

--- resources/views/user/live-class/privateform.blade.php
+++ resources/views/user/live-class/privateform.blade.php
@@ -13,6 +13,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="private-content"> 
+                    {{-- ["name"=>"phone","label"=>"Phone Number *","placeholder"=>"Phone Number","type"=>"text","size"=>12], --}}
                     {{-- <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfc-gOkpIRCfsvQjUjwPuVdj9lCnzcvNGjatEbWsgdxCUjXJQ/viewform?embedded=true" width="100%" height="1084" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe> --}}
                     <x-general-form 
                         :url="route('live-class.privateclass.form',$user->slug)" 
@@ -21,7 +22,7 @@
                             ["name"=>"email","label"=>"Email *","placeholder"=>"Email","type"=>"text","size"=>12],
                             ["name"=>"full_name","label"=>"Student Full Name *","placeholder"=>"Student Full Name","type"=>"text","size"=>12],
                             ["name"=>"parent_name","label"=>"Parent Name *","placeholder"=>"Parent Name","type"=>"text","size"=>12],
-                            ["name"=>"phone","label"=>"Phone Number *","placeholder"=>"Phone Number","type"=>"text","size"=>12],
+                           
                             ["name"=>"timeslot","label"=>"Select a class time (you can choose more than one) *","options"=>[["text"=>"Monday 6:30 p.m. (Online) - Year 6","value"=>"Monday 6:30 p.m. (Online) - Year 6"],["text"=>"Wednesday 6:30 p.m. (Online) - Year 5","value"=>"Wednesday 6:30 p.m. (Online) - Year 5"],["text"=>"Thursday 6:30 p.m. (Online) - Year 6","value"=>"Thursday 6:30 p.m. (Online) - Year 6"],["text"=>"Saturday 9:30 a.m. (F2F) - Year 5","value"=>"Saturday 9:30 a.m. (F2F) - Year 5"],["text"=>"Saturday 12 p.m. (F2F) - Year 5","value"=>"Saturday 12 p.m. (F2F) - Year 5"],["text"=>"Saturday 2:30 p.m. (F2F) - Year 6","value"=>"Saturday 2:30 p.m. (F2F) - Year 6"],["text"=>"Sunday 9:30 a.m. (F2F) - Year 5","value"=>"Sunday 9:30 a.m. (F2F) - Year 5"],["text"=>"Sunday 12 p.m. (F2F) - Year 5","value"=>"Sunday 12 p.m. (F2F) - Year 5"],["text"=>"Sunday 2:30 p.m. (F2F) - Year 6","value"=>"Sunday 2:30 p.m. (F2F) - Year 6"]],"type"=>"checkboxgroup","size"=>12],
                         ]' 
                         btnsubmit="Register"
