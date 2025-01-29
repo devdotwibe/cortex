a188ccf6cbf7d53173da56cf8be9ade3e1f5a33d
devdotwibe <59353592+devdotwibe@users.noreply.github.com>
2025-01-29 18:12:46 +0530

    up

--- app/Http/Controllers/User/LiveClassController.php
+++ app/Http/Controllers/User/LiveClassController.php
@@ -224,6 +224,9 @@ class LiveClassController extends Controller
         $cachepath=Storage::disk('private')->path('cache/'.md5($subLessonMaterial->pdf_file));
         $filepath=Storage::disk('private')->path($subLessonMaterial->pdf_file);
         File::ensureDirectoryExists($cachepath);
+
+        $subLessonMaterial->status="";
+        $subLessonMaterial->save();
         // if(!File::exists("$cachepath/render.map.json")){
         //     $pdfmap=ImageHelper::convertPdfToImage($filepath,$cachepath);
         //     file_put_contents("$cachepath/render.map.json",json_encode($pdfmap));
