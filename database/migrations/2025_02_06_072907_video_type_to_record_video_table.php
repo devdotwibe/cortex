ea7bd0e672a054b7b61f6f74a196e7bf34337668
ArjunlalDotwibe <arjundotwibe@outlook.com>
2025-02-06 13:04:32 +0530

    update code

--- database/migrations/2025_02_06_072907_video_type_to_record_video_table.php
+++ database/migrations/2025_02_06_072907_video_type_to_record_video_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('record_video', function (Blueprint $table) {
+        Schema::table('record_videos', function (Blueprint $table) {
             $table->string('video_type')->nullable();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('record_video', function (Blueprint $table) {
+        Schema::table('record_videos', function (Blueprint $table) {
             $table->dropColumn('video_type');
         });
     }
