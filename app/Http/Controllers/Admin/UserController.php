780d434db72e8a9a4904b3fa6eb684c17f8707af
ArjunlalDotwibe <arjundotwibe@outlook.com>
2025-02-10 16:23:14 +0530

    updsaet ode

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -353,10 +353,10 @@ class UserController extends Controller
             'success' => "Community status updated"
         ]);
     }
-    public function freeaccess(Request $request, User $user)
+    public function freeaccess(Request $request)
     {
         dd($request);
-        
+
         $user->update([
             'is_free_access' => $user->is_free_access ? false : true
         ]);
