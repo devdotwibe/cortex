70f16dcc778121766d470179bc987161ba5f087d
ArjunlalDotwibe <arjundotwibe@outlook.com>
2025-02-10 19:57:22 +0530

    up

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -35,6 +35,8 @@ class UserController extends Controller
     public function index(Request $request)
     {
         if ($request->ajax()) {
+
+            dd($request);
             if (!empty($request->usertype)) {
                 switch ($request->usertype) {
                     case 'free-users':
@@ -169,7 +171,7 @@ class UserController extends Controller
         $terms = [];
 
         $terms1 = ClassDetail::get();
-        $terms2 = LessonMaterial::get();
+        $terms2 = LessonMarequestterial::get();
         $terms3 = HomeWork::get();
         $terms4 = LessonRecording::get();
         
