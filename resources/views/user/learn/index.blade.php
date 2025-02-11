0ccc53e9a0eb539ba587870b409755cdcca1dba7
ArjunlalDotwibe <arjundotwibe@outlook.com>
2025-02-11 14:44:33 +0530

    udpate code

--- resources/views/user/learn/index.blade.php
+++ resources/views/user/learn/index.blade.php
@@ -13,7 +13,7 @@
         <div class="row {{ $user->free_access_terms }}">
             @foreach ($categorys as $k => $item)
             <div class="col-md-6">
-                @if (($user->is_free_access && in_array($item->id, explode(',', $user->free_access_terms)))||(optional($user->subscription())->status??"")=="subscribed")
+                @if (($user->is_free_access && in_array($item->id, explode(',', $user->free_access_terms)))||(optional($user->subscription())->status??"")=="subscribed" || $k==0)
                 <a href="{{ route('learn.show', $item->slug) }}">
                 @else
                     {{-- <a href="{{ route('pricing.index') }}#our-plans"> --}}
