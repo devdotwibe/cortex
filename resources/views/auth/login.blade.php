37816dcb709305ddf27ad350e1e437972caffb28
ArjunlalDotwibe <arjundotwibe@outlook.com>
2025-02-21 10:53:51 +0530

    updaste code

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -59,7 +59,7 @@
                                     <div class="form-check">
                                         <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                                         <label class="form-check-label" for="remember">
-                                            Remember me
+                                            Stay logged in
                                         </label>
                                     </div>
                                 </div>
