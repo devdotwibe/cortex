d473f5f43df80025a5736f369fd14e9586617445
ArjunlalDotwibe <arjund@twibe@3>
2025-04-21 19:40:12 +0530

    up

--- resources/views/user/live-class/privateform.blade.php
+++ resources/views/user/live-class/privateform.blade.php
@@ -15,7 +15,7 @@
                 <div class="private-content">
                     {{-- ["name"=>"phone","label"=>"Phone Number *","placeholder"=>"Phone Number","type"=>"text","size"=>12], --}}
                     {{-- <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfc-gOkpIRCfsvQjUjwPuVdj9lCnzcvNGjatEbWsgdxCUjXJQ/viewform?embedded=true" width="100%" height="1084" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe> --}}
-                    <x-general-form
+                    {{-- <x-general-form
                         :url="route('live-class.privateclass.form',$user->slug)"
                         :cancel="route('live-class.privateclass', $user->slug)"
                         :fields='[
@@ -26,7 +26,20 @@
                             ['name' => 'timeslot', 'label' => 'Select a class time (you can choose more than one) *', 'options' => $time_array, 'type' => 'checkboxgroup', 'size' => 12],
                         ]'
                         btnsubmit="Register"
-                    ></x-general-form>
+                    ></x-general-form> --}}
+
+                    <x-general-form
+                        :url="route('live-class.privateclass.form', $user->slug)"
+                        :cancel="route('live-class.privateclass', $user->slug)"
+                        :fields="[
+                            ['name' => 'email', 'label' => 'Email *', 'placeholder' => 'Email', 'type' => 'text', 'size' => 12],
+                            ['name' => 'full_name', 'label' => 'Student Full Name *', 'placeholder' => 'Student Full Name', 'type' => 'text', 'size' => 12],
+                            ['name' => 'parent_name', 'label' => 'Parent Name *', 'placeholder' => 'Parent Name', 'type' => 'text', 'size' => 12],
+                            ['name' => 'timeslot', 'label' => 'Select a class time (you can choose more than one) *', 'options' => $time_array, 'type' => 'checkboxgroup', 'size' => 12],
+                        ]"
+                        btnsubmit="Register"
+                    />
+
                 </div>
             </div>
         </div>
