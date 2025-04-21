b2f6cf11e2beb6d66829b1e4d51c0c1d273d80a4
ArjunlalDotwibe <arjund@twibe@3>
2025-04-21 19:23:41 +0530

    up

--- app/Http/Controllers/User/LiveClassController.php
+++ app/Http/Controllers/User/LiveClassController.php
@@ -27,14 +27,14 @@ use Imagick;
 
 class LiveClassController extends Controller
 {
-    
+
     public function index()
-    { 
+    {
         /**
          * @var User
          */
         $user=Auth::user();
-        $live_class =  LiveClassPage::first(); 
+        $live_class =  LiveClassPage::first();
         return view('user.live-class.index',compact('live_class','user'));
     }
     public function workshop(Request  $request){
@@ -42,16 +42,16 @@ class LiveClassController extends Controller
          * @var User
          */
         $user=Auth::user();
-        $live_class =  LiveClassPage::first(); 
+        $live_class =  LiveClassPage::first();
         return view('user.live-class.workshop',compact('user','live_class'));
     }
- 
+
     public function workshopform(Request  $request){
         /**
          * @var User
          */
         $user=Auth::user();
-        $live_class =  LiveClassPage::first(); 
+        $live_class =  LiveClassPage::first();
         if ($user->progress('intensive-workshop-payment','')=="paid"){
             return view('user.live-class.workshopform',compact('user','live_class'));
         }else{
@@ -62,15 +62,15 @@ class LiveClassController extends Controller
     public function privateclass(Request  $request,$live){
         /**
          * @var User
-         */ 
-        $user=Auth::user(); 
-       
-        $live_class =  LiveClassPage::first(); 
+         */
+        $user=Auth::user();
+
+        $live_class =  LiveClassPage::first();
         $setting = Settings::first();
         $timetables = Timetable::where('hide_time','!=','Y')->get();
         return view('user.live-class.private',compact('user','live_class','setting','timetables'));
     }
- 
+
     public function privateclassform(Request  $request){
         /**
          * @var User
@@ -84,7 +84,7 @@ class LiveClassController extends Controller
         //         'value' => $text,
         //     ];
         // })->toArray();
-        
+
         // $fields = [
         //     ["name" => "email", "label" => "Email *", "placeholder" => "Email", "type" => "text", "size" => 12],
         //     ["name" => "full_name", "label" => "Student Full Name *", "placeholder" => "Student Full Name", "type" => "text", "size" => 12],
@@ -92,11 +92,21 @@ class LiveClassController extends Controller
         //     ["name" => "timeslot", "label" => "Select a class time (you can choose more than one) *", "options" => $time_array, "type" => "checkboxgroup", "size" => 12],
         // ];
 
-        $live_class =  LiveClassPage::first();  
+        $time_array = Timetable::where('hide_time', '!=', 'Y')->get()->map(function($item) {
+            $text = $item->day . ' ' . $item->starttime . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . ' (' . $item->year . ') - Year ' . $item->type;
+            return [
+                'text' => $text,
+                'value' => $text,
+            ];
+        })->toArray();
 
-        return view('user.live-class.privateform',compact('user','live_class')); 
+        dd($time_array);
+
+        $live_class =  LiveClassPage::first();
+
+        return view('user.live-class.privateform',compact('user','live_class'));
     }
-    public function privateclassformsubmit(Request  $request){ 
+    public function privateclassformsubmit(Request  $request){
         $data=$request->validate([
             'email'=>['required','email:rfc,dns','unique:private_classes','max:250'],
             'full_name'=>['required','string','max:255'],
@@ -108,32 +118,32 @@ class LiveClassController extends Controller
          * @var User
          */
         $user=Auth::user();
-        $live_class =  LiveClassPage::first();  
+        $live_class =  LiveClassPage::first();
 
         $data['user_id']=$user->id;
 
-        PrivateClass::store($data); 
+        PrivateClass::store($data);
 
         return redirect()->route('live-class.privateclass', $user->slug)->with('success','Class Requested Succesfully');
 
     }
- 
+
     public function privateclassroom(Request  $request){
         /**
          * @var User
          */
         $user=Auth::user();
-        $live_class =  LiveClassPage::first();  
-        return view('user.live-class.privateclass',compact('user','live_class')); 
+        $live_class =  LiveClassPage::first();
+        return view('user.live-class.privateclass',compact('user','live_class'));
     }
     public function privateclassdetails(Request  $request){
         /**
          * @var User
          */
         $user=Auth::user();
-        $live_class =  LiveClassPage::first();  
+        $live_class =  LiveClassPage::first();
         $classdetail = ClassDetail::whereIn('id',TermAccess::where('type','class-detail')->where('user_id',$user->id)->select('term_id'))->get();
-        return view('user.live-class.class-detail',compact('user','live_class','classdetail')); 
+        return view('user.live-class.class-detail',compact('user','live_class','classdetail'));
     }
     public function privateclassterm(Request  $request,$live,ClassDetail $classDetail){
         /**
@@ -144,10 +154,10 @@ class LiveClassController extends Controller
         if(TermAccess::where('type','class-detail')->where('term_id',$classDetail->id)->where('user_id',$user->id)->count()==0){
             return abort(404);
         }
-        $live_class =  LiveClassPage::first();  
+        $live_class =  LiveClassPage::first();
         $sloteterms=[];
 
-        $sloteterms_items = [ 
+        $sloteterms_items = [
             [
                 'text' => "Monday 6:30 p.m. (Online) - Year 6",
                 'id' => "Monday 6:30 p.m. (Online) - Year 6"
@@ -185,7 +195,7 @@ class LiveClassController extends Controller
                 'id' => "Sunday 2:30 p.m. (F2F) - Year 6"
             ]
         ];
-        
+
         $sloteclass_terms = array_filter($sloteterms_items, function ($item) use ($classDetail) {
             return SubClassDetail::whereJsonContains('timeslot', $item['id'])
                 ->where('class_detail_id', $classDetail->id)
@@ -200,8 +210,8 @@ class LiveClassController extends Controller
         }
 
         // ->whereJsonContains('timeslot',$s) private class terms
-        
-        return view('user.live-class.class-detail-term',compact('user','live_class','classDetail','sloteterms')); 
+
+        return view('user.live-class.class-detail-term',compact('user','live_class','classDetail','sloteterms'));
     }
 
     public function privateclasslesson(Request  $request){
@@ -209,9 +219,9 @@ class LiveClassController extends Controller
          * @var User
          */
         $user=Auth::user();
-        $live_class =  LiveClassPage::first();  
+        $live_class =  LiveClassPage::first();
         $lessons = LessonMaterial::whereIn('id',TermAccess::where('type','lesson-material')->where('user_id',$user->id)->select('term_id'))->get();
-        return view('user.live-class.lesson-detail',compact('user','live_class','lessons')); 
+        return view('user.live-class.lesson-detail',compact('user','live_class','lessons'));
     }
     public function privateclasslessonshow(Request  $request,$live,LessonMaterial $lessonMaterial){
         /**
@@ -222,17 +232,17 @@ class LiveClassController extends Controller
         if(TermAccess::where('type','lesson-material')->where('term_id',$lessonMaterial->id)->where('user_id',$user->id)->count()==0){
             return abort(404);
         }
-        $live_class =  LiveClassPage::first();  
+        $live_class =  LiveClassPage::first();
         $lessons = SubLessonMaterial::where('lesson_material_id',$lessonMaterial->id)->paginate();
-        return view('user.live-class.lesson',compact('user','live_class','lessonMaterial','lessons')); 
+        return view('user.live-class.lesson',compact('user','live_class','lessonMaterial','lessons'));
     }
-    
+
     public function privateclasslessonpdf(Request  $request,$live,SubLessonMaterial $subLessonMaterial){
         /**
          * @var User
          */
         $user=Auth::user();
-        $live_class =  LiveClassPage::first();   
+        $live_class =  LiveClassPage::first();
         $lessonMaterial=LessonMaterial::find($subLessonMaterial->lesson_material_id);
 
         if(TermAccess::where('type','lesson-material')->where('term_id',$lessonMaterial->id)->where('user_id',$user->id)->count()==0){
@@ -252,8 +262,8 @@ class LiveClassController extends Controller
         //     $pdfmap=ImageHelper::convertPdfToImage($filepath,$cachepath);
         //     file_put_contents("$cachepath/render.map.json",json_encode($pdfmap));
         // }else{
-        //     $pdfmap=json_decode(file_get_contents("$cachepath/render.map.json"),true); 
-        // } 
+        //     $pdfmap=json_decode(file_get_contents("$cachepath/render.map.json"),true);
+        // }
         // if($request->ajax()){
         //     $key =$pdfmap['hash'];
         //     $part=$request->part??0;
@@ -265,11 +275,11 @@ class LiveClassController extends Controller
         //     ]);
         // }
 
-         
+
         //  $jobStatus = Cache::get("job_status_{$process->jobIdentifier}");
 
          if ($subLessonMaterial->status !== 'completed' && $subLessonMaterial->status !== 'failled' || $subLessonMaterial->status === '') {
-      
+
             // $process =ProcessFile::dispatch($filepath,$user,$subLessonMaterial,$cachepath);
             // ProcessFile::dispatchNow($filepath, $user, $subLessonMaterial, $cachepath);
 
@@ -281,7 +291,7 @@ class LiveClassController extends Controller
             if($subLessonMaterial->status !='processing')
             {
 
-                $subLessonMaterial->status = 'processing'; 
+                $subLessonMaterial->status = 'processing';
 
                 $subLessonMaterial->save();
 
@@ -302,27 +312,27 @@ class LiveClassController extends Controller
         }
         elseif ($subLessonMaterial->status === 'completed') {
 
-            $imgdata=json_decode(file_get_contents("$cachepath/render.map.json"),true); 
+            $imgdata=json_decode(file_get_contents("$cachepath/render.map.json"),true);
 
             if (request()->ajax())
             {
                 return response()->json(['message' => 'Render the pdf' ,'status' => 'completed']);
             }
-           
+
         }
 
         // $pdfmap['url']=route('live-class.privateclass.lessonpdf', ["live" =>$user->slug,"sub_lesson_material"=>$subLessonMaterial->slug ]);
-        return view('user.live-class.pdfrender',compact('user','live_class','subLessonMaterial','lessonMaterial','imgdata')); 
+        return view('user.live-class.pdfrender',compact('user','live_class','subLessonMaterial','lessonMaterial','imgdata'));
     }
 
 
-    
+
     public function privateclasslessonpdfload(Request  $request,$live,SubLessonMaterial $subLessonMaterial,$file){
-        $cachepath=Storage::disk('private')->path('cache/'.md5($subLessonMaterial->pdf_file)); 
+        $cachepath=Storage::disk('private')->path('cache/'.md5($subLessonMaterial->pdf_file));
         File::ensureDirectoryExists($cachepath);
         header('Content-Type: image/jpeg');
         print_r(file_get_contents("$cachepath/$file"));
         exit;
     }
-    
+
 }
