d66b74ebe6eb1d61bda46892e1ee998b80918f5f
ArjunlalDotwibe <arjund@twibe@3>
2025-04-22 13:22:47 +0530

    up

--- database/migrations/2025_04_15_140850_hide_time_to_timetable.php
+++ database/migrations/2025_04_15_140850_hide_time_to_timetable.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('timetable', function (Blueprint $table) {
-            $table->string('hide_time')->nullbale();
+            $table->string('hide_time')->nullable();
         });
     }
 
