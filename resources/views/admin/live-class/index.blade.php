0c30f903a02a8dc481b05533a2fef4eb0c5ee8d1
ArjunlalDotwibe <arjund@twibe@3>
2025-04-22 18:23:05 +0530

    up

--- resources/views/admin/live-class/index.blade.php
+++ resources/views/admin/live-class/index.blade.php
@@ -485,7 +485,7 @@ These open group sessions condense the entire Thinking Skills curriculum into te
 
                                             <p>{{ $timetable->day }}
                                                 <span>({{ $timetable->starttime }} {{ $timetable->starttime_am_pm }} -
-                                                    {{ $timetable->endtime }} {{ $timetable->endtime_am_pm }}) ({{ $timetable->type }}) Year - {{  $timetable->year }}</span>
+                                                    {{ $timetable->endtime }} {{ $timetable->endtime_am_pm }}) ({{ $timetable->type }}) {{  $timetable->year }}</span>
                                             </p>
 
                                             <div class="user-icons">
