5dbff9d8499f87d7afbab48c739e5337862ac6ef
ArjunlalDotwibe <arjund@twibe@3>
2025-04-22 19:11:26 +0530

    up

--- app/Http/Controllers/Admin/LiveClassController.php
+++ app/Http/Controllers/Admin/LiveClassController.php
@@ -405,8 +405,8 @@ class LiveClassController extends Controller
 
         $allTerms = $terms1->concat($terms2)->concat($terms3)->concat($terms4);
 
-        $sloteterms_items = Timetable::where('hide_time', '!=', 'Y')->orderBy('order_no')->get()->map(function($item) {
-            $text = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ')' . $item->year;
+        $sloteterms_items = Timetable::where('hide_time', '!=', 'Y')->whereNull('static')->orderBy('order_no')->get()->map(function($item) {
+            $text = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ') ' . $item->year;
             $value = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ') - Year ' . $item->year;
             return [
                 'text' => $text,
