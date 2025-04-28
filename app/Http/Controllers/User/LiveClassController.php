98b65c2927d11bb2be8f9bc43636d13d8f626d91
ArjunlalDotwibe <arjund@twibe@3>
2025-04-28 11:29:57 +0530

    u;

--- app/Http/Controllers/User/LiveClassController.php
+++ app/Http/Controllers/User/LiveClassController.php
@@ -320,13 +320,13 @@ class LiveClassController extends Controller
 
 
 
-    public function privateclasslessonpdfload(Request  $request,$live,SubLessonMaterial $subLessonMaterial,$file){
-        dd('trest');
-        // $cachepath=Storage::disk('private')->path('cache/'.md5($subLessonMaterial->pdf_file));
-        // File::ensureDirectoryExists($cachepath);
-        // header('Content-Type: image/jpeg');
-        // print_r(file_get_contents("$cachepath/$file"));
-        // exit;
-    }
+    // public function privateclasslessonpdfload(Request  $request,$live,SubLessonMaterial $subLessonMaterial,$file){
+    //     dd('trest');
+    //     // $cachepath=Storage::disk('private')->path('cache/'.md5($subLessonMaterial->pdf_file));
+    //     // File::ensureDirectoryExists($cachepath);
+    //     // header('Content-Type: image/jpeg');
+    //     // print_r(file_get_contents("$cachepath/$file"));
+    //     // exit;
+    // }
 
 }
