447ba91569c2eb10146a4b1635e36f5839dea6ee
ArjunlalDotwibe <arjund@twibe@3>
2025-05-06 18:12:41 +0530

    up

--- resources/views/user/question-bank/show.blade.php
+++ resources/views/user/question-bank/show.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.user')
 @section('title', $exam->subtitle($category->id,"Topic ".($category->getIdx()+1)).':'.$category->name)
 @section('content')
+
 <section class="header_nav">
     <div class="header_wrapp">
         <div class="header_title">
-            <div class="lesson">   
+            <div class="lesson">
             <a class="lesson-exit float-start" href="{{route('question-bank.index')}}"  title="Exit" data-title="Exit" aria-label="Exit" data-toggle="tooltip">
                 <img src="{{asset("assets/images/exiticon.svg")}}" alt="exiticon">
-            </a> 
+            </a>
             <div class="lesson-title">
                 <h5><span>{{$exam->subtitle($category->id,"Topic ".($category->getIdx()+1))}}</span><span> : </span><span>{{$category->name}}</span></h5>
             </div>
@@ -17,10 +18,10 @@
 </section>
 <section class="container set-container">
     <div class="container-wrap">
-        <div class="lesson">                  
+        <div class="lesson">
             {{-- <a class="lesson-exit float-start" href="{{route('question-bank.index')}}"  title="Exit" data-title="Exit" aria-label="Exit" data-toggle="tooltip">
                 <img src="{{asset("assets/images/exiticon.svg")}}" alt="exiticon">
-            </a> 
+            </a>
             <div class="lesson-title">
                 <h5><span>{{$exam->subtitle($category->id,"Topic ".($category->getIdx()+1))}}</span><span> : </span><span>{{$category->name}}</span></h5>
             </div>  --}}
@@ -39,44 +40,55 @@
             <div class="lesson-body">
                 <div class="row" id="lesson-list">
                     @forelse ($lessons as $k=> $item)
-                    <div class="col-md-6">                        
+                    <div class="col-md-6">
                         <div class="lesson-row">
-                            <div class="lesson-row-title"> 
+                            <div class="lesson-row-title">
                                 <h2>{{ $item->name }}</h2>
                             </div>
-                            <div class="lesson-row-sets"> 
+                            <div class="lesson-row-sets">
                                 @foreach ($item->setname()->whereHas('questions',function($qry)use($exam){
                                     $qry->where('exam_id',$exam->id);
                                 })->orderBy('created_at','asc')->get() as $sk=> $set)
                                     @if($set->time_of_exam && $set->time_of_exam !== '00 : 00')
 
-                                        <div class="sets-item">
-                                            
-                                            @if (($user->is_free_access && in_array('question_bank', explode(',', $user->free_access_terms)))||(optional($user->subscription())->status??"")=="subscribed"||($k == 0&&$sk==0)) 
+                                        @php
+                                            $user_access =false;
+                                        @endphp
 
-                                            <a @if($user->progress('exam-'.$exam->id.'-topic-'.$category->id.'-lesson-'.$item->id.'-set-'.$set->id.'-complete-review',"no")=="yes") 
+                                        @if (($user->is_free_access && in_array('question_bank', explode(',', $user->free_access_terms)))||(optional($user->subscription())->status??"")=="subscribed"||($k == 0&&$sk==0))
+                                            @php
+                                                $user_access =false;
+                                            @endphp
+
+                                        @endif
+
+                                        <div class="sets-item {{ !$user_access ? 'grey' : '' }}">
+
+                                            @if (($user->is_free_access && in_array('question_bank', explode(',', $user->free_access_terms)))||(optional($user->subscription())->status??"")=="subscribed"||($k == 0&&$sk==0))
+
+                                            <a @if($user->progress('exam-'.$exam->id.'-topic-'.$category->id.'-lesson-'.$item->id.'-set-'.$set->id.'-complete-review',"no")=="yes")
 
                                                 onclick="loadlessonsetreviews('{{route('question-bank.set.history',['category'=>$category->slug,'sub_category'=>$item->slug,'setname'=>$set->slug])}}')"
 
-                                                @elseif($user->progress('exam-'.$exam->id.'-topic-'.$category->id.'-lesson-'.$item->id.'-set-'.$set->id.'-complete-date',"")=="")  
+                                                @elseif($user->progress('exam-'.$exam->id.'-topic-'.$category->id.'-lesson-'.$item->id.'-set-'.$set->id.'-complete-date',"")=="")
 
-                                                @guest('admin') onclick="confimexam('{{route('question-bank.set.attempt',['category'=>$category->slug,'sub_category'=>$item->slug,'setname'=>$set->slug])}}')" @endguest 
+                                                @guest('admin') onclick="confimexam('{{route('question-bank.set.attempt',['category'=>$category->slug,'sub_category'=>$item->slug,'setname'=>$set->slug])}}')" @endguest
 
                                                 @else onclick="loadlessonsetreviews('{{route('question-bank.set.history',['category'=>$category->slug,'sub_category'=>$item->slug,'setname'=>$set->slug])}}')" @endif >
-                                                
+
                                             @else
                                             {{-- <a href="{{route('pricing.index')}}"> --}}
                                                 <a href="javascript:void(0);" onclick="showLockedModal()">
                                             @endif
                                                 <span class="sets-title">{{$set->name}}</span>
                                             </a>
-                                        </div>  
+                                        </div>
 
-                                    @endif                                  
+                                    @endif
                                 @endforeach
-                            </div> 
-                        </div> 
-                    </div>                     
+                            </div>
+                        </div>
+                    </div>
                     @empty
                     <div class="col-md-12">
                         <div class="empty-row">
@@ -84,8 +96,8 @@
                         </div>
                     </div>
                     @endforelse
-                </div>  
-            </div> 
+                </div>
+            </div>
         </div>
     </div>
 </section>
@@ -105,6 +117,9 @@
             </div>
             <div class="modal-body">
                 <p>The content is locked and a subscription is required.</p>
+
+                <p>If you are enrolled in our classes, this will be unlocked in Term 3 Week 7</p>
+
             </div>
             <div class="modal-footer">
                 <a href="{{ route('pricing.index') }}#our-plans" class="btn btn-primary">View Pricing Plans</a>
@@ -114,7 +129,7 @@
     </div>
 </div>
 
- 
+
 <div class="modal fade" id="review-history-modal" tabindex="-1" role="dialog" aria-labelledby="Label" aria-hidden="true">
     <div class="modal-dialog ">
         <div class="modal-content">
@@ -122,11 +137,11 @@
                 <h5 class="modal-title"><span  id="review-history-label" ></span></h5>
                 <button type="button" class="close" data-bs-dismiss="modal"    aria-label="Close"><span  aria-hidden="true">&times;</span></button>
             </div>
-            <div class="modal-body"> 
+            <div class="modal-body">
                 <div class="row">
                     <div class="col-xs-12">
                         <div class="table-outer" id="attemt-list">
-                             
+
                         </div>
                     </div>
                 </div>
@@ -136,21 +151,21 @@
     </div>
 </div>
 @endpush
-@push('footer-script') 
+@push('footer-script')
 
 
 <script>
     function showLockedModal() {
         document.getElementById('lockedModal').style.display = 'block';
     }
-    
+
     function closeLockedModal() {
         document.getElementById('lockedModal').style.display = 'none';
     }
     </script>
 
-    
-    <script> 
+
+    <script>
     localStorage.setItem("question-bank", "timed");
     let v='timed'
     function changemode(v){
@@ -168,21 +183,21 @@
             });
             timed= v
             summery_storage.totalcount={{$questioncount??0}};
-            summery_storage.questionids=[]; 
+            summery_storage.questionids=[];
             summery_storage.timercurrent={};
             summery_storage.flagcurrent={};
-            summery_storage.endTime=0; 
-            summery_storage.currentSlug=""; 
+            summery_storage.endTime=0;
+            summery_storage.currentSlug="";
             summery_storage.flagdx={};
             summery_storage.verifydx={};
             summery_storage.cudx=1;
 
             summery_storage.answeridx=[];
-            summery_storage.notansweridx=[];  
+            summery_storage.notansweridx=[];
             summery_storage.timerActive=timed;
             summery_storage.examActive=true;
             summery_storage.timetaken=0;
-            localStorage.setItem("question-bank-summery",JSON.stringify(summery_storage)) 
+            localStorage.setItem("question-bank-summery",JSON.stringify(summery_storage))
     async function confimexam(url){
         var mode = document.querySelector('input[name="timed"]:checked').value;
         if(await showConfirm({ title:"Start the question set in " + mode + " mode?" })){
@@ -212,7 +227,7 @@
                 processing: true,
                 serverSide: true,
                 searching: false,
-                bFilter: false,                
+                bFilter: false,
                 ajax: {
                     url:url
                 },
@@ -220,8 +235,8 @@
                     [0, 'DESC']
                 ],
                 initComplete: function() {
-                    var info = this.api().page.info(); 
-                    var json = this.api().ajax.json(); 
+                    var info = this.api().page.info();
+                    var json = this.api().ajax.json();
 
                     $('#restart-btn').attr('href', json.url);
                     $('#review-history-label').html(` ${json.name} `)
@@ -234,14 +249,14 @@
                         $("#attemt-list-table_wrapper #attemt-list-table_info").show();
                     } else {
                         $("#attemt-list-table_wrapper #attemt-list-table_info").hide();
-                    } 
+                    }
                 },
                 drawCallback: function() {
                     var info = this.api().page.info();
                     var json = this.api().ajax.json();
                     var mode = document.querySelector('input[name="timed"]:checked').value;
                     json.url = `${json.url}?timed=${mode}`;
-                    
+
                     $('#restart-btn').attr('href', json.url);
                     $('#review-history-label').html(` ${json.name} `)
                     if (info.pages > 1) {
@@ -253,9 +268,9 @@
                         $("#attemt-list-table_wrapper #attemt-list-table_info").show();
                     } else {
                         $("#attemt-list-table_wrapper #attemt-list-table_info").hide();
-                    } 
+                    }
                 },
-                columns: [ 
+                columns: [
 
                     {
                         data: 'DT_RowIndex',
@@ -282,14 +297,14 @@
                         searchable: false,
                     },
                     {
-                        data: 'action', 
+                        data: 'action',
                         orderable: false,
-                        searchable: false, 
+                        searchable: false,
                     },
                 ],
             })
         // $.get(url,function(res){
-        //     $.each(res.data,function(k,v){ 
+        //     $.each(res.data,function(k,v){
         //         $('#attemt-list').append(`
         //             <tr>
         //                 <td>${v.date}</td>
