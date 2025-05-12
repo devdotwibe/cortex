9c14d21c8b525791512b4a1e4b9dd67ea4687d28
ArjunlalDotwibe <arjund@twibe@3>
2025-05-12 10:41:06 +0530

    up

--- resources/views/user/learn/index.blade.php
+++ resources/views/user/learn/index.blade.php
@@ -127,7 +127,7 @@ function showLockedModal(slug) {
     }
     else if(slug =='Exam Performance')
     {
-        content="";
+        content="If you are enrolled in our classes, this will be unlocked in Term 3 Week 7";
     }
 
     if(content =="")
