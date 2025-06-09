8399fe24206fcdd42064f47dfe12bbd5ab62b2c5
ArjunlalDotwibe <arjund@twibe@3>
2025-06-09 12:36:30 +0530

    up

--- app/Http/Controllers/User/LiveClassController.php
+++ app/Http/Controllers/User/LiveClassController.php
@@ -271,12 +271,6 @@ class LiveClassController extends Controller
 
         //  $jobStatus = Cache::get("job_status_{$process->jobIdentifier}");
 
-         $subLessonMaterial->status = '';
-
-        $subLessonMaterial->save();
-
-        dd('tesdt');
-
          if ($subLessonMaterial->status !== 'completed' && $subLessonMaterial->status !== 'failled' || $subLessonMaterial->status === '') {
 
             // $process =ProcessFile::dispatch($filepath,$user,$subLessonMaterial,$cachepath);
