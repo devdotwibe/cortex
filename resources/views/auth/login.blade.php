4a3c8f703f86fdc8383f86bdb3739efd77e53cd0
ArjunlalDotwibe <arjund@twibe@3>
2025-07-28 11:20:41 +0530

    up

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,7 @@
             </div>
             <div class="login-right">
                 <div class="card @error('login') login-error @enderror">
-                    <h2>Welcome to Cortex</h2>
+                    <h2>Welcome to Cortex test</h2>
                     <h3>Login</h3>
                     @error('login')
                     <div class="alert alert-danger" role="alert">
@@ -39,16 +39,16 @@
                     @endsession
 
                     <form action="{{url()->current()}}" class="form" method="post">
-                        @csrf 
+                        @csrf
                         <div class="form-group">
                             <div class="form-data">
-                                <div class="forms-inputs mb-4"> 
+                                <div class="forms-inputs mb-4">
                                     <input autocomplete="email" name="email" type="text" placeholder="Enter your email or username" value="{{old('email')}}" class="form-control @error('email') is-invalid @enderror " >
                                     @error('email')
                                     <div class="invalid-feedback">{{$message}}</div>
                                     @enderror
                                 </div>
-                                <div class="forms-inputs mb-4"> 
+                                <div class="forms-inputs mb-4">
                                     <input autocomplete="current-password" name="password" type="password" placeholder="Enter your password" class="form-control @error('password') is-invalid @enderror " >
                                     @error('password')
                                     <div class="invalid-feedback">{{$message}}</div>
@@ -64,26 +64,26 @@
                                     </div>
                                 </div>
 
-                                <div class="mb-3"> 
-                                    <button type="submit" class="btn btn-dark w-100">Login</button> 
+                                <div class="mb-3">
+                                    <button type="submit" class="btn btn-dark w-100">Login</button>
                                 </div>
                             </div>
                         </div>
                     </form>
                     <p>Not registered? <a href="{{route("register")}}" >Register here</a>
 
-                       
-                    </p> 
 
-                    <p>   
-                        <a href="{{ route('password-reset') }}">Forgot your password?</a> 
-                    </p>  
+                    </p>
+
+                    <p>
+                        <a href="{{ route('password-reset') }}">Forgot your password?</a>
+                    </p>
 
                 </div>
             </div>
         </div>
     </div>
-</section> 
+</section>
 
 
 @endsection
