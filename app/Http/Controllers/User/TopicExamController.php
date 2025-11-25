959e2ba060ba373edfd74de0efb532d63f27769d
ArjunlalDotwibe <arjund@twibe@3>
2025-11-25 13:07:05 +0530

    up

--- app/Http/Controllers/User/TopicExamController.php
+++ app/Http/Controllers/User/TopicExamController.php
@@ -235,9 +235,9 @@ class TopicExamController extends Controller
             }
             $user->setProgress("exam-" . $exam->id . "-topic-" . $category->id . "-complete-review", 'yes');
 
-            // dispatch(new SubmitReview($review,$attemt))->onConnection('sync');
+            dispatch(new SubmitReview($review,$attemt))->onConnection('sync');
 
-            dispatch(new SubmitReview($review,$attemt))->onConnection('database')->onQueue('reviews');
+            // dispatch(new SubmitReview($review,$attemt))->onConnection('database')->onQueue('reviews');
 
             Session::forget("topic-test-attempt");
             if ($questioncnt > $passed) {
