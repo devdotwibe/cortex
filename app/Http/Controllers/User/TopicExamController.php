6eb4e864e71ed4d2abaceb171f8b31c3d9f5b281
ArjunlalDotwibe <arjund@twibe@3>
2025-11-25 14:34:18 +0530

    up

--- app/Http/Controllers/User/TopicExamController.php
+++ app/Http/Controllers/User/TopicExamController.php
@@ -25,6 +25,7 @@ use App\Trait\ResourceController;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Storage;
