d98f010805d40f6963484b5d6b18329730627037
ArjunlalDotwibe <arjund@twibe@3>
2025-11-25 10:54:33 +0530

    up

--- app/Http/Controllers/User/ExamQuestionController.php
+++ app/Http/Controllers/User/ExamQuestionController.php
@@ -198,6 +198,8 @@ class ExamQuestionController extends Controller
 
             if($request->ajax()){
 
+                try {
+
                 $userExam = UserExam ::findSlug($request->user_exam);
 
                 if($user->progress('exam-'.$exam->id.'-topic-'.$category->id.'-lesson-'.$subCategory->id.'-set-'.$setname->id.'-complete-date',"")==""){
@@ -301,6 +303,17 @@ class ExamQuestionController extends Controller
                     ];
 
                     return response()->json($response);
+
+                    }
+
+                    catch (\Exception $e) {
+
+                    \Log::error('Answer fetch error: ' . $e->getMessage());
+                    return response()->json([
+                        'error' => 'Failed to load answers',
+                        'message' => $e->getMessage()
+                    ], 500);
+                }
             }
 
             $questioncount=UserExamQuestion::where('user_exam_id',$userExam->id)
