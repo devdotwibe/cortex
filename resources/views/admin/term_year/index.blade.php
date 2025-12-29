6c18315f66761813d3cba2a5e54e944d665172e8
ArjunlalDotwibe <arjund@twibe@3>
2025-12-29 15:15:44 +0530

    yuo

--- resources/views/admin/term_year/index.blade.php
+++ resources/views/admin/term_year/index.blade.php
@@ -49,25 +49,24 @@
 
 @section('modals')
 
-<div class="modal fade" id="delete_modal" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
+
+<div class="modal fade show" id="delete_modal" tabindex="-1" role="dialog" aria-labelledby="delete_modal" aria-modal="true" style="display: block;">
+    <div class="modal-dialog">
         <div class="modal-content">
 
             <div class="modal-header">
-                <h5 class="modal-title">Delete Confirmation Required</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                <h5 class="modal-title" id="delete_modalLablel">Delete Confirmation Required</h5>
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
             </div>
-
             <div class="modal-body">
-                <p>Are you sure you want to delete this record?</p>
+
+                     <p>Are you sure you want to delete the record </p>
+
             </div>
 
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-                    Cancel
-                </button>
-                <button type="button" class="btn btn-danger" id="confirmDeleteBtn">
-                    Delete
+               <button type="button" class="btn btn-danger" id="confirmDeleteBtn">
+                     Delete
                 </button>
             </div>
 
