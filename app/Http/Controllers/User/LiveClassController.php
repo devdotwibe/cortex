6dd74ff194d07627d8215ac60b51b20bd131833c
ArjunlalDotwibe <arjund@twibe@3>
2026-01-07 15:10:02 +0530

    yo

--- app/Http/Controllers/User/LiveClassController.php
+++ app/Http/Controllers/User/LiveClassController.php
@@ -80,7 +80,10 @@ class LiveClassController extends Controller
         $live_class =  LiveClassPage::first();
 
         $time_array = Timetable::where('hide_time', '!=', 'Y')->orderBy('order_no')->get()->map(function($item) {
-            $value = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ') ' . $item->year;
+
+            $term_year = $item->term_year ? '-'.$item->term_year : '';
+
+            $value = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ') ' . $item->year . $term_year;
             $text = $item->day . ' (' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) .'-'. str_replace(' ', '', $item->endtime) . ' ' . implode('.', str_split(strtolower($item->endtime_am_pm))) .  ') (' . $item->type . ') ';
             return [
                 'text' => $text,
