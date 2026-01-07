aab3070c242df590da2be2702635c3718fc0c319
ArjunlalDotwibe <arjund@twibe@3>
2026-01-07 12:34:15 +0530

    up

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -208,7 +208,7 @@ class UserController extends Controller
 
         $time_slot = Timetable::where('hide_time', '!=', 'Y')->whereNull('static')->orderBy('order_no')->get()->map(function($item) {
 
-            $term_year = $item->year ? '-'.$item->year : '';
+            $term_year = $item->term_year ? '-'.$item->term_year : '';
 
             $text = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ')' . $item->year . $term_year;
             $value = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ')' . $item->year;
