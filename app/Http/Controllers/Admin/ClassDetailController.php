578b9976b8b9ac56e3f7351abd826180936094fe
ArjunlalDotwibe <arjund@twibe@3>
2026-01-19 16:02:14 +0530

    up

--- app/Http/Controllers/Admin/ClassDetailController.php
+++ app/Http/Controllers/Admin/ClassDetailController.php
@@ -71,8 +71,12 @@ class ClassDetailController extends Controller
                 ->buildTable();
         }
 
+
         $time_slot = Timetable::where('hide_time', '!=', 'Y')->orderBy('order_no')->get()->map(function($item) {
-            $text = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ')' . $item->year;
+
+            $term_year = $item->term_year ? '-'.$item->term_year : '';
+
+            $text = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ')' . $item->year .$term_year;
             $value = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ')' . $item->year;
             return [
                 'text' => $text,
