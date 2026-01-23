568ab38ef96c00f4a22696b6829706b2d4e1a73b
ArjunlalDotwibe <arjund@twibe@3>
2026-01-23 20:29:37 +0530

    up

--- app/Http/Controllers/Admin/LiveClassController.php
+++ app/Http/Controllers/Admin/LiveClassController.php
@@ -599,7 +599,7 @@ class LiveClassController extends Controller
         $sloteterms_items = Timetable::where('hide_time', '!=', 'Y')->whereNull('static')->orderBy('order_no')->get()->map(function($item) {
             $term_year = $item->term_year ? '-'.$item->term_year : '';
             $text = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ') ' . $item->year .$term_year;
-            $value = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ') ' . $item->year;
+            $value = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ') ' . $item->year .$term_year;
             return [
                 'text' => $text,
                 'id' => $value,
