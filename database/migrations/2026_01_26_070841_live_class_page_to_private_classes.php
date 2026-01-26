b22cb109077923f8f545a298b6352aeae225a262
ArjunlalDotwibe <arjund@twibe@3>
2026-01-26 12:40:18 +0530

    up

---
+++ database/migrations/2026_01_26_070841_live_class_page_to_private_classes.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('live_class_pages', function (Blueprint $table) {
+
+            $table->json('timeslot_ids')->nullable()->after('timeslot');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('live_class_pages', function (Blueprint $table) {
+
+             $table->dropColumn('timeslot_ids');
+        });
+    }
+};
