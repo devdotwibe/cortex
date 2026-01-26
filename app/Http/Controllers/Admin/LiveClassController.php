eddacc1b2d8406ea69e29829790cb95310a5e94d
ArjunlalDotwibe <arjund@twibe@3>
2026-01-26 14:52:35 +0530

    up

--- app/Http/Controllers/Admin/LiveClassController.php
+++ app/Http/Controllers/Admin/LiveClassController.php
@@ -433,7 +433,7 @@ class LiveClassController extends Controller
             if(!empty($request->timeslot)){
                 $slot= $request->timeslot;
                 $this->where(function($qry)use($slot){
-                    $qry->whereJsonContains('timeslot',$slot);
+                    $qry->whereJsonContains('timeslot_ids',$slot);
                 });
             }
 
@@ -605,7 +605,7 @@ class LiveClassController extends Controller
             $value = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ') ' . $item->year .$term_year;
             return [
                 'text' => $text,
-                'id' => $value,
+                'id' => $item->id,
             ];
         })->toArray();
 
