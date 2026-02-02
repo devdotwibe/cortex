93364a4ad32a526a9935306f244d853f4a226796
ArjunlalDotwibe <arjund@twibe@3>
2026-02-02 15:42:31 +0530

    up

--- app/Http/Controllers/User/LiveClassController.php
+++ app/Http/Controllers/User/LiveClassController.php
@@ -190,12 +190,24 @@ class LiveClassController extends Controller
         //     ]
         // ];
 
+        // $sloteterms_items = Timetable::where('hide_time', '!=', 'Y')->orderBy('order_no')->get()->map(function($item) {
+        //     $text = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ')' . $item->year;
+        //     $value = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ')' . $item->year;
+        //     return [
+        //         'text' => $text,
+        //         'id' => $value,
+        //     ];
+        // })->toArray();
+
         $sloteterms_items = Timetable::where('hide_time', '!=', 'Y')->orderBy('order_no')->get()->map(function($item) {
-            $text = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ')' . $item->year;
-            $value = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ')' . $item->year;
+
+            $term_year = $item->term_year ? '-'.$item->term_year : '';
+
+            $text = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ') ' . $item->year .$term_year;
+            $value = $item->day . ' ' . str_replace(' ', '', $item->starttime) . ' ' . implode('.', str_split(strtolower($item->starttime_am_pm))) . '. (' . $item->type . ') ' . $item->year .$term_year;
             return [
                 'text' => $text,
-                'id' => $value,
+                'value' => $value,
             ];
         })->toArray();
 
